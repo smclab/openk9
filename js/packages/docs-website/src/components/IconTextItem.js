5c060c8c7e1ff1af65223921d6e8dc243540d113
Greta Ermacora <greta.ermacora@smc.it>
2021-03-01 18:17:00 +0100

    Create initial components for the landing page

---
+++ js/packages/docs-website/src/components/IconTextItem.js
@@ -0,0 +1,25 @@
+import React from "react";
+import clsx from "clsx";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./iconTextItem.module.css";
+
+export function IconTextItem({ iconSrc, title, description, align }) {
+  const iconUrl = useBaseUrl(iconSrc);
+
+  return (
+    <div
+      className={clsx(
+        styles.iconTextItem,
+        align === "left"
+          ? styles.left
+          : align === "right"
+          ? styles.right
+          : styles.center,
+      )}
+    >
+      {iconUrl && <img alt={title} className={styles.iconItem} src={iconUrl} />}
+      <h4 className={styles.titleIconItem}>{title}</h4>
+      <p className={styles.descriptionIconItem}>{description}</p>
+    </div>
+  );
+}
