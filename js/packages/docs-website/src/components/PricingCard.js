6c60f66853cfb01b2bfd5ed1aca2f052fb9867cf
Greta Ermacora <greta.ermacora@smc.it>
2021-03-03 16:14:42 +0100

    Add components Landing Page

---
+++ js/packages/docs-website/src/components/PricingCard.js
@@ -0,0 +1,29 @@
+import React from "react";
+import clsx from "clsx";
+import useBaseUrl from "@docusaurus/useBaseUrl";
+import styles from "./pricingCard.module.css";
+
+export function PricingCard({ type, price, points, isDisabled }) {
+const imageUrl = useBaseUrl("img/check-mark.svg");
+  return (
+    <div className={clsx(styles.pricingCard, isDisabled && styles.disabledCard)}>
+      <h4 className={clsx(styles.type, isDisabled && styles.disabledType)}>{type}</h4>
+      <div className={styles.price}>
+        <h3 className={styles.amount}>{price}</h3>
+        {price !== "Free" && (<span className={styles.period}>per month</span>)}
+      </div>
+      <div className={styles.points}>
+      {points &&
+        points.length > 0 &&
+        points.map((point, idx) => (
+          <div key={idx}>
+            {point.isChecked && (
+              <img className={styles.iconCheck} src={imageUrl} alt={`Option price {type}`} />
+            )}
+            <span>{point.name}</span>
+          </div>
+        ))}
+        </div>
+    </div>
+  );
+}
