a518fff7d23c85a43f382443e08efd2e07ca2256
Giulio Zausa <mail.zausa.giulio@gmail.com>
2021-03-04 12:58:48 +0100

    Added coming soon page

---
+++ js-packages/docs-website/src/pages/comingsoon.js
@@ -0,0 +1,31 @@
+import React from "react";
+import clsx from "clsx";
+import Layout from "@theme/Layout";
+import styles from "./styles.module.css";
+
+function ComingSoon() {
+  return (
+    <Layout title="OpenK9" description="Search. Everywhere.">
+      <header>
+        <div className="openK9-wrapper">
+          <h1 className={styles.pageTitle}>
+            This Page is{" "}
+            <span className={clsx(styles.primary, styles.secondRow)}>
+              Coming Soon
+            </span>
+          </h1>
+          <p className={styles.pageDescription}>
+            We are working hard to build great docs to help you quickstart your
+            search engine.
+          </p>
+          <p className={styles.pageDescription}>
+            In the meantime, you can check out{" "}
+            <a href="https://github.com/smclab.openk9">GitHub repository</a>.
+          </p>
+        </div>
+      </header>
+    </Layout>
+  );
+}
+
+export default ComingSoon;
