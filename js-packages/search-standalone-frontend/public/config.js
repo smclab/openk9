85fe9d46b65451858cccc6e6c9d8307b27b260c1
Giulio Zausa <mail.zausa.giulio@gmail.com>
2021-03-05 19:14:23 +0100

    Added web tab

--- js-packages/search-standalone-frontend/public/config.js
+++ js-packages/search-standalone-frontend/public/config.js
@@ -1,5 +1,6 @@
 window.config = {
   querySourceBarShortcuts: [
+    { id: "web", text: "web" },
     { id: "document", text: "document" },
     { id: "user", text: "user" },
     { id: "email", text: "email" },
