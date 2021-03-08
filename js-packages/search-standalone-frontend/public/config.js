de73a3469ceb44cdb6b1a11775d8f36ee9a1f8c5
Daniele Caldarini <daniele.caldarini@smc.it>
2021-03-08 10:34:09 +0100

    removed shortcuts bar

--- js-packages/search-standalone-frontend/public/config.js
+++ js-packages/search-standalone-frontend/public/config.js
@@ -1,10 +1,10 @@
 window.config = {
   querySourceBarShortcuts: [
     { id: "web", text: "web" },
-    { id: "document", text: "document" },
-    { id: "user", text: "user" },
-    { id: "email", text: "email" },
-    { id: "calendar", text: "calendar" },
-    { id: "application", text: "application" },
+    // { id: "document", text: "document" },
+    // { id: "user", text: "user" },
+    // { id: "email", text: "email" },
+    // { id: "calendar", text: "calendar" },
+    // { id: "application", text: "application" },
   ],
 };
