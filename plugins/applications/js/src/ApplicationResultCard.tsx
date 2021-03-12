20b65a66e98c16ee0f0f502421d7810befabbe36
Giulio Zausa <mail.zausa.giulio@gmail.com>
2021-03-12 16:52:57 +0100

    Added JS interface for applications

---
+++ plugins/applications/js/src/ApplicationResultCard.tsx
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2020-present SMC Treviso s.r.l. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import React from "react";
+import { createUseStyles } from "react-jss";
+import { Highlight, ResultCard, ThemeType } from "@openk9/search-ui-components";
+
+import { ApplicationResultItem } from "./types";
+
+const useStyles = createUseStyles((theme: ThemeType) => ({
+  card: {
+    alignItems: "center",
+    "&:focus, &:hover h4": {
+      textDecoration: "underline",
+    },
+  },
+  centering: {
+    display: "flex",
+    width: 64,
+    flexShrink: 0,
+    justifyContent: "center",
+    marginRight: theme.spacingUnit * 2,
+    margin: theme.spacingUnit,
+  },
+  iconArea: {
+    display: "flex",
+    backgroundColor: theme.digitalLakePrimary,
+    borderRadius: theme.borderRadius,
+    fontSize: 24,
+  },
+}));
+
+export function ApplicationResultCard({
+  data,
+  className,
+  ...rest
+}: {
+  data: ApplicationResultItem;
+  onSelect?: () => void;
+} & React.AnchorHTMLAttributes<HTMLAnchorElement>): JSX.Element {
+  const classes = useStyles();
+
+  return (
+    <ResultCard
+      href={data.source.application.URL}
+      target="_blank"
+      className={classes.card}
+      {...rest}
+    >
+      <div className={classes.centering}>
+        <div className={classes.iconArea}>
+          <img height={32} src={data.source.application.icon} />
+        </div>
+      </div>
+      <div>
+        <h4>
+          <Highlight
+            text={data.source.application.title}
+            highlight={data.highlight["application.title"]}
+          />
+        </h4>
+        <Highlight
+          text={data.source.application.description}
+          highlight={data.highlight["application.description"]}
+        />
+      </div>
+    </ResultCard>
+  );
+}
