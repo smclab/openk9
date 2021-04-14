1a8354e67de255acb7270ae45dbc3f571b894723
Cristian Bianco <cristian.bianco@smc.it>
2021-04-14 15:12:48 +0200

    typo

--- osgi/entity-manager/entity-manager-api/src/main/java/io/openk9/entity/manager/api/Constants.java
+++ osgi/entity-manager/entity-manager-api/src/main/java/io/openk9/entity/manager/api/Constants.java
@@ -5,7 +5,7 @@ public interface Constants {
 	String ENTITY_ID_FIELD = "id";
 	String ENTITY_NAME_FIELD = "name";
 	String ENTITY_TYPE_FIELD = "type";
-	String ENTITY_TENANT_ID_FIELD = "type";
+	String ENTITY_TENANT_ID_FIELD = "tenantId";
 	String ENTITY = "entity";
 	String ENTITY_INDEX_SUFFIX = "-entity";
 
