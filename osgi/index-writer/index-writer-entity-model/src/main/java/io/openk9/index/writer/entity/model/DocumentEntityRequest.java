c12a5d85530ee0612180b46edc7c8fa64c355fd6
Cristian Bianco <cristian.bianco@smc.it>
2021-05-18 11:20:45 +0200

    Upgrade dependencies and added reactor agent

---
+++ osgi/index-writer/index-writer-entity-model/src/main/java/io/openk9/index/writer/entity/model/DocumentEntityRequest.java
@@ -0,0 +1,18 @@
+package io.openk9.index.writer.entity.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DocumentEntity {
+	private long id;
+	private long tenantId;
+	private String name;
+	private String type;
+	private double score;
+}
