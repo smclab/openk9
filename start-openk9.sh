7f2dc5bf51e70f1153e71892a42f180f6921976e
Daniele Caldarini <daniele.caldarini@smc.it>
2021-05-20 15:24:44 +0200

    deleted sh scripts

--- start-openk9.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker-compose -f docker-compose-elasticsearch.yml -f docker-compose-datasource.yml -f docker-compose-rabbitmq.yml up -d
-docker-compose -f docker-compose-keycloak.yml up -d
-docker-compose -f docker-compose-plugin-driver-manager.yml up -d
-docker-compose -f docker-compose-ingestion.yml -f docker-compose-index-writer.yml -f docker-compose-entity-manager.yml -f docker-compose-searcher.yml up -d
-docker-compose -f docker-compose-parser.yml -f docker-compose-ml.yml up -d 
-docker-compose -f docker-compose-frontend.yml up -d
