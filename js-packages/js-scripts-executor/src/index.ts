bfcf1ba8443df1a62697011e6e9c47a477f9a347
Giulio Zausa <mail.zausa.giulio@gmail.com>
2021-06-30 10:14:56 +0200

    Fixed deprecated bodyParser

--- js-packages/js-scripts-executor/src/index.ts
+++ js-packages/js-scripts-executor/src/index.ts
@@ -16,10 +16,9 @@
  */
 
 import express from "express";
-import bodyParser from "body-parser";
 
 const app = express();
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.post("/exec", async (req, res) => {
   try {
