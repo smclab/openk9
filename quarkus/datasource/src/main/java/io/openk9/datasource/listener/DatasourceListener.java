82dce6bf9ca6faa0239ea7aabfc609de71f55024
Cristian Bianco <cristian.bianco@smc.it>
2021-10-25 17:22:51 +0200

    Refactoring emitter

--- quarkus/datasource/src/main/java/io/openk9/datasource/listener/DatasourceListener.java
+++ quarkus/datasource/src/main/java/io/openk9/datasource/listener/DatasourceListener.java
@@ -1,13 +1,13 @@
 package io.openk9.datasource.listener;
 
+import io.openk9.datasource.emitter.datasource.DeleteEmitter;
+import io.openk9.datasource.emitter.datasource.InsertEmitter;
+import io.openk9.datasource.emitter.datasource.UpdateEmitter;
 import io.openk9.datasource.model.Datasource;
-import org.eclipse.microprofile.reactive.messaging.Channel;
-import org.eclipse.microprofile.reactive.messaging.Emitter;
 import org.jboss.logging.Logger;
 import org.quartz.SchedulerException;
 
 import javax.enterprise.inject.spi.CDI;
-import javax.inject.Inject;
 import javax.persistence.PostPersist;
 import javax.persistence.PostRemove;
 import javax.persistence.PreUpdate;
@@ -17,13 +17,17 @@ public class DatasourceListener {
 	@PostPersist
 	public void beforeAdd(Datasource datasource) {
 		_createOrUpdateScheduler(datasource);
-		_insertEmitter.send(datasource);
+		InsertEmitter insertEmitter =
+			CDI.current().select(InsertEmitter.class).get();
+		insertEmitter.send(datasource);
 	}
 
 	@PreUpdate
 	public void beforeUpdate(Datasource datasource) {
 		_createOrUpdateScheduler(datasource);
-		_updateEmitter.send(datasource);
+		UpdateEmitter updateEmitter =
+			CDI.current().select(UpdateEmitter.class).get();
+		updateEmitter.send(datasource);
 	}
 
 	@PostRemove
@@ -38,6 +42,11 @@ public class DatasourceListener {
 			_log.error(e.getMessage(), e);
 		}
 
+		DeleteEmitter deleteEmitter =
+			CDI.current().select(DeleteEmitter.class).get();
+
+		deleteEmitter.send(datasource);
+
 	}
 
 	private void _createOrUpdateScheduler(Datasource datasource) {
@@ -51,21 +60,7 @@ public class DatasourceListener {
 			_log.error(e.getMessage(), e);
 		}
 
-		_deleteEmitter.send(datasource);
-
 	}
-
-	@Inject
-	@Channel("datasource-DELETE-Datasource")
-	Emitter<Datasource> _deleteEmitter;
-
-	@Inject
-	@Channel("datasource-INSERT-Datasource")
-	Emitter<Datasource> _insertEmitter;
-
-	@Inject
-	@Channel("datasource-UPDATE-Datasource")
-	Emitter<Datasource> _updateEmitter;
 
 	private static final Logger _log = Logger.getLogger(
 		DatasourceListener.class);
