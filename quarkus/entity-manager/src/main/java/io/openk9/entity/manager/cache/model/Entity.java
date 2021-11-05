73f8a4fbf09ff440cdd88296c58db1548c63ad75
Cristian Bianco <cristian.bianco@smc.it>
2021-11-05 09:22:46 +0100

    Implements Comparable

--- quarkus/entity-manager/src/main/java/io/openk9/entity/manager/cache/model/Entity.java
+++ quarkus/entity-manager/src/main/java/io/openk9/entity/manager/cache/model/Entity.java
@@ -13,11 +13,27 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class Entity implements Serializable {
+public class Entity implements Serializable, Comparable<Entity> {
 	private Long id;
 	@EqualsAndHashCode.Include
 	private Long cacheId;
 	private Long tenantId;
 	private String name;
 	private String type;
+
+	@Override
+	public int compareTo(Entity other) {
+		Entity p1 = this;
+		int res = String.CASE_INSENSITIVE_ORDER.compare(
+			p1.getName(), other.getName());
+		if (res != 0) {
+			return res;
+		}
+		res = String.CASE_INSENSITIVE_ORDER.compare(
+			p1.getType(), other.getType());
+		if (res != 0) {
+			return res;
+		}
+		return Long.compare(p1.getTenantId(), other.getTenantId());
+	}
 }
