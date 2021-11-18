a919a4958b62f1384226cca0fd916a5c0a59a614
Cristian Bianco <cristian.bianco@smc.it>
2021-11-18 16:24:43 +0100

    Fix comparator

--- osgi/search/search-service/src/main/java/io/openk9/search/query/internal/http/QueryAnalysisHttpHandler.java
+++ osgi/search/search-service/src/main/java/io/openk9/search/query/internal/http/QueryAnalysisHttpHandler.java
@@ -267,22 +267,37 @@ public class QueryAnalysisHttpHandler implements RouterHandler, HttpHandler {
 			Set<String> lKeys = o1.keySet();
 			Set<String> rKeys = o2.keySet();
 
+			double scoreO1 = toDouble(o1.getOrDefault("score", -1.0));
+			double scoreO2 = toDouble(o2.getOrDefault("score", -1.0));
+
+			int scoreCompared = Double.compare(scoreO1, scoreO2);
+
 			if (lKeys.size() == rKeys.size()) {
 				for (String lKey : lKeys) {
 					if (!lKey.equals("score")) {
 						Object lValue = o1.get(lKey);
 						Object rValue = o2.get(lKey);
 						if (!Objects.deepEquals(lValue, rValue)) {
+
+							if (scoreCompared != 0) {
+								return scoreCompared;
+							}
+
 							return -1;
 						}
 					}
 				}
 			}
+			else {
+				if (scoreCompared != 0) {
+					return scoreCompared;
+				}
+				else {
+					return -1;
+				}
+			}
 
-			double scoreO1 = toDouble(o1.getOrDefault("score", -1.0));
-			double scoreO2 = toDouble(o2.getOrDefault("score", -1.0));
-
-			return -Double.compare(scoreO1, scoreO2);
+			return 0;
 
 		}
 
