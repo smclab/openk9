cd9ccf54f1bd102db4168d30be95ba5da68359f7
Cristian Bianco <cristian.bianco@smc.it>
2021-11-22 15:12:50 +0100

    Fix: function score query

--- osgi/search/search-service/src/main/java/io/openk9/search/query/internal/parser/DateOrderQueryParser.java
+++ osgi/search/search-service/src/main/java/io/openk9/search/query/internal/parser/DateOrderQueryParser.java
@@ -1,8 +1,11 @@
 package io.openk9.search.query.internal.parser;
 
+import io.openk9.plugin.driver.manager.model.DocumentTypeDTO;
+import io.openk9.plugin.driver.manager.model.PluginDriverDTO;
 import io.openk9.search.api.query.QueryParser;
 import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.functionscore.FunctionScoreQueryBuilder;
 import org.elasticsearch.index.query.functionscore.ScoreFunctionBuilders;
 import org.osgi.service.component.annotations.Activate;
 import org.osgi.service.component.annotations.Component;
@@ -11,6 +14,7 @@ import org.osgi.service.metatype.annotations.Designate;
 import org.osgi.service.metatype.annotations.ObjectClassDefinition;
 import reactor.core.publisher.Mono;
 
+import java.util.Collection;
 import java.util.function.Consumer;
 
 @Component(
@@ -36,15 +40,37 @@ public class DateOrderQueryParser implements QueryParser {
 	@Override
 	public Mono<Consumer<BoolQueryBuilder>> apply(Context context) {
 
-		return Mono.fromSupplier(() ->
-			(bool) -> bool.should(
-				QueryBuilders
-					.functionScoreQuery(
-						QueryBuilders.matchAllQuery(),
-						ScoreFunctionBuilders.linearDecayFunction(
-							_fieldName, null, _scale)
-					)
-			)
+		return Mono.fromSupplier(() -> (bool) -> {
+
+			FunctionScoreQueryBuilder.FilterFunctionBuilder[] filterFunctionBuilders =
+				context
+					.getPluginDriverDocumentTypeList()
+					.stream()
+					.map(PluginDriverDTO::getDocumentTypes)
+					.flatMap(Collection::stream)
+					.map(DocumentTypeDTO::getName)
+					.map(s -> {
+
+						String fieldName = s + "." + _fieldName;
+
+						return new FunctionScoreQueryBuilder.FilterFunctionBuilder(
+							QueryBuilders.existsQuery(fieldName),
+							ScoreFunctionBuilders.linearDecayFunction(
+								fieldName, null, _scale)
+						);
+					})
+					.toArray(
+						FunctionScoreQueryBuilder.FilterFunctionBuilder[]::new);
+
+
+			bool.should(
+					QueryBuilders
+						.functionScoreQuery(
+							QueryBuilders.matchAllQuery(),
+							filterFunctionBuilders
+						)
+				);
+			}
 		);
 	}
 
