35827379501b78348b777590f7bafdde972d6312
Cristian Bianco <cristian.bianco@smc.it>
2021-12-10 16:49:58 +0100

    Added _name field

--- osgi/vertx/vertx-configuration-plugin/src/main/java/io/openk9/vertx/configuration/plugin/ConsulRegisterNode.java
+++ osgi/vertx/vertx-configuration-plugin/src/main/java/io/openk9/vertx/configuration/plugin/ConsulRegisterNode.java
@@ -26,7 +26,10 @@ import java.util.function.Supplier;
 )
 public class ConsulRegisterNode implements Supplier<String> {
 
-	private String _nodeId;
+	@Override
+	public String get() {
+		return _name;
+	}
 
 	@Activate
 	void activate() {
@@ -34,13 +37,12 @@ public class ConsulRegisterNode implements Supplier<String> {
 		ServiceOptions options = new ServiceOptions();
 		String address =
 			getenv.getOrDefault("CONSUL_SERVICE_ADDRESS", "localhost");
-		String name =
-			getenv.getOrDefault("CONSUL_SERVICE_NAME", address);
+		_name = getenv.getOrDefault("CONSUL_SERVICE_NAME", address);
 		options.setAddress(address);
 		options.setPort(Integer.parseInt(getenv.getOrDefault("CONSUL_SERVICE_PORT", "8080")));
-		options.setName(name);
+		options.setName(_name);
 		options.setCheckOptions(new CheckOptions().setTtl("15s"));
-		_nodeId = String.join("-", name, randomString());
+		_nodeId = String.join("-", _name, randomString());
 		options.setId(_nodeId);
 		_consulClient
 			.registerService(options)
@@ -55,11 +57,15 @@ public class ConsulRegisterNode implements Supplier<String> {
 			.toCompletionStage()
 			.toCompletableFuture().join();
 		_nodeId = null;
+		_name = null;
 	}
 
 	public String randomString() {
 		return UUID.randomUUID().toString();
 	}
+
+	private volatile String _nodeId;
+	private volatile String _name;
 
 	@Reference
 	private ConsulClient _consulClient;
@@ -67,11 +73,4 @@ public class ConsulRegisterNode implements Supplier<String> {
 	private static final Logger _log = LoggerFactory.getLogger(
 		ConsulRegisterNode.class);
 
-	@Override
-	public String get() {
-		Map<String, String> getenv = System.getenv();
-		String address =
-			getenv.getOrDefault("CONSUL_SERVICE_ADDRESS", "localhost");
-		return getenv.getOrDefault("CONSUL_SERVICE_NAME", address);
-	}
 }
