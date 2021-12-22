28cd196afb8ee6151082c4bec060a696b3c8b603
Cristian Bianco <cristian.bianco@smc.it>
2021-12-22 12:52:13 +0100

    Fix lastIngestionDate

--- quarkus/datasource/src/main/java/io/openk9/datasource/processor/DatasourceProcessor.java
+++ quarkus/datasource/src/main/java/io/openk9/datasource/processor/DatasourceProcessor.java
@@ -8,60 +8,18 @@ import io.openk9.datasource.model.Tenant;
 import io.openk9.datasource.processor.payload.DatasourceContext;
 import io.openk9.datasource.processor.payload.IngestionDatasourcePayload;
 import io.openk9.datasource.processor.payload.IngestionPayload;
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import io.smallrye.reactive.messaging.annotations.Blocking;
 import io.vertx.core.json.JsonObject;
-import org.eclipse.microprofile.context.ManagedExecutor;
-import org.eclipse.microprofile.context.ThreadContext;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.eclipse.microprofile.reactive.messaging.Outgoing;
-import reactor.core.Disposable;
-import reactor.core.publisher.Sinks;
-import reactor.core.scheduler.Schedulers;
 
-import javax.annotation.PostConstruct;
-import javax.annotation.PreDestroy;
 import javax.enterprise.context.ApplicationScoped;
 import javax.transaction.Transactional;
-import java.time.Duration;
 import java.time.Instant;
 import java.util.List;
 
 @ApplicationScoped
 public class DatasourceProcessor {
-
-	@PostConstruct
-	public void start() {
-
-		ManagedExecutor executor = ManagedExecutor.builder()
-			.propagated(ThreadContext.CDI, ThreadContext.TRANSACTION)
-			.build();
-
-		ThreadContext threadContext = ThreadContext.builder()
-			.propagated(ThreadContext.CDI, ThreadContext.TRANSACTION)
-			.build();
-
-		_many = Sinks
-			.unsafe()
-			.many()
-			.multicast()
-			.onBackpressureBuffer();
-
-		_disposable = _many
-			.asFlux()
-			.groupBy(Datasource::getDatasourceId)
-			.flatMap(group -> group.sample(Duration.ofSeconds(30)))
-			.subscribeOn(Schedulers.fromExecutorService(executor))
-			.subscribe(threadContext.contextualConsumer(PanacheEntityBase::persistAndFlush));
-
-	}
-
-	@PreDestroy
-	public void stop() {
-		_disposable.dispose();
-		_many.emitComplete(Sinks.EmitFailureHandler.FAIL_FAST);
-	}
-
 
 	@Incoming("ingestion")
 	@Outgoing("ingestion-datasource")
@@ -76,12 +34,6 @@ public class DatasourceProcessor {
 
 		Datasource datasource =
 			Datasource.findById(datasourceId);
-
-		datasource.setLastIngestionDate(
-			Instant.ofEpochMilli(
-				ingestionPayload.getParsingDate()));
-
-		_many.tryEmitNext(datasource);
 
 		Tenant tenant = Tenant.findById(datasource.getTenantId());
 
@@ -105,12 +57,15 @@ public class DatasourceProcessor {
 			datasource, tenant, enrichPipeline, enrichItemList
 		);
 
+		datasource.setLastIngestionDate(
+			Instant.ofEpochMilli(
+				ingestionPayload.getParsingDate()));
+
+		datasource.persistAndFlush();
+
 		return IngestionDatasourcePayload.of(
 			ingestionPayload, datasourceContext);
 
 	}
-
-	private Sinks.Many<Datasource> _many;
-	private Disposable _disposable;
 
 }
