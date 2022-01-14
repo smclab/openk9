4775bb87dd4cad5b977e788ef7d1cdd30cba8755
Frederik Batuna <fbatuna@key-biz.it>
2022-01-14 15:16:51 +0100

    refactor pdf renderers

--- js-packages/search-frontend/src/renderer-components/DetailAttribute.tsx
+++ js-packages/search-frontend/src/renderer-components/DetailAttribute.tsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { css } from "styled-components/macro";
 
-type DetailLinkProps = {
+type DetailAttributeProps = {
+  label: string;
   children: React.ReactNode;
 };
-export function DetailLink({ children }: DetailLinkProps) {
+export function DetailAttribute({ label, children }: DetailAttributeProps) {
   return (
     <div
       css={css`
-        font-size: 0.8em;
         margin-bottom: 8px;
-        word-wrap: break-word;
-        word-break: break-word;
       `}
     >
+      <strong>{label}: </strong>
       {children}
     </div>
   );
--- js-packages/search-frontend/src/renderers/openk9/pdf/PdfDetail.tsx
+++ js-packages/search-frontend/src/renderers/openk9/pdf/PdfDetail.tsx
@@ -2,7 +2,14 @@ import { faFilePdf } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { css } from "styled-components/macro";
-import { HighlightedText } from "../../../renderer-components/HighlightedText";
+import { myTheme } from "../../../components/myTheme";
+import { DetailAttribute } from "../../../renderer-components/DetailAttribute";
+import { DetailContainer } from "../../../renderer-components/DetailContainer";
+import { DetailIconContainer } from "../../../renderer-components/DetailIconContainer";
+import { DetailLink } from "../../../renderer-components/DetailLink";
+import { DetailTextContent } from "../../../renderer-components/DetailTextContent";
+import { DetailTitle } from "../../../renderer-components/DetailTitle";
+import { HighlightableText } from "../../../renderer-components/HighlightableText";
 import { PdfResultItem } from "./PdfItem";
 
 type PdfDetailProps = {
@@ -10,46 +17,20 @@ type PdfDetailProps = {
 };
 export function PdfDetail({ result }: PdfDetailProps) {
   return (
-    <div
-      css={css`
-        display: grid;
-        grid-row-gap: 8px;
-        grid-auto-flow: row;
-      `}
-    >
-      <FontAwesomeIcon icon={faFilePdf} />
-      <div
-        css={css`
-          font-size: 1.5em;
-          font-weight: 500;
-          word-wrap: break-word;
-          word-break: break-word;
-        `}
-      >
-        {result.highlight["document.title"] ? (
-          <HighlightedText text={result.highlight["document.title"][0]} />
-        ) : (
-          result.source.document?.title
-        )}
-      </div>
-      <div
-        css={css`
-          font-size: 0.8em;
-          word-wrap: break-word;
-          word-break: break-word;
-        `}
-      >
-        {result.highlight["document.url"] ? (
-          <HighlightedText text={result.highlight["document.url"][0]} />
-        ) : (
-          result.source.document?.url
-        )}
-      </div>
+    <DetailContainer>
+      <DetailIconContainer>
+        <FontAwesomeIcon icon={faFilePdf} />
+      </DetailIconContainer>
+      <DetailTitle>
+        <HighlightableText result={result} path="document.title" />
+      </DetailTitle>
+      <DetailLink>
+        <HighlightableText result={result} path="document.url" />
+      </DetailLink>
       {result.source.file?.lastModifiedDate && (
-        <div>
-          <strong>Last modified</strong> :{" "}
-          {new Date(result.source.file?.lastModifiedDate).toLocaleString()}
-        </div>
+        <DetailAttribute label="Last modified">
+          {new Date(result.source.file.lastModifiedDate).toLocaleString()}
+        </DetailAttribute>
       )}
       <div>
         {result.source.resources.binaries.map((binary) => {
@@ -61,29 +42,14 @@ export function PdfDetail({ result }: PdfDetailProps) {
               css={css`
                 width: 100%;
                 height: 50vh;
-                border: none;
+                border: 1px solid ${myTheme.searchBarBorderColor};
                 border-radius: 4px;
               `}
             />
           );
         })}
       </div>
-      <div
-        css={css`
-          word-wrap: break-word;
-          word-break: break-word;
-        `}
-      >
-        {result.highlight["document.content"] ? (
-          result.highlight["document.content"].map((text, index) => (
-            <div key={index}>
-              <HighlightedText text={text} />
-            </div>
-          ))
-        ) : (
-          <div>{result.source.document?.content}</div>
-        )}
-      </div>
-    </div>
+      <DetailTextContent result={result} path="document.content" />
+    </DetailContainer>
   );
 }
