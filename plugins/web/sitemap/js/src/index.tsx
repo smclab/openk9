55d7e7cfcfa1161354bde270554b1e813827cf8c
Daniele Caldarini <daniele.caldarini@smc.it>
2022-03-04 14:28:10 +0100

    fixed index sitemap plugin

--- plugins/web/sitemap/js/src/index.tsx
+++ plugins/web/sitemap/js/src/index.tsx
@@ -44,5 +44,29 @@ export const plugin: Plugin<WebResultItem> = {
           ]
       }`,
     },
+    {
+      type: "ENRICH",
+      displayName: "Web Sitemap Async Tika",
+      serviceName:
+        "io.openk9.plugins.web.sitemap.enrichprocessor.AsyncTikaEnrichProcessor",
+      initialSettings: `
+      {
+          "type_mapping": {
+              "application/pdf": "pdf",
+              "application/msword": "word",
+              "application/vnd.openxmlformats-officedocument.wordprocessingml.document": "word",
+              "application/vnd.ms-excel": "excel",
+              "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet": "excel",
+              "application/vnd.ms-powerpoint": "powerpoint",
+              "application/vnd.openxmlformats-officedocument.presentationml.presentation": "powerpoint",
+              "text/plain": "plaintext",
+              "application/vnd.oasis.opendocument.spreadsheet": "excel",
+              "application/vnd.oasis.opendocument.text": "word",
+          "application/vnd.oasis.opendocument.presentation": "powerpoint",
+              "application/epub+zip": "epub"
+          },
+      "max_length": 10000
+      }`,
+    },
   ],
 };
