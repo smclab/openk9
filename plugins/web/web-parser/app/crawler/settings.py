9850d9144a6f8667979702e49d9ada38fb2b5331
Daniele Caldarini <daniele.caldarini@smc.it>
2022-03-04 12:52:19 +0100

    obey robots to false

--- plugins/web/web-parser/app/crawler/settings.py
+++ plugins/web/web-parser/app/crawler/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = 'crawler.spiders'
 #USER_AGENT = 'crawler (+http://www.yourdomain.com)'
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
