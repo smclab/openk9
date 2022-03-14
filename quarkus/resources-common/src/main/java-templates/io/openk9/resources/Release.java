89ad53c2249ee949226cc2e74c148028f0d997fe
Cristian Bianco <cristian.bianco@smc.it>
2022-03-14 10:37:07 +0100

    Refactoring quarkus maven project.

---
+++ quarkus/resources-common/src/main/java-templates/io/openk9/resources/Release.java
@@ -0,0 +1,26 @@
+package io.openk9.resources;
+
+public final class Release {
+
+	public static String getProductName() {
+		return "${product.name}";
+	}
+
+	public static String getVersion() {
+		return "${product.version}";
+	}
+
+	public static void printRelease() {
+		String output =
+			Logo.getLogo() +
+			Logo.getEmptySpace() +
+			String.format("%s (%s)", getProductName(), getVersion());
+
+		System.out.println(output);
+	}
+
+	static {
+		printRelease();
+	}
+	
+}
