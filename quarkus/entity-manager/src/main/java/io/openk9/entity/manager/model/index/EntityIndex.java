0bd194d43f5486c894d37a0fa9e97d5d13adf8c8
Cristian Bianco <cristian.bianco@smc.it>
2022-03-15 19:44:34 +0100

    added empty constructor.

--- quarkus/entity-manager/src/main/java/io/openk9/entity/manager/model/index/EntityIndex.java
+++ quarkus/entity-manager/src/main/java/io/openk9/entity/manager/model/index/EntityIndex.java
@@ -3,22 +3,24 @@ package io.openk9.entity.manager.model.index;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 
 @Data
 @Builder
 @AllArgsConstructor(staticName = "of")
+@NoArgsConstructor
 @ToString(onlyExplicitlyIncluded = true)
 public class EntityIndex {
 	@ToString.Include
-	private final String id;
+	private String id;
 	@ToString.Include
-	private final Long graphId;
+	private Long graphId;
 	@ToString.Include
-	private final long tenantId;
+	private long tenantId;
 	@ToString.Include
-	private final String name;
+	private String name;
 	@ToString.Include
-	private final String type;
+	private String type;
 	private float score;
 }
