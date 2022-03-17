c649f0b2024ff5bdcd173e62482a0128307cea2a
Cristian Bianco <cristian.bianco@smc.it>
2022-03-17 13:02:13 +0100

    Added AsyncEmailEnrichProcessor

---
+++ plugins/email/src/main/java/io/openk9/plugins/email/enrichprocessor/AsyncEmailEnrichProcessor.java
@@ -0,0 +1,20 @@
+package io.openk9.plugins.email.enrichprocessor;
+
+import io.openk9.search.enrich.api.AsyncEnrichProcessor;
+import io.openk9.search.enrich.api.EnrichProcessor;
+import org.osgi.service.component.annotations.Component;
+
+@Component(immediate = true, service = EnrichProcessor.class)
+public class AsyncEmailEnrichProcessor implements AsyncEnrichProcessor {
+
+    @Override
+    public String destinationName() {
+        return "io.openk9.email";
+    }
+
+    @Override
+    public String name() {
+        return AsyncEmailEnrichProcessor.class.getName();
+    }
+
+}
