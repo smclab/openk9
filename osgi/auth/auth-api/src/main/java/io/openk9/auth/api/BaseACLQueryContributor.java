290c60742135455fb0f30418df78c31e082d4a0b
Cristian Bianco <cristian.bianco@smc.it>
2022-05-02 17:42:08 +0200

    Added Base ACL contributor class

---
+++ osgi/auth/auth-api/src/main/java/io/openk9/auth/api/BaseACLQueryContributor.java
@@ -0,0 +1,36 @@
+package io.openk9.auth.api;
+
+import org.elasticsearch.index.query.BoolQueryBuilder;
+import org.elasticsearch.index.query.QueryBuilders;
+
+import java.util.List;
+import java.util.Map;
+
+public abstract class BaseACLQueryContributor implements ACLQueryContributor {
+
+	public abstract String getPluginName();
+
+	@Override
+	public void accept(UserInfo userInfo, BoolQueryBuilder boolQueryBuilder) {
+
+		Map<String, List<String>> realmAccess = userInfo.getRealmAccess();
+
+		List<String> roles = realmAccess.getOrDefault("roles", List.of());
+
+		if (!roles.isEmpty()) {
+
+			String fieldName = "acl." + getPluginName() + ".rolesName.keyword";
+
+			BoolQueryBuilder innerQuery = QueryBuilders.boolQuery();
+
+			for (String role : roles) {
+				innerQuery.should(QueryBuilders.matchQuery(fieldName, role));
+			}
+
+			boolQueryBuilder.must(innerQuery);
+
+		}
+
+	}
+
+}
