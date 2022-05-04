c3a444ab4d6dbb3ef533b9bfef99fb7a69f1d2e6
Frederik Batuna <frederik.batuna@smc.it>
2022-05-04 21:39:42 +0200

    fix

--- js-packages/plugin-template-developer-tool/webpack.config.js
+++ js-packages/plugin-template-developer-tool/webpack.config.js
@@ -7,34 +7,28 @@ module.exports = ({ proxyTarget }) => ({
     index: "./src/template-examples.tsx",
   },
   devtool: "inline-source-map",
-  resolveLoader: {
-    // Configure how Webpack finds `loader` modules.
-    modules: [
-      path.resolve(__dirname, "../../node_modules"),
-      path.resolve(__dirname, "node_modules"),
-      "./node_modules",
-    ],
-  },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            plugins: [
-              "macros",
-              require("react-refresh/babel"),
-              [
-                "@babel/plugin-transform-runtime",
-                {
-                  regenerator: true,
-                },
-              ],
+        loader: require.resolve("babel-loader"),
+        options: {
+          plugins: [
+            require.resolve("babel-plugin-macros"),
+            require.resolve("react-refresh/babel"),
+            [
+              require.resolve("@babel/plugin-transform-runtime"),
+              {
+                regenerator: true,
+              },
             ],
-            presets: ["@babel/preset-env", "@babel/react", "@babel/typescript"],
-          },
+          ],
+          presets: [
+            require.resolve("@babel/preset-env"),
+            require.resolve("@babel/preset-react"),
+            require.resolve("@babel/preset-typescript"),
+          ],
         },
       },
       {
@@ -50,11 +44,6 @@ module.exports = ({ proxyTarget }) => ({
   plugins: [new ReactRefreshWebpackPlugin()],
   resolve: {
     extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name].js",
-    libraryTarget: "umd",
   },
   devServer: {
     hot: true,
