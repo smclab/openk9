54869be14cf6d1ea0c75a9bb43189632df7c3f63
Cristian Bianco <cristian.bianco@smc.it>
2022-05-12 18:57:44 +0200

    [query analysis] Fix: added strip in split method and managed token index

--- osgi/search/search-service/src/main/java/io/openk9/search/query/internal/query/parser/util/Utils.java
+++ osgi/search/search-service/src/main/java/io/openk9/search/query/internal/query/parser/util/Utils.java
@@ -17,12 +17,68 @@
 
 package io.openk9.search.query.internal.query.parser.util;
 
+import io.openk9.common.api.constant.Strings;
 import io.openk9.search.api.query.parser.Tuple;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 public class Utils {
 
+	public static Map<Integer, ? extends TokenIndex> toTokenIndexMap(
+		String searchText) {
+
+		Map<Integer, StringBuilderTokenIndex> map = new HashMap<>();
+
+		for (int i = 0, count = 0;  i < searchText.length(); i++) {
+
+			char c = searchText.charAt(i);
+
+			if (Character.isWhitespace(c)) {
+
+				if (!map.containsKey(count)) {
+					continue;
+				}
+
+				StringBuilderTokenIndex stringBuilderIndex = map.get(count);
+
+				if (stringBuilderIndex != null) {
+					stringBuilderIndex.setEndIndex(i);
+				}
+
+				count++;
+			}
+			else {
+
+				final int index = i;
+
+				StringBuilderTokenIndex stringBuilderIndex =
+					map.computeIfAbsent(
+						count, integer -> new StringBuilderTokenIndex(index));
+
+				stringBuilderIndex.append(c);
+
+			}
+		}
+
+		return map;
+
+	}
 	public static String[] split(String s) {
-		return s.split("\\s+");
+		return s.strip().split("\\s+");
+	}
+
+	public static boolean equalsIgnoreSpaces(String str1, String str2) {
+		return Objects.equals(
+			removeBlankSpaces(str1), removeBlankSpaces(str2));
+	}
+
+	public static String removeBlankSpaces(String s) {
+		if (s == null || s.isBlank()) {
+			return Strings.BLANK;
+		}
+		return s.replaceAll("\\s+", Strings.BLANK);
 	}
 
 	public static <T> Tuple<T> toTuple(T[] rhs) {
@@ -34,6 +90,69 @@ public class Utils {
 
 	public static String[] toArray(Tuple<String> tuple) {
 		return tuple.toArray();
+	}
+
+	private static class StringBuilderTokenIndex
+		implements TokenIndex, Appendable {
+		public StringBuilderTokenIndex(int startIndex) {
+			this.startIndex = startIndex;
+			this.sb = new StringBuilder();
+		}
+		@Override
+		public int getStartIndex() {
+			return startIndex;
+		}
+
+		@Override
+		public int getEndIndex() {
+			return endIndex;
+		}
+
+		@Override
+		public String getToken() {
+			return result == null ? (result = sb.toString()) : result;
+		}
+
+		@Override
+		public Appendable append(CharSequence csq) {
+			return sb.append(csq);
+		}
+
+		@Override
+		public Appendable append(CharSequence csq, int start, int end) {
+			return sb.append(csq, start, end);
+		}
+
+		@Override
+		public Appendable append(char c) {
+			return sb.append(c);
+		}
+
+		public void setEndIndex(int endIndex) {
+			this.endIndex = endIndex;
+		}
+
+		@Override
+		public String toString() {
+			return "TokenIndex{" +
+				   "startIndex=" + startIndex +
+				   ", endIndex=" + endIndex +
+				   ", token=" + getToken() +
+				   '}';
+		}
+
+		private final int startIndex;
+		private int endIndex;
+		private final StringBuilder sb;
+
+		private String result;
+
+	}
+
+	public interface TokenIndex {
+		int getStartIndex();
+		int getEndIndex();
+		String getToken();
 	}
 
 }
