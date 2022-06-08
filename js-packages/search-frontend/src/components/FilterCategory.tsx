302548a8c473d44233c600ff33c34324b9fb2424
daniele caldarini <daniele.caldarini@smc.it>
2022-06-08 20:50:46 +0200

    ch: filter page size

--- js-packages/search-frontend/src/components/FilterCategory.tsx
+++ js-packages/search-frontend/src/components/FilterCategory.tsx
@@ -209,7 +209,7 @@ export function useInfiniteSuggestions(
   activeSuggestionCategory: number,
   suggestKeyword: string,
 ) {
-  const pageSize = 10;
+  const pageSize = 15;
   const client = useOpenK9Client();
   return useInfiniteQuery(
     [
