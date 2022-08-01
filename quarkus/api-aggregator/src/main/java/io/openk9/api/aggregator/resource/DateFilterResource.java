d75a4430d55730fbe5495344388fa10df6c1196e
Cristian Bianco <cristian.bianco@smc.it>
2022-08-01 10:48:19 +0200

    Issue #234: added date_filter entity
    (cherry picked from commit c711b260809bb061e84bd69040a77be84a014f37)

---
+++ quarkus/api-aggregator/src/main/java/io/openk9/api/aggregator/resource/DateFilterResource.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright (c) 2020-present SMC Treviso s.r.l. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.openk9.api.aggregator.resource;
+
+import io.openk9.api.aggregator.api.DateFilterHttp;
+import io.openk9.api.aggregator.client.DateFilterClient;
+import lombok.experimental.Delegate;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+
+@Path("/api/datasource")
+public class DateFilterResource extends FaultTolerance implements DateFilterHttp {
+	@RestClient
+	@Inject
+	@Delegate
+	DateFilterClient _datasourceClient;
+}
