e18880d2c9b7ee9bd2784a3ddf6297a9e44eb71a
Frederik Batuna <frederik.batuna@smc.it>
2022-09-19 15:53:41 +0200

    add: css badge cust

--- js-packages/search-frontend/src/renderer-components/Badge.tsx
+++ js-packages/search-frontend/src/renderer-components/Badge.tsx
@@ -1,22 +1,8 @@
 import React from "react";
-import { css } from "styled-components/macro";
 
 type BadgeProps = {
   children: React.ReactNode;
 };
 export function Badge({ children }: BadgeProps) {
-  return (
-    <div
-      css={css`
-        padding: 8px 16px;
-        background-color: var(
-          --openk9-embeddable-search--secondary-background-color
-        );
-        margin-right: 8px;
-        border-radius: 4px;
-      `}
-    >
-      {children}
-    </div>
-  );
+  return <div className="openk9-embeddable-search--result-badge">{children}</div>;
 }
