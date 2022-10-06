4d0abf48edabed3f8027a98b9b7c25f8c01e94f1
daniele caldarini <daniele.caldarini@smc.it>
2022-10-06 15:06:37 +0200

    Revert "new logs in TikaProcessor to debug multiple binaries case"
    This reverts commit be6f009578dfb528601eb506c3c4fb790a7f42e0.

--- quarkus/tika/tika-common/src/main/java/io/openk9/tika/TikaProcessor.java
+++ quarkus/tika/tika-common/src/main/java/io/openk9/tika/TikaProcessor.java
@@ -49,8 +49,6 @@ public class TikaProcessor {
 
         JsonObject payload = jsonObject.getJsonObject("payload");
 
-        logger.info(jsonObject.toString());
-
         String replyTo = jsonObject.getString("replyTo");
 
         JsonArray binaries =
