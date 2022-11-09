54f96bc1679bc4407c13a281ec9653d90f53910a
Cristian Bianco <cristian.bianco@smc.it>
2022-11-09 16:45:34 +0100

    Issue #206: fix ... Normal scoped producer method may not return null..

--- quarkus/datasource/src/main/java/io/openk9/datasource/tenant/impl/DefaultTenantResolver.java
+++ quarkus/datasource/src/main/java/io/openk9/datasource/tenant/impl/DefaultTenantResolver.java
@@ -2,14 +2,8 @@ package io.openk9.datasource.tenant.impl;
 
 import io.openk9.datasource.tenant.TenantResolver;
 import io.vertx.core.Vertx;
-import io.vertx.ext.web.RoutingContext;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.context.RequestScoped;
-import javax.enterprise.context.control.RequestContextController;
-import javax.enterprise.inject.spi.BeanManager;
-import javax.enterprise.inject.spi.CDI;
-import javax.inject.Inject;
 
 @ApplicationScoped
 public class DefaultTenantResolver implements TenantResolver {
@@ -20,47 +14,15 @@ public class DefaultTenantResolver implements TenantResolver {
 
 	@Override
 	public String getTenantName() {
-
-		if (beanManager.isScope(RequestScoped.class)) {
-			return CDI
-				.current()
-				.select(RoutingContext.class)
-				.get()
-				.get(TENANT_ID);
-		}
-
 		return Vertx.currentContext().get(TENANT_ID);
 
 	}
 
 	@Override
 	public void setTenant(String name) {
-
-		if (beanManager.isScope(RequestScoped.class)) {
-			try {
-				requestContextController.activate();
-				CDI
-					.current()
-					.select(RoutingContext.class)
-					.get()
-					.put(TENANT_ID, name);
-			}
-			finally {
-				requestContextController.deactivate();
-			}
-		}
-		else {
-			Vertx.currentContext().put(TENANT_ID, name);
-		}
-
+		Vertx.currentContext().put(TENANT_ID, name);
 	}
 
 	public static final String TENANT_ID = "tenantId";
-
-	@Inject
-	BeanManager beanManager;
-
-	@Inject
-	RequestContextController requestContextController;
 
 }
