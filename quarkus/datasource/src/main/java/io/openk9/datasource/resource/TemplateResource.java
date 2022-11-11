34175197f74299b42eb859feffdbbb0b193ec963
daniele caldarini <daniele.caldarini@smc.it>
2022-11-11 15:29:40 +0100

    Issue #206: refactored current bucket endpoints; added getTemplateCompiled endpoint;

--- quarkus/datasource/src/main/java/io/openk9/datasource/resource/TemplateResource.java
+++ quarkus/datasource/src/main/java/io/openk9/datasource/resource/TemplateResource.java
@@ -4,8 +4,11 @@ import io.openk9.datasource.model.DocTypeTemplate;
 import io.openk9.datasource.model.dto.DocTypeTemplateDTO;
 import io.openk9.datasource.resource.util.BaseK9EntityResource;
 import io.openk9.datasource.service.DocTypeTemplateService;
+import io.smallrye.mutiny.Uni;
 
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 @Path("/templates")
 public class TemplateResource extends
@@ -13,6 +16,14 @@ public class TemplateResource extends
 
 	protected TemplateResource(DocTypeTemplateService service) {
 		super(service);
+	}
+
+	@GET
+	@Path("/{id}/compiled")
+	public Uni<String> getTemplateCompiled(@PathParam("id") long docTypeTemplateId) {
+
+		return service.findById(docTypeTemplateId).map(
+			DocTypeTemplate::getCompiled);
 	}
 
 }
--- quarkus/datasource/src/main/java/io/openk9/datasource/web/BucketResource.java
+++ quarkus/datasource/src/main/java/io/openk9/datasource/web/BucketResource.java
@@ -1,7 +1,14 @@
 package io.openk9.datasource.web;
 
-import io.openk9.datasource.model.Tab;
+
+import io.openk9.datasource.model.DataIndex_;
+import io.openk9.datasource.model.Datasource_;
+import io.openk9.datasource.model.DocType;
+import io.openk9.datasource.model.DocTypeTemplate;
+import io.openk9.datasource.model.DocType_;
 import io.openk9.datasource.model.Bucket;
+import io.openk9.datasource.model.SuggestionCategory;
+import io.openk9.datasource.model.Tab;
 import io.openk9.datasource.model.TenantBinding;
 import io.openk9.datasource.model.TenantBinding_;
 import io.openk9.datasource.model.Bucket_;
@@ -26,17 +33,82 @@ import java.util.ArrayList;
 import java.util.List;
 
 @ApplicationScoped
-@Path("/v1/tab")
-public class TabResource {
+@Path("/buckets")
+public class BucketResource {
 
 	@Context
 	HttpServerRequest request;
 
-	@Path("/get-by-virtualhost")
+	@Path("/current/templates")
+	@GET
+	public Uni<List<TemplateResponseDto>> getTemplates() {
+
+		return getDocTypeTemplateList(request.host());
+
+	}
+
+	@Path("/current/tabs")
 	@GET
 	public Uni<List<TabResponseDto>> getTabs() {
 
 		return getTabList(request.host());
+
+	}
+
+	@Path("/current/suggestionCategories")
+	@GET
+	public Uni<List<SuggestionCategory>> getSuggestionCategories() {
+
+		return getSuggestionCategoryList(request.host());
+
+	}
+
+	private Uni<List<TemplateResponseDto>> getDocTypeTemplateList(String virtualhost) {
+		return transactionInvoker.withTransaction(session -> {
+
+			CriteriaBuilder cb = transactionInvoker.getCriteriaBuilder();
+
+			CriteriaQuery<DocTypeTemplate> query = cb.createQuery(DocTypeTemplate.class);
+
+			Root<Bucket> from = query.from(Bucket.class);
+
+			Join<Bucket, TenantBinding> tenantBindingJoin =
+				from.join(Bucket_.tenantBinding);
+
+			Join<DocType, DocTypeTemplate> fetch =
+				from.join(Bucket_.datasources)
+					.join(Datasource_.dataIndex)
+					.join(DataIndex_.docTypes)
+					.join(DocType_.docTypeTemplate);
+
+			query.select(fetch);
+
+			query.where(
+				cb.equal(
+					tenantBindingJoin.get(TenantBinding_.virtualHost),
+					virtualhost
+				)
+			);
+
+
+			return session.createQuery(query).getResultList().map(docTypeTemplates -> {
+
+				List<TemplateResponseDto> responseDtos = new ArrayList<>();
+
+				for (DocTypeTemplate docTypeTemplate : docTypeTemplates) {
+
+					TemplateResponseDto templateResponseDto = new TemplateResponseDto();
+					templateResponseDto.setName(docTypeTemplate.getName());
+					templateResponseDto.setId(docTypeTemplate.getId());
+
+					responseDtos.add(templateResponseDto);
+				}
+
+				return responseDtos;
+
+			});
+
+		});
 
 	}
 
@@ -90,6 +162,34 @@ public class TabResource {
 
 	}
 
+	private Uni<List<SuggestionCategory>> getSuggestionCategoryList(String virtualhost) {
+		return transactionInvoker.withTransaction(session -> {
+
+			CriteriaBuilder cb = transactionInvoker.getCriteriaBuilder();
+
+			CriteriaQuery<SuggestionCategory> query = cb.createQuery(SuggestionCategory.class);
+
+			Root<Bucket> from = query.from(Bucket.class);
+
+			Join<Bucket, TenantBinding> tenantBindingJoin =
+				from.join(Bucket_.tenantBinding);
+
+			Join<Bucket, SuggestionCategory> fetch = from.join(Bucket_.suggestionCategories);
+
+			query.select(fetch);
+
+			query.where(
+				cb.equal(
+					tenantBindingJoin.get(TenantBinding_.virtualHost),
+					virtualhost
+				)
+			);
+
+			return session.createQuery(query).getResultList();
+		});
+
+	}
+
 	@Data
 	@AllArgsConstructor
 	@NoArgsConstructor
@@ -108,9 +208,15 @@ public class TabResource {
 		private List<String> values;
 	}
 
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	public static class TemplateResponseDto {
+		private String name;
+		private Long id;
+	}
 
 	@Inject
 	TransactionInvoker transactionInvoker;
 
 }
-
