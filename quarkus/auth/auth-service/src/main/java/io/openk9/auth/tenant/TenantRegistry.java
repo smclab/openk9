4f64181f7df187e1ac76cea3fb4ad11949112713
Cristian Bianco <cristian.bianco@smc.it>
2022-11-14 12:45:03 +0100

    Issue #206: fix tenantManager injection

--- quarkus/auth/auth-service/src/main/java/io/openk9/auth/tenant/TenantRegistry.java
+++ quarkus/auth/auth-service/src/main/java/io/openk9/auth/tenant/TenantRegistry.java
@@ -4,17 +4,16 @@ import io.openk9.api.tenantmanager.TenantManager;
 import io.smallrye.mutiny.Uni;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.enterprise.inject.Instance;
 import javax.inject.Inject;
 
 @ApplicationScoped
 public class TenantRegistry {
 
 	public Uni<TenantManager.Tenant> getTenantByVirtualHost(String virtualHost) {
-		return tenantManager.get().getTenantByVirtualHost(virtualHost);
+		return tenantManager.getTenantByVirtualHost(virtualHost);
 	}
 
 	@Inject
-	Instance<TenantManager> tenantManager;
+	TenantManager tenantManager;
 
 }
