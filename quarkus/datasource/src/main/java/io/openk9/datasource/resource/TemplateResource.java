eb452743fd73310c7415188c881b321851db75bf
daniele caldarini <daniele.caldarini@smc.it>
2022-11-14 12:28:39 +0100

    Issue #206: fix Produces getTemplateCompiled in TemplateResource

--- quarkus/datasource/src/main/java/io/openk9/datasource/resource/TemplateResource.java
+++ quarkus/datasource/src/main/java/io/openk9/datasource/resource/TemplateResource.java
@@ -9,6 +9,7 @@ import io.smallrye.mutiny.Uni;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 
 @Path("/templates")
 public class TemplateResource extends
@@ -19,6 +20,7 @@ public class TemplateResource extends
 	}
 
 	@GET
+	@Produces("text/javascript")
 	@Path("/{id}/compiled")
 	public Uni<String> getTemplateCompiled(@PathParam("id") long docTypeTemplateId) {
 
