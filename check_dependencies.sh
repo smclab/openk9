ec8bde1f2cdb2ed2b31af216ff67fe613f64219e
Cristian Bianco <cristian.bianco@smc.it>
2022-11-25 10:27:48 +0100

    fix ci

--- check_dependencies.sh
+++ check_dependencies.sh
@@ -4,13 +4,14 @@ git_changes="$(git diff-tree --no-commit-id --name-only -r "$(git log --format="
 
 echo "Checking for changes in the following files:"
 echo "$git_changes"
+echo ""
 
 check_changes() {
   for dir in $@
   do
       for change in $git_changes
       do
-        changeDirName=$(dirname $change)
+        changeDirName=$(dirname "$change")
         if [[ $dir == *$changeDirName* ]]
         then
           echo "Changes found in $dir"
@@ -23,10 +24,13 @@ check_changes() {
 
 for project_name in $OPENK9_PROJECT_NAMES
 do
-  project_dirs="$(cd core ; ./mvnw -q --also-make exec:exec -Dexec.executable="pwd" -pl $project_name)"
-  if check_changes $project_dirs ; then
+  project_dirs="$(cd core || exit ; ./mvnw -q --also-make exec:exec -Dexec.executable="pwd" -pl "$project_name")"
+  if check_changes "$project_dirs" ; then
     echo "Project $project_name has changes"
-    (cd core ; ./mvnw package --batch-mode -pl $project_name -am -Dquarkus.container-image.build=true -Dquarkus.container-image.push=true -Dquarkus.container-image.tag=$OPENK9_CONTAINER_IMAGE_TAG)
+    echo ""
+    echo "Starting build"
+    echo ""
+    (cd core || exit ; ./mvnw package --batch-mode -pl "$project_name" -am -Dquarkus.container-image.build=true -Dquarkus.container-image.push=true -Dquarkus.container-image.tag="$OPENK9_CONTAINER_IMAGE_TAG")
   else
     echo "Project $project_name has no changes"
   fi
