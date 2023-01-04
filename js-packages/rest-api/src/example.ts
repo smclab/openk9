209cee09a3096df37cd65e9a8395bfcfa11e3ba3
daniele caldarini <daniele.caldarini@smc.it>
2023-01-04 10:13:47 +0100

    Issue #305: removed rest-api and fixed Dockerfile

--- js-packages/rest-api/src/example.ts
+++
@@ -1,8 +0,0 @@
-import { OpenK9Client } from "./index";
-
-const client = OpenK9Client({ tenant: "https://demo.openk9.io" });
-
-client.doSearch({
-  searchQuery: [{ tokenType: "TEXT", values: ["palazzo"], filter: false }],
-  range: [0, 2],
-});
