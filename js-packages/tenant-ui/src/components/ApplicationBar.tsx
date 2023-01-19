8e1e06079a28473f861fbc5dfc914a61dc61543e
lorenzo venneri <lorenzo.venneri@smc.it>
2023-01-19 10:01:43 +0100

    Issue #353: add dropdown user info
    (cherry picked from commit ba5102c0b591f3b4e4d1c2fd271540f00ebc7fe2)

--- js-packages/tenant-ui/src/components/ApplicationBar.tsx
+++ js-packages/tenant-ui/src/components/ApplicationBar.tsx
@@ -1,10 +1,16 @@
 import ClayIcon from "@clayui/icon";
 import { Route, Routes } from "react-router-dom";
-import ClayButton, { ClayButtonWithIcon } from "@clayui/button";
-import { keycloak, useAuthentication } from "./authentication";
+import ClayButton from "@clayui/button";
+import { getUserProfile, keycloak, useAuthentication } from "./authentication";
+import ClayDropDown from "@clayui/drop-down";
+import React from "react";
 
 export function ApplicationBar({ isSideMenuOpen, onSideMenuToggle }: { isSideMenuOpen: boolean; onSideMenuToggle(isOpen: boolean): void }) {
   const { isAuthenticated } = useAuthentication();
+  const [name, setName] = React.useState("");
+  getUserProfile().then((data) => {
+    setName(JSON.parse(JSON.stringify(data))?.preferred_username);
+  });
   return (
     <div
       className="control-menu-container"
@@ -45,14 +51,45 @@ export function ApplicationBar({ isSideMenuOpen, onSideMenuToggle }: { isSideMen
                 </ClayButton>
               )}
               {isAuthenticated && (
-                <ClayButtonWithIcon
-                  aria-label=""
-                  symbol="logout"
-                  displayType="danger"
-                  onClick={() => {
-                    keycloak.logout();
-                  }}
-                />
+                <ClayDropDown
+                  trigger={
+                    <button className="btn btn-unstyled nav-btn " style={{ border: "1px solid #8F8F8F", width: "50px", height: "35px" }}>
+                      <ClayIcon symbol={"user"} style={{ color: "white" }} fontSize="25px" />
+                    </button>
+                  }
+                >
+                  <ClayDropDown.ItemList>
+                    <style type="text/css">
+                      {`
+                          .dropdown-item:focus, .dropdown-item.focus
+                          {
+                           background-color: white;
+                           box-shadow:none;
+                          }
+                      `}
+                    </style>
+                    <ClayDropDown.Item>
+                      <React.Fragment>
+                        <div style={{ display: "inline-block", width: "100%" }}>
+                          <ClayIcon symbol={"user"} style={{ color: "black" }} />
+                          <span style={{ marginLeft: "10px" }}> {name}</span>
+                        </div>
+                      </React.Fragment>
+                    </ClayDropDown.Item>
+                    <ClayDropDown.Divider />
+                    <ClayDropDown.Item>
+                      <div
+                        style={{ display: "inline-block", width: "100%" }}
+                        onClick={() => {
+                          keycloak.logout();
+                        }}
+                      >
+                        <ClayIcon symbol={"sign-in"} style={{ color: "black" }} />
+                        <span style={{ marginLeft: "10px" }}> Logout</span>
+                      </div>
+                    </ClayDropDown.Item>
+                  </ClayDropDown.ItemList>
+                </ClayDropDown>
               )}
             </li>
           </ul>
