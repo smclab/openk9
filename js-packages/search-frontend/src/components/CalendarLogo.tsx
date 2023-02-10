80868a9e70422534f708974b31c5c4cc600f264e
lorenzo venneri <lorenzo.venneri@smc.it>
2023-02-10 11:16:29 +0100

    Issue #378: add remove filter calendar

--- js-packages/search-frontend/src/components/CalendarLogo.tsx
+++ js-packages/search-frontend/src/components/CalendarLogo.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function CalendarLogo({ size = 26 }) {
   return (
-    <svg height="24" viewBox="0 0 24 24" width="24">
+    <svg width="35" height="35" viewBox="0 0 24 24">
       <path d="M0 0h24v24H0z" fill="white" />
       <path
         d="M20 3h-1V1h-2v2H7V1H5v2H4c-1.1 0-2 .9-2 2v16c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2zm0 18H4V8h16v13z"
---
+++ js-packages/search-frontend/src/components/SeparatorLogo.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function SeparatorLogo({ size = 26 }) {
+  return (
+    <svg width="2" height="28" viewBox="0 0 1 16" fill="none">
+      <line x1="0.5" y1="2.18557e-08" x2="0.499999" y2="16" stroke="#949494" />
+    </svg>
+  );
+}
