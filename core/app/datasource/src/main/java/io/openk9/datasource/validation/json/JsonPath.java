401e917b442d3fa3dbcf102316a2d4df4f96044b
Cristian Bianco <cristian.bianco@smc.it>
2023-03-01 16:03:41 +0100

    Fix: validator annotation

--- core/app/datasource/src/main/java/io/openk9/datasource/validation/json/JsonPath.java
+++ core/app/datasource/src/main/java/io/openk9/datasource/validation/json/JsonPath.java
@@ -1,6 +1,8 @@
 package io.openk9.datasource.validation.json;
 
 import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -10,4 +12,16 @@ import java.lang.annotation.Target;
 @Target({ElementType.FIELD, ElementType.METHOD})
 @Retention(RetentionPolicy.RUNTIME)
 public @interface JsonPath {
+	String message() default "not a valid json path";
+
+	Class<?>[] groups() default {};
+
+	Class<? extends Payload>[] payload() default {};
+
+	@Target({ ElementType.TYPE })
+	@Retention(RetentionPolicy.RUNTIME)
+	@Documented
+	@interface List {
+		Json[] value();
+	}
 }
