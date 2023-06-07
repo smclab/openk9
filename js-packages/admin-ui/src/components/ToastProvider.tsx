bfdaa497c55f73f0b2cc8b46655bfd2a463a0986
lorenzo venneri <lorenzo.venneri@smc.it>
2023-06-07 10:36:37 +0200

    Issue #466: change toast provider

--- js-packages/admin-ui/src/components/ToastProvider.tsx
+++ js-packages/admin-ui/src/components/ToastProvider.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ClayAlert, { DisplayType } from "@clayui/alert";
+import "@clayui/css/lib/css/atlas.css";
 
 type ToastDefinition = { displayType: DisplayType; title: string; content: React.ReactNode };
 
@@ -16,25 +17,36 @@ export function ToastProvider({ children }: ToastProviderProps) {
   const addToast = React.useCallback(({ displayType, title, content }: ToastDefinition) => {
     const key = new Date().toJSON();
     setToastItems((toastItems) => [...toastItems, { key, displayType, title, content }]);
+    setTimeout(function () {
+      setToastItems((toastItems) => toastItems.filter((item) => item.key !== key));
+    }, 5000);
   }, []);
+
   return (
     <ToastContext.Provider value={addToast}>
       {children}
-      <ClayAlert.ToastContainer>
-        {toastItems.map(({ key, displayType, title, content }) => (
-          <ClayAlert
-            key={key}
-            displayType={displayType}
-            autoClose={5000}
-            onClose={() => {
-              setToastItems((toastItems) => toastItems.filter((item) => item.key !== key));
-            }}
-            title={title}
-          >
-            {content}
-          </ClayAlert>
-        ))}
-      </ClayAlert.ToastContainer>
+      <div className="toast-container" style={{ position: "fixed", bottom: "20px", right: "20px", zIndex: "1" }}>
+        {toastItems.map(({ key, displayType, title, content }) => {
+          const classNames = `alert alert-${displayType}`;
+          return (
+            <div className={classNames} role={displayType} style={{ minWidth: "250px" }} key={key}>
+              <strong className="lead">{title}</strong>
+              {content}
+              <button
+                aria-label="Close"
+                className="close"
+                data-dismiss="alert"
+                type="button"
+                onClick={() => {
+                  setToastItems((toastItems) => toastItems.filter((item) => item.key !== key));
+                }}
+              >
+                X
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </ToastContext.Provider>
   );
 }
