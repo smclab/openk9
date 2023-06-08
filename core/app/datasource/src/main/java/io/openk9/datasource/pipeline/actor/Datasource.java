f532040d97d011f1028af8d8acec09d2038771d6
Cristian Bianco <cristian.bianco@smc.it>
2023-06-08 14:50:07 +0200

    Issue #461 Fix: NPE

--- core/app/datasource/src/main/java/io/openk9/datasource/pipeline/actor/Datasource.java
+++ core/app/datasource/src/main/java/io/openk9/datasource/pipeline/actor/Datasource.java
@@ -25,12 +25,12 @@ public class Datasource {
 	public record GetDatasource(
 		String tenantId, long datasourceId, long parsingDate, ActorRef<Response> replyTo)
 		implements Command {}
-	private enum BusyFinished implements Command {INSTANCE}
+	private record Finished(io.openk9.datasource.model.Datasource datasource, Throwable throwable) implements Command {}
 	private record DatasourceModel(
-		io.openk9.datasource.model.Datasource datasource, String tenantId, long parsingDate,
-		ActorRef<Response> replyTo) implements Command {}
+		io.openk9.datasource.model.Datasource datasource
+	) implements Command {}
 	private record DatasourceModelError(
-		Throwable exception, ActorRef<Response> replyTo) implements Command {}
+		Throwable exception) implements Command {}
 	private record CreateRandomDataIndex(
 		String tenantId, long parsingDate, io.openk9.datasource.model.Datasource datasource,
 		ActorRef<Response> replyTo) implements Command {}
@@ -54,30 +54,29 @@ public class Datasource {
 	private static Behavior<Command> idle(
 		ActorContext<Command> ctx, TransactionInvoker txInvoker) {
 
+		ctx.getLog().info("Start idle state");
+
 		return Behaviors.receive(Command.class)
 			.onMessage(GetDatasource.class, message -> onGetDatasource(message, ctx, txInvoker))
 			.build();
 	}
 
-	private static Behavior<Command> onDatasourceModelError(ActorContext<Command> ctx, DatasourceModelError message) {
+	private static Behavior<Command> onDatasourceModelError(
+		ActorContext<Command> ctx, DatasourceModelError message) {
 		Throwable exception = message.exception;
-		message.replyTo.tell(new Failure(exception));
-		ctx.getSelf().tell(BusyFinished.INSTANCE);
+		ctx.getSelf().tell(new Finished(null, exception));
 		return Behaviors.same();
 	}
 
 	private static Behavior<Command> onDatasourceModel(
-		DatasourceModel message, ActorContext<Command> ctx, TransactionInvoker txInvoker) {
+		DatasourceModel message, ActorContext<Command> ctx, TransactionInvoker txInvoker,
+		String tenantId, long parsingDate, ActorRef<Response> replyTo) {
 
 		io.openk9.datasource.model.Datasource datasource = message.datasource;
 
 		DataIndex dataIndex = datasource.getDataIndex();
 
 		Logger log = ctx.getLog();
-
-		long parsingDate = message.parsingDate;
-		ActorRef<Response> replyTo = message.replyTo;
-		String tenantId = message.tenantId;
 
 		if (dataIndex == null) {
 			log.info("datasource with id {} has no dataIndex, start random creation", datasource.getId());
@@ -94,22 +93,20 @@ public class Datasource {
 		if (lastIngestionDate == null || !lastIngestionDate.isEqual(offsetDateTime)) {
 			log.info("update last ingestion date for datasource with id {}", datasource.getId());
 			datasource.setLastIngestionDate(offsetDateTime);
-			mergeDatasource(
-				ctx.getSelf(), tenantId, txInvoker, datasource, parsingDate,
-				replyTo);
+			mergeDatasource(ctx.getSelf(), tenantId, txInvoker, datasource);
 			return Behaviors.same();
 		}
 
 		log.info("start pipeline for datasource with id {}", datasource.getId());
 
-		replyTo.tell(new Success(datasource));
-		ctx.getSelf().tell(BusyFinished.INSTANCE);
+		ctx.getSelf().tell(new Finished(datasource, null));
 
 		return Behaviors.same();
 	}
 
 	private static Behavior<Command> onGetDatasource(
 		GetDatasource message, ActorContext<Command> ctx, TransactionInvoker txInvoker) {
+
 
 		VertxUtil.runOnContext(() ->
 			txInvoker.withStatelessTransaction(message.tenantId, s ->
@@ -125,29 +122,31 @@ public class Datasource {
 				.onItemOrFailure()
 				.invoke((d, t) -> {
 					if (t != null) {
-						ctx.getSelf().tell(new DatasourceModelError(t, message.replyTo));
+						ctx.getSelf().tell(new DatasourceModelError(t));
 					}
 					if (d != null) {
-						ctx.getSelf().tell(
-							new DatasourceModel(
-								d, message.tenantId, message.parsingDate, message.replyTo));
+						ctx.getSelf().tell(new DatasourceModel(d));
 					}
 				})
 			)
 		);
 
-		return busy(ctx, txInvoker, new ArrayList<>());
+		return busy(
+			ctx, txInvoker, new ArrayList<>(),
+			message.tenantId, message.parsingDate,
+			message.replyTo);
 	}
 
 	private static Behavior<Command> busy(
-		ActorContext<Command> ctx, TransactionInvoker txInvoker, List<Command> lags) {
+		ActorContext<Command> ctx, TransactionInvoker txInvoker, List<Command> lags,
+		String tenantId, long parsingDate, ActorRef<Response> replyTo) {
 
 		return Behaviors.receive(Command.class)
-			.onMessage(GetDatasource.class, message -> onBusyGetMessage(ctx, txInvoker, lags, message))
-			.onMessage(DatasourceModel.class, message -> onDatasourceModel(message, ctx, txInvoker))
+			.onMessage(GetDatasource.class, message -> onBusyGetMessage(ctx, txInvoker, lags, message, tenantId, parsingDate, replyTo))
+			.onMessage(DatasourceModel.class, message -> onDatasourceModel(message, ctx, txInvoker, tenantId, parsingDate, replyTo))
 			.onMessage(DatasourceModelError.class, message -> onDatasourceModelError(ctx, message))
 			.onMessage(CreateRandomDataIndex.class, message -> onCreateRandomDataIndex(ctx, txInvoker, message))
-			.onMessageEquals(BusyFinished.INSTANCE, () -> onBusyFinished(ctx, txInvoker, lags))
+			.onMessage(Finished.class, (message) -> onFinished(ctx, txInvoker, lags, replyTo, message))
 			.build();
 	}
 
@@ -173,14 +172,23 @@ public class Datasource {
 			"creating random dataIndex: {} for datasource: {}", indexName, datasource.getId());
 
 		mergeDatasource(
-			ctx.getSelf(), tenantId, transactionInvoker, datasource,
-			createRandomDataIndex.parsingDate, createRandomDataIndex.replyTo);
+			ctx.getSelf(), tenantId, transactionInvoker, datasource);
 
 		return Behaviors.same();
 	}
 
-	private static Behavior<Command> onBusyFinished(
-		ActorContext<Command> ctx, TransactionInvoker txInvoker, List<Command> lags) {
+	private static Behavior<Command> onFinished(
+		ActorContext<Command> ctx, TransactionInvoker txInvoker, List<Command> lags,
+		ActorRef<Response> replyTo, Finished finished) {
+
+		Throwable throwable = finished.throwable;
+
+		if (throwable != null) {
+			replyTo.tell(new Failure(throwable));
+		}
+		else {
+			replyTo.tell(new Success(finished.datasource));
+		}
 
 		for (Command message : lags) {
 			ctx.getSelf().tell(message);
@@ -191,22 +199,23 @@ public class Datasource {
 
 	private static Behavior<Command> onBusyGetMessage(
 		ActorContext<Command> ctx, TransactionInvoker txInvoker,
-		List<Command> lags, GetDatasource message) {
+		List<Command> lags, Command message, String tenantId,
+		long parsingDate, ActorRef<Response> replyTo) {
 		List<Command> newLags = new ArrayList<>(lags);
 		newLags.add(message);
 		ctx.getLog().info("Datasource actor is busy... lags count: " + newLags.size());
-		return busy(ctx, txInvoker, newLags);
+		return busy(
+			ctx, txInvoker, newLags, tenantId, parsingDate, replyTo);
 	}
 
 	private static void mergeDatasource(
 		ActorRef<Command> self, String tenantId,
-		TransactionInvoker transactionInvoker, io.openk9.datasource.model.Datasource datasource,
-		long parsingDate, ActorRef<Response> replyTo) {
+		TransactionInvoker transactionInvoker, io.openk9.datasource.model.Datasource datasource) {
 
 		VertxUtil.runOnContext(() ->
 			transactionInvoker
 				.withTransaction(tenantId, s -> s.merge(datasource))
-				.invoke(d -> self.tell(new DatasourceModel(d, tenantId, parsingDate, replyTo)))
+				.invoke(d -> self.tell(new DatasourceModel(d)))
 		);
 	}
 
