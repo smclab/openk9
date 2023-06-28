11ec02d90504cc46cd607633ca84c97ae1d73669
Mirko Zizzari <mirko.zizzari@smc.it>
2023-06-28 09:57:11 +0200

    Issue #495: override toString

--- core/app/datasource/src/main/java/io/openk9/datasource/model/ScheduleId.java
+++ core/app/datasource/src/main/java/io/openk9/datasource/model/ScheduleId.java
@@ -1,10 +1,12 @@
 package io.openk9.datasource.model;
 
 import lombok.Getter;
+import lombok.ToString;
 
 import java.util.UUID;
 
 @Getter
+@ToString
 public class ScheduleId {
 
 	private final String value;
