a49c6c05640955844282e00b829b9521051b4a7e
Mirko Zizzari <mirko.zizzari@smc.it>
2023-08-28 15:28:49 +0200

    Issue #506: ignore wrappee object

--- core/app/datasource/src/main/java/io/openk9/datasource/model/LocalizedSuggestionCategory.java
+++ core/app/datasource/src/main/java/io/openk9/datasource/model/LocalizedSuggestionCategory.java
@@ -1,17 +1,21 @@
 package io.openk9.datasource.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.openk9.datasource.model.util.LocalizedEntity;
 import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.experimental.Delegate;
 
 import java.util.Map;
 
 @Data
+@EqualsAndHashCode(callSuper = true)
 public class LocalizedSuggestionCategory
 	extends SuggestionCategory
 	implements LocalizedEntity<SuggestionCategory> {
 
+	@JsonIgnore
 	@Delegate(excludes = PanacheEntityBase.class)
 	private final SuggestionCategory wrappee;
 	private final Map<String, String> translationMap;
