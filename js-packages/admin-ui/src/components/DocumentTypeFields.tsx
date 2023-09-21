e1b42442ef94bbc9c514502064fb16ecdd5a2065
lucac <luca.callocchia@gmail.com>
2023-09-21 16:13:07 +0200

    add remove doctypes level

--- js-packages/admin-ui/src/components/DocumentTypeFields.tsx
+++ js-packages/admin-ui/src/components/DocumentTypeFields.tsx
@@ -16,11 +16,11 @@ import { DocumentTypeFieldsQuery } from "./SubFieldsDocumentType";
 import { ClayToggle } from "@clayui/form";
 import { ContainerFluid, StyleToggle } from "./Form";
 import { ClayTooltipProvider } from "@clayui/tooltip";
+import { CategoricalChartFunc } from "recharts/types/chart/generateCategoricalChart";
 import ClayIcon from "@clayui/icon";
 import ClayList from "@clayui/list";
 import DropDown from "@clayui/drop-down";
 import { apolloClient } from "./apolloClient";
-import { log } from "console";
 
 gql`
   mutation DeleteDocumentTypeField($documentTypeId: ID!, $documentTypeFieldId: ID!) {
@@ -61,12 +61,15 @@ export function DocumentTypeFields() {
         variables: { searchText: "", parentId: 0 },
         skip: !documentTypeId,
       }),
-      level: 0,
     },
   ]);
-  allDocTypes.forEach((all, index) => {
-    console.log(all, index);
-  });
+
+  React.useEffect(() => {
+    console.log(allDocTypes);
+  }, [allDocTypes]);
+  // allDocTypes.forEach((all, index) => {
+  //   console.log(all, index);
+  // });
   const documentTypeFieldsParentsQuery = useDocTypeFieldsByParentQuery({
     variables: { searchText: "", parentId: 0 },
     skip: !documentTypeId,
@@ -78,147 +81,233 @@ export function DocumentTypeFields() {
     refetchQueries: [{ query: DocumentTypeFieldsQuery, variables: { documentTypeId: documentTypeId! } }],
   });
 
-  async function addElement({ parentId, level }: { parentId: number; level: number }) {
+  // async function addElement({ parentId, level }: { parentId: number; level: number }) {
+  //   try {
+  //     const { data } = await apolloClient.query({
+  //       query: DocumentTypeFieldsParentQuery,
+  //       variables: { searchText: "", parentId: parentId },
+  //     });
+  //     setAllDocTypes((prevAllDocTypes) => [
+  //       ...prevAllDocTypes,
+  //       {
+  //         idParent: parentId,
+  //         queryData: data,
+  //         level: level,
+  //       },
+  //     ]);
+  //   } catch (error) {
+  //     console.error("Errore nella query:", error);
+  //   }
+  // }
+
+  // const allLevels: (
+  //   | {
+  //       __typename?: "DocTypeField" | undefined;
+  //       id?: string | null | undefined;
+  //       name?: string | null | undefined;
+  //       fieldName?: string | null | undefined;
+  //       searchable: boolean;
+  //       exclude?: boolean | null | undefined;
+  //       sortable: boolean;
+  //       parent?:
+  //         | { __typename?: "DocTypeField" | undefined; id?: string | null | undefined; fieldName?: string | null | undefined }
+  //         | null
+  //         | undefined;
+  //     }
+  //   | null
+  //   | undefined
+  // )[][] = [];
+  // const firstLevel: (
+  //   | {
+  //       __typename?: "DocTypeField" | undefined;
+  //       id?: string | null | undefined;
+  //       name?: string | null | undefined;
+  //       fieldName?: string | null | undefined;
+  //       searchable: boolean;
+  //       exclude?: boolean | null | undefined;
+  //       sortable: boolean;
+  //       parent?:
+  //         | { __typename?: "DocTypeField" | undefined; id?: string | null | undefined; fieldName?: string | null | undefined }
+  //         | null
+  //         | undefined;
+  //     }
+  //   | null
+  //   | undefined
+  // )[] = [];
+  // documentTypeFieldsParentsQuery.data?.docTypeFieldsByParent?.edges?.forEach((element) => {
+  //   // console.log(element?.node)
+  //   firstLevel.push(element?.node);
+  // });
+  // allLevels.push(firstLevel);
+
+  async function addRemoveLevel({ parentId }: { parentId: number }) {
     try {
       const { data } = await apolloClient.query({
         query: DocumentTypeFieldsParentQuery,
         variables: { searchText: "", parentId: parentId },
       });
-      setAllDocTypes((prevAllDocTypes) => [
-        ...prevAllDocTypes,
-        {
-          idParent: parentId,
-          queryData: data,
-          level: level,
-        },
-      ]);
+
+      const queryResult = data?.docTypeFieldsByParent?.edges;
+      const allDocTypesLength = allDocTypes.length;
+      for (let index = allDocTypesLength - 1; index >= 0; index--) {
+        const parent = allDocTypes[index];
+        const queryResultLength = queryResult.length;
+        if (queryResultLength === 0) {
+          allDocTypes.splice(index + 1, 1);
+        }
+        for (let i = 0; i < queryResultLength; i++) {
+          const son = queryResult[i].node;
+          if (
+            parent.queryData.data?.docTypeFieldsByParent?.edges?.find((document) => document?.node?.id === son.parent.id) ||
+            !parent.queryData.data
+          ) {
+            allDocTypes.splice(index + 1, 1);
+          }
+        }
+      }
+      const elementToAdd = {
+        idParent: parentId,
+        queryData: data,
+      };
+      if (elementToAdd.queryData?.docTypeFieldsByParent?.edges.length > 0) {
+        // allDocTypes.push(elementToAdd);
+        // setAllDocTypes(allDocTypes);
+        setAllDocTypes((prevArray) => [...prevArray, elementToAdd]);
+      }
+
+      console.log("allDocTypes", allDocTypes);
     } catch (error) {
       console.error("Errore nella query:", error);
     }
   }
   if (!documentTypeId) throw new Error();
 
-  function CreateDocType(
+  function CreateDocType({
+    documentType,
+  }: {
     documentType: {
-      __typename?: "DefaultEdge_DocTypeField" | undefined;
-      node?:
-        | {
-            __typename?: "DocTypeField" | undefined;
-            id?: string | null | undefined;
-            name?: string | null | undefined;
-            fieldName?: string | null | undefined;
-            searchable: boolean;
-            exclude?: boolean | null | undefined;
-            sortable: boolean;
-            parent?:
-              | { __typename?: "DocTypeField" | undefined; id?: string | null | undefined; fieldName?: string | null | undefined }
-              | null
-              | undefined;
-          }
-        | null
-        | undefined;
-    } | null
-  ): JSX.Element {
+      idParent: number;
+      queryData: QueryResult<
+        DocTypeFieldsByParentQuery,
+        Exact<{
+          searchText?: InputMaybe<string> | undefined;
+          parentId: any;
+        }>
+      >;
+    } | null;
+  }): JSX.Element {
+    const sameData = documentType?.queryData.data?.docTypeFieldsByParent?.edges;
     return (
-      <React.Fragment>
-        <ClayList.Item flex className="align-items-center">
-          <ClayList.ItemField expand>
-            <ClayList.ItemTitle>{formatName({ id: documentType?.node?.id, name: documentType?.node?.name })}</ClayList.ItemTitle>
-          </ClayList.ItemField>
-          <ClayList.ItemField>
-            <ClayList.Item flex style={{ border: "none" }}>
-              <Link to={`newSubFields/${documentType?.node?.id}/new`}>
-                <ClayList.QuickActionMenu.Item aria-label="Plus doc type fields" title="Plus doc type fields" symbol="plus" />
-              </Link>
+      <div>{sameData?.map((sinlgeData) => <div   onClick={() => {
+                    addRemoveLevel({ parentId: Number(sinlgeData?.node?.id || 0) });
+                  }}>{sinlgeData?.node?.name}</div>)}</div>
+      // <React.Fragment>{sameData?.map((sinlgeData) => {
+      //   return(
+      //     <ClayList.Item flex className="align-items-center">
+      //     <ClayList.ItemField expand>
+      //       <ClayList.ItemTitle>{formatName({ id: sinlgeData?.node?.id, name: sinlgeData?.node?.name })}</ClayList.ItemTitle>
+      //     </ClayList.ItemField>
+      //     <ClayList.ItemField>
+      //       <ClayList.Item flex style={{ border: "none" }}>
+      //         <Link to={`newSubFields/${sinlgeData?.node?.id}/new`}>
+      //           <ClayList.QuickActionMenu.Item aria-label="Plus doc type fields" title="Plus doc type fields" symbol="plus" />
+      //         </Link>
 
-              <DropDown
-                trigger={
-                  <button className="btn btn-unstyled nav-btn nav-btn-monospaced" style={{ width: "50px", height: "35px" }}>
-                    <ClayIcon symbol={"angle-down"} style={{ color: "black" }} />
-                  </button>
-                }
-              >
-                <DropDown.ItemList>
-                  <DropDown.Item>
-                    <div style={{ display: "flex", justifyContent: "space-between" }}>
-                      <div>
-                        <label>Searchable</label>
-                      </div>
-                      <div>
-                        <ClayToggle
-                          toggled={documentType?.node?.searchable ?? false}
-                          onToggle={(schedulable) => {
-                            const documentTypeElement = documentType?.node;
-                            if (documentTypeElement && documentTypeElement.id && documentTypeElement.name && documentTypeElement.parent) {
-                            }
-                          }}
-                        />
-                      </div>
-                    </div>
-                  </DropDown.Item>
-                  <DropDown.Divider />
-                  <DropDown.Item>
-                    <div style={{ display: "flex", justifyContent: "space-between" }}>
-                      <div>
-                        <label>Exclude</label>
-                      </div>
-                      <div>
-                        <ClayToggle
-                          toggled={documentType?.node?.exclude ?? false}
-                          onToggle={(schedulable) => {
-                            const documentTypeElement = documentType?.node;
-                            if (documentTypeElement && documentTypeElement.id && documentTypeElement.name && documentTypeElement.parent) {
-                            }
-                          }}
-                        />
-                      </div>
-                    </div>
-                  </DropDown.Item>
-                  <DropDown.Divider />
-                  <DropDown.Item>
-                    <div style={{ display: "flex", justifyContent: "space-between" }}>
-                      <div>
-                        <label>Sortable</label>
-                      </div>
-                      <div>
-                        <ClayToggle
-                          toggled={documentType?.node?.sortable ?? false}
-                          onToggle={(schedulable) => {
-                            const documentTypeElement = documentType?.node;
-                            if (documentTypeElement && documentTypeElement.id && documentTypeElement.name && documentTypeElement.parent) {
-                            }
-                          }}
-                        />
-                      </div>
-                    </div>
-                  </DropDown.Item>
-                </DropDown.ItemList>
-              </DropDown>
-              <ClayList.QuickActionMenu.Item
-                aria-label="View sub doc type fields"
-                title="View sub doc type fields"
-                onClick={() => {
-                  const level = findLevel({ docTypes: allDocTypes, addedId: Number(documentType?.node?.parent?.id || 0) });
-                  addElement({ parentId: Number(documentType?.node?.id || 0), level });
-                }}
-                symbol="angle-right"
-              />
-            </ClayList.Item>
-          </ClayList.ItemField>
-        </ClayList.Item>
-      </React.Fragment>
+      //         <DropDown
+      //           trigger={
+      //             <button className="btn btn-unstyled nav-btn nav-btn-monospaced" style={{ width: "50px", height: "35px" }}>
+      //               <ClayIcon symbol={"angle-down"} style={{ color: "black" }} />
+      //             </button>
+      //           }
+      //         >
+      //           <DropDown.ItemList>
+      //             <DropDown.Item>
+      //               <div style={{ display: "flex", justifyContent: "space-between" }}>
+      //                 <div>
+      //                   <label>Searchable</label>
+      //                 </div>
+      //                 <div>
+      //                   <ClayToggle
+      //                     toggled={sinlgeData?.node?.searchable ?? false}
+      //                     onToggle={(schedulable) => {
+      //                       const documentTypeElement = sinlgeData?.node;
+      //                       if (documentTypeElement && documentTypeElement.id && documentTypeElement.name && documentTypeElement.parent) {
+      //                       }
+      //                     }}
+      //                   />
+      //                 </div>
+      //               </div>
+      //             </DropDown.Item>
+      //             <DropDown.Divider />
+      //             <DropDown.Item>
+      //               <div style={{ display: "flex", justifyContent: "space-between" }}>
+      //                 <div>
+      //                   <label>Exclude</label>
+      //                 </div>
+      //                 <div>
+      //                   <ClayToggle
+      //                     toggled={sinlgeData?.node?.exclude ?? false}
+      //                     onToggle={(schedulable) => {
+      //                       const documentTypeElement = sinlgeData?.node;
+      //                       if (documentTypeElement && documentTypeElement.id && documentTypeElement.name && documentTypeElement.parent) {
+      //                       }
+      //                     }}
+      //                   />
+      //                 </div>
+      //               </div>
+      //             </DropDown.Item>
+      //             <DropDown.Divider />
+      //             <DropDown.Item>
+      //               <div style={{ display: "flex", justifyContent: "space-between" }}>
+      //                 <div>
+      //                   <label>Sortable</label>
+      //                 </div>
+      //                 <div>
+      //                   <ClayToggle
+      //                     toggled={sinlgeData?.node?.sortable ?? false}
+      //                     onToggle={(schedulable) => {
+      //                       const documentTypeElement = sinlgeData?.node;
+      //                       if (documentTypeElement && documentTypeElement.id && documentTypeElement.name && documentTypeElement.parent) {
+      //                       }
+      //                     }}
+      //                   />
+      //                 </div>
+      //               </div>
+      //             </DropDown.Item>
+      //           </DropDown.ItemList>
+      //         </DropDown>
+      //         <ClayList.QuickActionMenu.Item
+      //           aria-label="View sub doc type fields"
+      //           title="View sub doc type fields"
+      //           onClick={() => {
+      //             addRemoveLevel({ parentId: Number(sinlgeData?.node?.id || 0) });
+      //           }}
+      //           symbol="angle-right"
+      //         />
+      //       </ClayList.Item>
+      //     </ClayList.ItemField>
+      //   </ClayList.Item>
+      //   )
+      // })}
+      // </React.Fragment>
     );
   }
 
   return (
     <React.Fragment>
-      <ContainerFluid>
-        <ClayList>
-          {documentTypeFieldsParentsQuery.data?.docTypeFieldsByParent?.edges?.map((documentType, index) => {
-            return CreateDocType(documentType);
-          })}
-        </ClayList>
-      </ContainerFluid>
+      {allDocTypes.map((docType, index) => {
+        console.log(docType)
+        return (
+          <div>
+            <ContainerFluid>
+              {index}
+              <ClayList>
+                <CreateDocType documentType={docType} />
+              </ClayList>
+            </ContainerFluid>
+          </div>
+        );
+      })}
     </React.Fragment>
   );
 }
@@ -226,7 +315,6 @@ export function DocumentTypeFields() {
 type DocTypesType = {
   idParent: number;
   element: React.ReactNode;
-  level: number;
 };
 
 type DocTypesQueryType = {
@@ -238,16 +326,15 @@ type DocTypesQueryType = {
       parentId: any;
     }>
   >;
-  level: number;
 };
 
-function removeElementsByLevels(elements: DocTypesType[], levelSelect: number): DocTypesType[] {
-  return elements.filter((elementInfo) => elementInfo.level <= levelSelect);
-}
+// function removeElementsByLevels(elements: DocTypesType[], levelSelect: number): DocTypesType[] {
+//   return elements.filter((elementInfo) => elementInfo.level <= levelSelect);
+// }
 
-function findLevel({ docTypes, addedId }: { docTypes: Array<DocTypesQueryType>; addedId: number }): number {
-  const docElement = docTypes.find((document) => document.idParent === addedId);
-  let nextLevel = 0;
-  if (docElement?.level === undefined) nextLevel = docTypes.reduce((max, doc) => (doc.level > max ? doc.level : max), 0);
-  return docElement?.level || nextLevel;
-}
+// function findLevel({ docTypes, addedId }: { docTypes: Array<DocTypesQueryType>; addedId: number }): number {
+//   const docElement = docTypes.find((document) => document.idParent === addedId);
+//   let nextLevel = 0;
+//   if (docElement?.level === undefined) nextLevel = docTypes.reduce((max, doc) => (doc.level > max ? doc.level : max), 0);
+//   return docElement?.level || nextLevel;
+// }
