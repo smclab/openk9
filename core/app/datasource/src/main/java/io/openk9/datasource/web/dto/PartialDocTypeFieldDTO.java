ed419e0f35a02cf90482faa1bc329cc81218119d
Mirko Zizzari <mirko.zizzari@smc.it>
2023-09-21 17:38:17 +0200

    Issue #591: get docTypeField path for sortable fields

--- core/app/datasource/src/main/java/io/openk9/datasource/web/dto/PartialDocTypeFieldDTO.java
+++ core/app/datasource/src/main/java/io/openk9/datasource/web/dto/PartialDocTypeFieldDTO.java
@@ -6,7 +6,7 @@ public record PartialDocTypeFieldDTO(String field, Long id, String label) {
 
 	public static PartialDocTypeFieldDTO of(DocTypeField docTypeField) {
 		return new PartialDocTypeFieldDTO(
-			docTypeField.getFieldName(),
+			docTypeField.getPath(),
 			docTypeField.getId(),
 			docTypeField.getName()
 		);
