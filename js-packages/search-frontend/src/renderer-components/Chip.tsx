1a6e2af2e0e91f8a43b35d38a96fd8a1ee53118f
lorenzo venneri <lorenzo.venneri@smc.it>
2023-09-25 12:27:08 +0200

    Issu #597: fix chip component

--- js-packages/search-frontend/src/renderer-components/Chip.tsx
+++ js-packages/search-frontend/src/renderer-components/Chip.tsx
@@ -2,27 +2,37 @@ import React from "react";
 
 type ChipProps = {
   title: string;
+  customStyles?: CustomStyles;
 };
-export function Chip({ title }: ChipProps) {
+
+type CustomStyles = {
+  button?: React.CSSProperties;
+  div?: React.CSSProperties;
+};
+
+export function Chip({ title, customStyles }: ChipProps) {
+  const buttonStyles = {
+    padding: "4px 10px",
+    borderRadius: "20px",
+    border: "1px solid #C0272B",
+    background: "white",
+    fontSize: "10px",
+    fontWeight: "700",
+    lineHeight: "12px",
+    ...(customStyles?.button || {}),
+  };
+
+  const divStyles = {
+    display: "flex",
+    alignItems: "baseline",
+    color: "#c0272b",
+    gap: "8px",
+    ...(customStyles?.div || {}),
+  };
+
   return (
-    <button
-      style={{
-        padding: "8px 16px",
-        borderRadius: "20px",
-        border: "1px solid #F9EDEE",
-        background: "#F9EDEE",
-      }}
-      aria-label={`chip of ${title}`}
-    >
-      <div
-        style={{
-          display: "flex",
-          alignItems: "baseline",
-          color: "#c0272b",
-        }}
-      >
-        {title}
-      </div>
+    <button style={buttonStyles} aria-label={`chip of ${title}`}>
+      <div style={divStyles}>{title}</div>
     </button>
   );
 }
