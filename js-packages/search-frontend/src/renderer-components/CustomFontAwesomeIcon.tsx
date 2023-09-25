c5a3838ed7815c56aaca147df34373604b11cc70
daniele caldarini <daniele.caldarini@smc.it>
2023-09-25 21:42:41 +0200

    added new icons in CustomFontAwesomeIcon.tsx

--- js-packages/search-frontend/src/renderer-components/CustomFontAwesomeIcon.tsx
+++ js-packages/search-frontend/src/renderer-components/CustomFontAwesomeIcon.tsx
@@ -4,6 +4,9 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faForumbee } from "@fortawesome/free-brands-svg-icons";
 import { faCalendar } from "@fortawesome/free-solid-svg-icons";
 import { faFileAlt } from "@fortawesome/free-solid-svg-icons/faFileAlt";
+import { faUser } from "@fortawesome/free-solid-svg-icons";
+import { faSitemap } from "@fortawesome/free-solid-svg-icons";
+import { faWikipediaW } from "@fortawesome/free-brands-svg-icons";
 
 export function CustomFontAwesomeIcon<E>({
   result
@@ -17,6 +20,14 @@ export function CustomFontAwesomeIcon<E>({
   if (realResult.source.documentTypes.includes("calendar")) {
 	  return <FontAwesomeIcon icon={faCalendar} />;
 	}
-
+  if (realResult.source.documentTypes.includes("user")) {
+ 	  return <FontAwesomeIcon icon={faUser} />;
+ 	}
+   if (realResult.source.documentTypes.includes("site")) {
+  	  return <FontAwesomeIcon icon={faSitemap} />;
+  	}
+   if (realResult.source.documentTypes.includes("wiki")) {
+   	  return <FontAwesomeIcon icon={faWikipediaW} />;
+   	}
   return <FontAwesomeIcon icon={faFileAlt} />;
 }
