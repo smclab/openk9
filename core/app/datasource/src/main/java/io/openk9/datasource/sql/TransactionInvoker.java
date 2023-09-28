2ea2b5129cfc85a96129b6e2c17689fb14aa84c7
Mirko Zizzari <mirko.zizzari@smc.it>
2023-09-28 11:53:59 +0200

    Issue #562: txInvoker delegate Mutiny.SessionFactory

--- core/app/datasource/src/main/java/io/openk9/datasource/sql/TransactionInvoker.java
+++ core/app/datasource/src/main/java/io/openk9/datasource/sql/TransactionInvoker.java
@@ -29,141 +29,49 @@ public abstract class TransactionInvoker {
 		return em.getCriteriaBuilder();
 	}
 
-	public <T> Uni<T> withTransaction(Supplier<Uni<T>> fun) {
-		return withTransaction(null, (session, transaction) -> fun.get());
+	public <T> Uni<T> withTransaction(Function<Mutiny.Session, Uni<T>> fun) {
+		return em.withTransaction(fun);
 	}
 
-	public <T> Uni<T> withTransaction(
-		Function<Mutiny.Session, Uni<T>> fun) {
-		return withTransaction(null, (session, transaction) -> fun.apply(session));
+	public <T> Uni<T> withTransaction(BiFunction<Mutiny.Session, Mutiny.Transaction, Uni<T>> fun) {
+		return em.withTransaction(fun);
 	}
 
-	public <T> Uni<T> withTransaction(
-		BiFunction<Mutiny.Session, Mutiny.Transaction, Uni<T>> fun) {
-
-		return withTransaction(null, fun);
-
-	}
-
-	public <T> Uni<T> withTransaction(String schema, Supplier<Uni<T>> fun) {
-		return withTransaction(schema, (session, transaction) -> fun.get());
-	}
-
-	public <T> Uni<T> withTransaction(
-		String schema, Function<Mutiny.Session, Uni<T>> fun) {
-		return withTransaction(schema, (session, transaction) -> fun.apply(session));
+	public <T> Uni<T> withTransaction(String schema, Function<Mutiny.Session, Uni<T>> fun) {
+		return em.withTransaction(schema, (session, transaction) -> fun.apply(session));
 	}
 
 	public <T> Uni<T> withTransaction(
 		String schema, BiFunction<Mutiny.Session, Mutiny.Transaction, Uni<T>> fun) {
 
-		return withTransaction(
-			schema,
-			em::withTransaction,
-			Mutiny.Session::isOpen,
-			Mutiny.Session::createNativeQuery, fun);
-
-	}
-
-	public <T> Uni<T> withStatelessTransaction(Supplier<Uni<T>> fun) {
-		return withStatelessTransaction(null, (session, transaction) -> fun.get());
-
+		return em.withTransaction(schema, fun);
 	}
 
 	public <T> Uni<T> withStatelessTransaction(
 		Function<Mutiny.StatelessSession, Uni<T>> fun) {
 
-		return withStatelessTransaction(null, (session, transaction) -> fun.apply(session));
-
-	}
-
-	public <T> Uni<T> withStatelessTransaction(String schema, Supplier<Uni<T>> fun) {
-		return withStatelessTransaction(schema, (session, transaction) -> fun.get());
+		return em.withStatelessTransaction((session, transaction) -> fun.apply(session));
 
 	}
 
 	public <T> Uni<T> withStatelessTransaction(
 		String schema, Function<Mutiny.StatelessSession, Uni<T>> fun) {
 
-		return withStatelessTransaction(schema, (session, transaction) -> fun.apply(session));
+		return em.withStatelessTransaction(schema, (session, transaction) -> fun.apply(session));
 
 	}
 
 	public <T> Uni<T> withStatelessTransaction(
 		BiFunction<Mutiny.StatelessSession, Mutiny.Transaction, Uni<T>> fun) {
 
-		return withStatelessTransaction(null, fun);
+		return em.withStatelessTransaction(fun);
 
 	}
 
 	public <T> Uni<T> withStatelessTransaction(
 		String schema, BiFunction<Mutiny.StatelessSession, Mutiny.Transaction, Uni<T>> fun) {
 
-		return withTransaction(
-			schema,
-			em::withStatelessTransaction,
-			Mutiny.StatelessSession::isOpen,
-			Mutiny.StatelessSession::createNativeQuery, fun);
-
-	}
-
-	private <S, T> Uni<T> withTransaction(
-		String schema,
-		Function<BiFunction<S, Mutiny.Transaction, Uni<T>>, Uni<T>> withTx,
-		java.util.function.Predicate<S> isOpen,
-		BiFunction<S, String, Mutiny.Query<?>> createNativeQuery,
-		BiFunction<S, Mutiny.Transaction, Uni<T>> stmt) {
-
-		return Uni
-			.createFrom()
-			.deferred(() -> withTx.apply((s, t) -> {
-
-				Context context = Vertx.currentContext();
-
-				String currentSchema = context.getLocal("currentSchema");
-
-				if (!multiTenancyConfig.isEnabled() || currentSchema != null) {
-					return stmt.apply(s, t);
-				}
-
-				if (!isOpen.test(s)) {
-					throw new IllegalStateException(
-						"unexpected state: Hibernate session is not active in tenant transaction interceptor");
-				}
-
-				String sessionTenantId = tenantResolver.getTenantName();
-
-				if (sessionTenantId == null && schema != null) {
-					sessionTenantId = schema;
-					tenantResolver.setTenant(schema);
-				}
-				else if (schema != null) {
-					sessionTenantId = schema;
-				}
-
-				String newSchema = sessionTenantId;
-
-				if (!validator.validate(newSchema).isEmpty()) {
-					// double check just in case to avoid potential SQL injection
-					// (hint appreciated: how to properly escape postgres string literal here instead?)
-					throw new IllegalStateException(
-						"unexpected state: Hibernate session is not active in tenant transaction interceptor");
-				}
-
-				return createNativeQuery.apply(s, alterSchemaSession(newSchema))
-					.executeUpdate()
-					.invoke(() -> context.putLocal("currentSchema", newSchema))
-					.flatMap((ignore) -> {
-						try {
-							return stmt.apply(s, t);
-						} catch (Exception e) {
-							if (e instanceof K9Error) {
-								return Uni.createFrom().failure(e);
-							}
-							return Uni.createFrom().failure(new K9Error(e));
-						}
-					});
-			}));
+		return em.withStatelessTransaction(schema, fun);
 
 	}
 
