4894fb3d7a071ddd52daf13d4cfb837581807653
lorenzo venneri <lorenzo.venneri@smc.it>
2023-11-08 10:23:48 +0100

    Issue #412: install react-flow and view element

--- js-packages/admin-ui/src/components/Rules.tsx
+++ js-packages/admin-ui/src/components/Rules.tsx
@@ -3,6 +3,8 @@ import { useDeleteRulesMutation, useRulesQuery } from "../graphql-generated";
 import { AddRuleToQueryAnalyses, QueryAnalysesRule, RemoveRuleFromQueryAnalyses } from "./QueryAnalysesRules";
 import { formatName, Table } from "./Table";
 import { useToast } from "./ToastProvider";
+import ReactFlow, { addEdge, MiniMap, Controls, Background, ReactFlowProvider, Node, Edge } from "react-flow-renderer";
+import React from "react";
 
 export const RulesQuery = gql`
   query Rules($searchText: String, $cursor: String) {
@@ -46,21 +48,39 @@ export function Rules() {
       showToast({ displayType: "danger", title: "Rule error", content: error.message ?? "" });
     },
   });
+  if (rulesQuery.loading) return <div>caricamento</div>;
+  const elements: Node[] = [];
+  const rules = rulesQuery.data?.rules?.edges;
+
+  if (rules) {
+    const nodeWidth = 100; // Larghezza del nodo
+    const nodeHeight = 50; // Altezza del nodo
+
+    // Calcola la posizione del primo nodo in modo che sia centrato
+    const startX = 400;
+    const startY = 30;
+
+    rules.forEach((ruleElement, index) => {
+      const rule = ruleElement?.node;
+      const x = startX + index * nodeWidth;
+      const y = startY + index * nodeHeight;
+      elements.push({
+        id: rule?.id || "",
+        type: "default",
+        data: { label: rule?.name || "" },
+        position: { x, y },
+      });
+    });
+  }
+
   return (
-    <Table
-      data={{
-        queryResult: rulesQuery,
-        field: (data) => data?.rules,
-      }}
-      onCreatePath="/rules/new"
-      onDelete={(rule) => {
-        if (rule?.id) deleteRuleMutate({ variables: { id: rule.id } });
-      }}
-      columns={[
-        { header: "Name", content: (rule) => formatName(rule) },
-        { header: "Left Hand Side", content: (rule) => rule?.lhs },
-        { header: "Right Hand Side", content: (rule) => rule?.rhs },
-      ]}
-    />
+    <React.Fragment>
+      ciao
+      <ReactFlow nodes={elements} style={{ height: "600px", margin: "0 auto" }}>
+        <MiniMap />
+        <Controls />
+        <Background />
+      </ReactFlow>
+    </React.Fragment>
   );
 }
