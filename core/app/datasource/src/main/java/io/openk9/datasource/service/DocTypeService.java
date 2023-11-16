91d5b63ac2759611551ed3ba31654073b076da59
Mirko Zizzari <mirko.zizzari@smc.it>
2023-11-16 11:24:18 +0100

    issue #562: flush updates

--- core/app/datasource/src/main/java/io/openk9/datasource/service/DocTypeService.java
+++ core/app/datasource/src/main/java/io/openk9/datasource/service/DocTypeService.java
@@ -301,6 +301,7 @@ public class DocTypeService extends BaseK9EntityService<DocType, DocTypeDTO> {
 			.call(docType -> s.createQuery(updateDocTypeField).executeUpdate())
 			.call(docType -> s.createQuery(deleteDocTypeFields).executeUpdate())
 			.call(docType -> s.createQuery(updateDocType).executeUpdate())
+			.call(s::flush)
 			.call(s::remove)
 		);
 	}
