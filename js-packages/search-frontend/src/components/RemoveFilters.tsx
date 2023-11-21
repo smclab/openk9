e5dcefec1a19ed1d1ff96bbcd8cf02e2c6652cf3
lorenzo venneri <lorenzo.venneri@smc.it>
2023-11-21 16:55:04 +0100

    Issue #732: change svg remove filters

--- js-packages/search-frontend/src/components/RemoveFilters.tsx
+++ js-packages/search-frontend/src/components/RemoveFilters.tsx
@@ -4,6 +4,7 @@ import React from "react";
 import { DeleteLogo } from "./DeleteLogo";
 import { ConfigurationUpdateFunction } from "../embeddable/entry";
 import { css } from "styled-components/macro";
+import { CircleDelete } from "../svgElement/CircleDelete";
 
 export function RemoveFilters({
   onConfigurationChange,
@@ -18,21 +19,22 @@ export function RemoveFilters({
           onConfigurationChange({ filterTokens: [] });
         }}
         css={css`
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        padding: 8px 12px;
-        gap: 3px;
-        background: #ffffff;
-        border: 1px solid  var(--openk9-embeddable-search--secondary-active-color);
-        border-radius: 20px;
-        white-space: nowrap;
-        cursor: pointer;
-        color: var(--openk9-embeddable-search--secondary-active-color);
+          display: flex;
+          justify-content: center;
+          align-items: center;
+          padding: 8px 12px;
+          gap: 3px;
+          background: #ffffff;
+          border: 1px solid
+            var(--openk9-embeddable-search--secondary-active-color);
+          border-radius: 20px;
+          white-space: nowrap;
+          cursor: pointer;
+          color: var(--openk9-embeddable-search--secondary-active-color);
         `}
       >
+        <CircleDelete />
         {t("remove-filters")}
-        <DeleteLogo heightParam={8} widthParam={8} colorSvg={"#C0272B"} />
       </button>
     </div>
   );
