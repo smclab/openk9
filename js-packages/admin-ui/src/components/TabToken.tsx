f08be468e0aec45b09c7ecce5cd1c7edf6a0f2a4
lucac <luca.callocchia@gmail.com>
2023-12-01 09:36:55 +0100

    Issue #727: tokenTab: manage extra params (boost and fuzziness)

--- js-packages/admin-ui/src/components/TabToken.tsx
+++ js-packages/admin-ui/src/components/TabToken.tsx
@@ -15,6 +15,7 @@ import {
 import { TabTokens } from "./TabTokens";
 import {
   TokenType,
+  useAddExtraParamMutation,
   useBindDocTypeFieldToTabTokenMutation,
   useCreateOrUpdateTabTokenMutation,
   useDocTypeFieldOptionsTokenTabQuery,
@@ -35,6 +36,10 @@ const TabTokenQuery = gql`
       tokenType
       docTypeField {
         id
+      }
+      extraParams {
+        key
+        value
       }
     }
   }
@@ -64,6 +69,24 @@ gql`
   }
 `;
 
+gql`
+  mutation addExtraParam($id: ID!, $key: String, $value: String!) {
+    addExtraParam(id: $id, key: $key, value: $value) {
+      name
+    }
+  }
+`;
+
+enum fuzziness {
+  ZERO = "ZERO",
+  ONE = "ONE",
+  TWO = "TWO",
+  AUTO = "AUTO",
+}
+
+const fuzzinessDefaultValue = fuzziness.ZERO.toString();
+const boostDefaultValue = "50";
+
 export function TabToken() {
   const { tabTokenId = "new" } = useParams();
   const navigate = useNavigate();
@@ -86,6 +109,27 @@ export function TabToken() {
       }
     },
   });
+
+  const [addExtraParamMutate, addExtraParamMutation] = useAddExtraParamMutation({
+    refetchQueries: [TabTokens],
+  });
+
+  const boostValue = tabTokenTabQuery.data?.tokenTab?.extraParams?.find((element: any) => element.key === "boost")?.value?.toString();
+  const fuzzinessValue = tabTokenTabQuery.data?.tokenTab?.extraParams
+    ?.find((element: any) => element.key === "fuzziness")
+    ?.value?.toString();
+  const tokenTypeInitialValue = tabTokenTabQuery.data?.tokenTab?.tokenType;
+
+  const originalValues = {
+    name: tabTokenTabQuery.data?.tokenTab?.name,
+    description: tabTokenTabQuery.data?.tokenTab?.description,
+    value: tabTokenTabQuery.data?.tokenTab?.value,
+    filter: tabTokenTabQuery.data?.tokenTab?.filter,
+    tokenType: tabTokenTabQuery.data?.tokenTab?.tokenType,
+    boost: boostValue,
+    fuzziness: fuzzinessValue,
+  };
+
   const form = useForm({
     initialValues: React.useMemo(
       () => ({
@@ -94,10 +138,12 @@ export function TabToken() {
         value: "",
         filter: true,
         tokenType: TokenType.Autocomplete,
+        boost: boostDefaultValue,
+        fuzziness: fuzzinessDefaultValue,
       }),
       []
     ),
-    originalValues: tabTokenTabQuery.data?.tokenTab,
+    originalValues: originalValues,
     isLoading: tabTokenTabQuery.loading || createOrUpdateTabTokenMutation.loading,
     onSubmit(data) {
       createOrUpdateTabTokenMutate({
@@ -106,9 +152,27 @@ export function TabToken() {
           ...data,
         },
       });
+
+      if (data.tokenType === "TEXT" || data.tokenType === "FILTER") {
+        addExtraParamMutate({
+          variables: {
+            id: tabTokenId,
+            key: "fuzziness",
+            value: data.fuzziness ? data.fuzziness : fuzzinessDefaultValue,
+          },
+        });
+        addExtraParamMutate({
+          variables: {
+            id: tabTokenId,
+            key: "boost",
+            value: data.boost ? data.boost : boostDefaultValue,
+          },
+        });
+      }
     },
     getValidationMessages: fromFieldValidators(createOrUpdateTabTokenMutation.data?.tokenTab?.fieldValidators),
   });
+
   return (
     <React.Fragment>
       <ContainerFluid>
@@ -122,7 +186,21 @@ export function TabToken() {
           <TextInput label="Name" {...form.inputProps("name")} />
           <TextArea label="Description" {...form.inputProps("description")} />
           <TextInput label="Value" {...form.inputProps("value")} description="Value it must match for this token" />
-          <EnumSelect label="Token Type" dict={TokenType} {...form.inputProps("tokenType")} />
+          <EnumSelect
+            label="Token Type"
+            dict={TokenType}
+            {...form.inputProps("tokenType")}
+            onChange={(tokenType: TokenType) => {
+              form.inputProps("tokenType").onChange(tokenType);
+              if (tokenType !== tokenTypeInitialValue) {
+                form.inputProps("boost").onChange(boostDefaultValue);
+                form.inputProps("fuzziness").onChange(fuzzinessDefaultValue);
+              } else {
+                form.inputProps("boost").onChange(boostValue ? boostValue : boostDefaultValue);
+                form.inputProps("fuzziness").onChange(fuzzinessValue ? fuzzinessValue : fuzzinessDefaultValue);
+              }
+            }}
+          />
           <BooleanInput label="Filter" {...form.inputProps("filter")} />
           {tabTokenId !== "new" && (
             <form
@@ -146,6 +224,12 @@ export function TabToken() {
                 description={"Document Type Field associated to this token"}
               />
             </form>
+          )}
+          {(form.inputProps("tokenType").value === "TEXT" || form.inputProps("tokenType").value === "FILTER") && (
+            <div>
+              <TextInput label="boost" {...form.inputProps("boost")} />
+              <EnumSelect label="fuzziness" dict={fuzziness} {...form.inputProps("fuzziness")} />
+            </div>
           )}
           <div className="sheet-footer">
             <CustomButtom nameButton={tabTokenId === "new" ? "Create" : "Update"} canSubmit={!form.canSubmit} typeSelectet="submit" />
