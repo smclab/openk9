49b1c110755608736c33143b8913df1fe41eb3ce
Mirko Zizzari <mirko.zizzari@smc.it>
2024-02-02 19:15:47 +0100

    issue #330: implements grpc k8s deployEnrichItem

---
+++ core/app/k8s-client/src/main/java/io/openk9/k8sclient/grpc/K8sOperatorService.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2020-present SMC Treviso s.r.l. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.openk9.k8sclient.grpc;
+
+import io.fabric8.kubernetes.client.KubernetesClient;
+import io.openk9.k8s.crd.Manifest;
+import io.openk9.k8s.operator.grpc.DeployEnrichItemRequest;
+import io.openk9.k8s.operator.grpc.DeployEnrichItemResponse;
+import io.openk9.k8s.operator.grpc.DeployMLEnrichItemRequest;
+import io.openk9.k8s.operator.grpc.DeployMLEnrichItemResponse;
+import io.openk9.k8s.operator.grpc.DeployPluginDriverRequest;
+import io.openk9.k8s.operator.grpc.DeployPluginDriverResponse;
+import io.openk9.k8s.operator.grpc.K8sOperator;
+import io.quarkus.grpc.GrpcService;
+import io.smallrye.mutiny.Uni;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+
+import javax.inject.Inject;
+
+@GrpcService
+public class K8sOperatorService implements K8sOperator {
+	@Inject
+	KubernetesClient k8sClient;
+
+	@Inject
+	@ConfigProperty(name = "openk9.kubernetes-client.namespace")
+	String namespace;
+
+	@Inject
+	@ConfigProperty(name = "openk9.kubernetes-client.manifest-type")
+	Manifest.Type manifestType;
+
+	@Override
+	public Uni<DeployEnrichItemResponse> deployEnrichItem(DeployEnrichItemRequest request) {
+		var chart = request.getChart();
+		var schemaName = request.getSchemaName();
+		var version = request.getVersion();
+
+		return Uni
+			.createFrom()
+			.emitter(emitter -> {
+				var resource = k8sClient.resource(Manifest.builder()
+					.targetNamespace(namespace)
+					.chart(chart)
+					.version(version)
+					.tenant(schemaName)
+					.type(manifestType)
+					.build()
+					.asResource()
+				).createOrReplace();
+
+				emitter.complete(DeployEnrichItemResponse
+					.newBuilder()
+					.setStatus(resource.getMetadata().getName())
+					.build()
+				);
+			});
+	}
+
+	@Override
+	public Uni<DeployMLEnrichItemResponse> deployMLEnrichItem(DeployMLEnrichItemRequest request) {
+		return null;
+	}
+
+	@Override
+	public Uni<DeployPluginDriverResponse> deployPluginDriver(DeployPluginDriverRequest request) {
+		return null;
+	}
+
+}
