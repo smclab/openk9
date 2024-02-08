52fb4641594b9af9b3a7813ce897a5f33d75249b
Mirko Zizzari <mirko.zizzari@smc.it>
2024-02-08 17:42:43 +0100

    issue #330: adds grpc client test

---
+++ core/app/tenant-manager/src/test/java/io/openk9/tenantmanager/grpc/TenantManagerGrpcServiceTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2020-present SMC Treviso s.r.l. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.openk9.tenantmanager.grpc;
+
+import com.google.protobuf.Empty;
+import io.openk9.tenantmanager.BaseTenantManagerQuarkusTest;
+import io.quarkus.grpc.GrpcClient;
+import io.quarkus.test.junit.QuarkusTest;
+import io.quarkus.test.vertx.RunOnVertxContext;
+import io.quarkus.test.vertx.UniAsserter;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+@QuarkusTest
+class TenantManagerGrpcServiceTest extends BaseTenantManagerQuarkusTest {
+
+	@GrpcClient
+	TenantManager client;
+
+	@Test
+	@RunOnVertxContext
+	void findTenantList(UniAsserter asserter) {
+
+		asserter.assertThat(
+			() -> whenTenantManagerInitialized(
+				client.findTenantList(Empty.newBuilder().build())),
+			response -> Assertions.assertTrue(response.getTenantResponseList().isEmpty())
+		);
+
+	}
+
+}
