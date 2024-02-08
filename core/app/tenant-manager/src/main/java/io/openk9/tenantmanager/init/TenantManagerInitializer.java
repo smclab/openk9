8d0162a17289f74851d7292e3b956669581b7e5d
Mirko Zizzari <mirko.zizzari@smc.it>
2024-02-08 17:40:52 +0100

    issue #330: advises on eventbus when ready

--- core/app/tenant-manager/src/main/java/io/openk9/tenantmanager/init/TenantManagerInitializer.java
+++ core/app/tenant-manager/src/main/java/io/openk9/tenantmanager/init/TenantManagerInitializer.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2020-present SMC Treviso s.r.l. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.openk9.tenantmanager.init;
 
 import io.openk9.tenantmanager.dto.SchemaTuple;
@@ -7,17 +24,40 @@ import io.openk9.tenantmanager.service.DatasourceLiquibaseService;
 import io.openk9.tenantmanager.service.TenantService;
 import io.quarkus.runtime.Startup;
 import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.core.eventbus.EventBus;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.jboss.logging.Logger;
 
+import java.util.LinkedList;
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import java.util.LinkedList;
 
 @ApplicationScoped
 @Startup
 public class TenantManagerInitializer {
+
+	public static final String INITIALIZED = "TenantManagerInitializer#INITIALIZED";
+	public static final String ERROR = "TenantManagerInitializer#ERROR";
+
+	@ConfigProperty(name = "quarkus.datasource.reactive.url")
+	String openk9DatasourceUrl;
+	@ConfigProperty(name = "quarkus.datasource.username")
+	String datasourceUsername;
+	@ConfigProperty(name = "quarkus.datasource.password")
+	String datasourcePassword;
+	@ConfigProperty(name = "openk9.tenant-manager.liquibase.change-log")
+	String changeLogLocation;
+	@Inject
+	DatasourceLiquibaseService liquibaseService;
+	@Inject
+	TenantService tenantService;
+	@Inject
+	Logger logger;
+	@Inject
+	LiquibaseValidatorActorSystem liquibaseValidatorActorSystem;
+	@Inject
+	EventBus eventBus;
 
 	@PostConstruct
 	public void onStart() {
@@ -27,7 +67,8 @@ public class TenantManagerInitializer {
 		paramsList.add(
 			new Params(null, null, changeLogLocation, null, null,
 				liquibaseService.toJdbcUrl(openk9DatasourceUrl),
-				datasourceUsername, datasourcePassword)
+				datasourceUsername, datasourcePassword
+			)
 		);
 
 		liquibaseValidatorActorSystem
@@ -43,45 +84,33 @@ public class TenantManagerInitializer {
 						for (SchemaTuple schema : schemas) {
 							schemaParamList.add(
 								new Params(
-									schema.schemaName(), schema.liquibaseSchemaName(),
-									liquibaseService.getChangeLogLocation(), liquibaseService.getChangeLogLockTableName(),
-									liquibaseService.getChangeLogTableName(), liquibaseService.getDatasourceJdbcUrl(),
-									datasourceUsername, datasourcePassword)
+									schema.schemaName(),
+									schema.liquibaseSchemaName(),
+									liquibaseService.getChangeLogLocation(),
+									liquibaseService.getChangeLogLockTableName(),
+									liquibaseService.getChangeLogTableName(),
+									liquibaseService.getDatasourceJdbcUrl(),
+									datasourceUsername,
+									datasourcePassword
+								)
 							);
 						}
 
 						return liquibaseValidatorActorSystem.validateSchemas(schemaParamList);
 
 					})))
-			.subscribe().with(
-				nothing -> logger.info("Tenant Upgrade Finished"),
-				throwable -> logger.error("Tenant Upgrade Failed", throwable));
+			.subscribe()
+			.with(
+				nothing -> {
+					logger.info("Tenant Upgrade Finished");
+					eventBus.send(INITIALIZED, INITIALIZED);
+				},
+				throwable -> {
+					logger.error("Tenant Upgrade Failed", throwable);
+					eventBus.send(ERROR, ERROR);
+				}
+			);
 
 	}
-
-	@ConfigProperty(name = "quarkus.datasource.reactive.url")
-	String openk9DatasourceUrl;
-
-	@ConfigProperty(name = "quarkus.datasource.username")
-	String datasourceUsername;
-
-	@ConfigProperty(name = "quarkus.datasource.password")
-	String datasourcePassword;
-
-	@ConfigProperty(name = "openk9.tenant-manager.liquibase.change-log")
-	String changeLogLocation;
-
-	@Inject
-	DatasourceLiquibaseService liquibaseService;
-
-	@Inject
-	TenantService tenantService;
-
-	@Inject
-	Logger logger;
-	
-	@Inject
-	LiquibaseValidatorActorSystem liquibaseValidatorActorSystem;
-
 
 }
