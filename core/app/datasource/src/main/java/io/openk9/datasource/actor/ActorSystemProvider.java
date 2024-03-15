c3bcdb05b2dfaeb92f8be7c2485aeb42e43ed9b9
Mirko Zizzari <mirko.zizzari@smc.it>
2024-03-15 18:29:43 +0100

    issue #758: fixes actorSystem config order

--- core/app/datasource/src/main/java/io/openk9/datasource/actor/ActorSystemProvider.java
+++ core/app/datasource/src/main/java/io/openk9/datasource/actor/ActorSystemProvider.java
@@ -1,7 +1,23 @@
+/*
+ * Copyright (c) 2020-present SMC Treviso s.r.l. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.openk9.datasource.actor;
 
 import akka.actor.typed.ActorSystem;
-import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import io.quarkus.runtime.Startup;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
@@ -19,11 +35,16 @@ public class ActorSystemProvider {
 	@PostConstruct
 	void init() {
 
-		Config defaultConfig = ConfigFactory.load(clusterFile);
-		Config config = defaultConfig.withFallback(ConfigFactory.load());
+		var applicationConfig = ConfigFactory.load();
+		var defaultClusterConfig = ConfigFactory.load(clusterFile);
+		var combined = applicationConfig.withFallback(defaultClusterConfig);
+		var complete = ConfigFactory.load(combined);
 
 		actorSystem = ActorSystem.create(
-            Initializer.create(actorSystemBehaviorInitializerInstance), "datasource", config);
+			Initializer.create(actorSystemBehaviorInitializerInstance),
+			"datasource",
+			complete
+		);
 
 		for (ActorSystemInitializer actorSystemInitializer : actorSystemInitializerInstance) {
 			actorSystemInitializer.init(actorSystem);
