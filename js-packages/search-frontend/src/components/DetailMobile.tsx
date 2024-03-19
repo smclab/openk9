2c4abffcc335a3a632a5e6e6f4f7f2f0fb42bbe4
lorenzo venneri <lorenzo.venneri@smc.it>
2024-03-19 10:13:02 +0100

    Issue #904: fix accessibility modal

--- js-packages/search-frontend/src/components/DetailMobile.tsx
+++ js-packages/search-frontend/src/components/DetailMobile.tsx
@@ -9,60 +9,72 @@ import { ModalDetail } from "./ModalDetail";
 export type DetailMobileProps<E> = {
   result: GenericResultItem<E> | null;
   setDetailMobile: any;
-  onClose(): void; 
+  onClose(): void;
 };
 function DetailMobile<E>(props: DetailMobileProps<E>) {
   const result = props.result as any;
   const setDetailMobile = props.setDetailMobile as any;
-  const action=props.onClose ;
+  const action = props.onClose;
   const renderers = useRenderers();
-    const modalRef = React.useRef(null);
-    const [isOpen,setIsOpen]=React.useState(true)
-  
-    React.useEffect(() => {
-      const modalElement = modalRef.current as any;
+  const modalRef = React.useRef(null);
+  const [isOpen, setIsOpen] = React.useState(true);
 
-      if (modalElement ) {
-        const focusableElements = modalElement?.querySelectorAll(
-          'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'
-        );
-        const firstElement = focusableElements[0];
-        const lastElement = focusableElements[focusableElements.length - 1];
-  
-        const handleTabKeyPress = (event:any) => {
-          if (event.key === "Tab") {
-            if (event.shiftKey && document.activeElement === firstElement) {
-              event.preventDefault();
-              lastElement.focus();
-            } else if (
-              !event.shiftKey &&
-              document.activeElement === lastElement
-            ) {
-              event.preventDefault();
-              firstElement.focus();
-            }
+  React.useEffect(() => {
+    const modalElement = modalRef.current as any;
+
+    if (modalElement) {
+      const focusableElements = modalElement?.querySelectorAll(
+        'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])',
+      );
+      const firstElement = focusableElements[0];
+      const lastElement = focusableElements[focusableElements.length - 1];
+
+      const handleTabKeyPress = (event: any) => {
+        if (event.key === "Tab") {
+          if (event.shiftKey && document.activeElement === firstElement) {
+            event.preventDefault();
+            lastElement.focus();
+          } else if (
+            !event.shiftKey &&
+            document.activeElement === lastElement
+          ) {
+            event.preventDefault();
+            firstElement.focus();
           }
-        };
-  
-        const handleEscapeKeyPress = (event:any) => {
-          if (event.key === "Escape") {
-            setIsOpen(false)
-          }
-        };
-  
-        modalElement?.addEventListener("keydown", handleTabKeyPress);
-        modalElement?.addEventListener("keydown", handleEscapeKeyPress);
-  
-        return () => {
-          modalElement?.removeEventListener("keydown", handleTabKeyPress);
-          modalElement?.removeEventListener("keydown", handleEscapeKeyPress);
-        };
-      }
-    }, [isOpen,setIsOpen,result]);
-  
+        }
+      };
+
+      const handleEscapeKeyPress = (event: any) => {
+        if (event.key === "Escape") {
+          setIsOpen(false);
+        }
+      };
+
+      modalElement?.addEventListener("keydown", handleTabKeyPress);
+      modalElement?.addEventListener("keydown", handleEscapeKeyPress);
+
+      return () => {
+        modalElement?.removeEventListener("keydown", handleTabKeyPress);
+        modalElement?.removeEventListener("keydown", handleEscapeKeyPress);
+      };
+    }
+  }, [isOpen, setIsOpen, result]);
+
   const componet = (
-    <div ref={modalRef} style={{height:"100%"}}>
-    <DetailMemo result={result} setDetailMobile={setDetailMobile} isMobile={true} actionOnCLose={action} />
+    <div
+      ref={modalRef}
+      style={{ height: "100%" }}
+      id="dialog-modal-detail"
+      role="dialog"
+      className="modal"
+      aria-modal={isOpen ? "true" : "false"}
+    >
+      <DetailMemo
+        result={result}
+        setDetailMobile={setDetailMobile}
+        isMobile={true}
+        actionOnCLose={action}
+      />
     </div>
   );
 
