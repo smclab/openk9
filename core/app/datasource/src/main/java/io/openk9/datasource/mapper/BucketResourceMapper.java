1db2e4493df4ea6b2510d97aa15e95ba6d5844f3
Mirko Zizzari <mirko.zizzari@smc.it>
2024-03-26 12:38:46 +0100

    issue #909: get sortings translations

--- core/app/datasource/src/main/java/io/openk9/datasource/mapper/BucketResourceMapper.java
+++ core/app/datasource/src/main/java/io/openk9/datasource/mapper/BucketResourceMapper.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2020-present SMC Treviso s.r.l. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.openk9.datasource.mapper;
 
 import io.openk9.datasource.model.Bucket;
@@ -32,10 +49,14 @@ public interface BucketResourceMapper {
 	BucketResource.CurrentBucket toCurrentBucket(Bucket bucket);
 
 	default List<TabResponseDTO> toTabResponseDtoList(List<Tab> tabList) {
-		return toTabResponseDtoList(tabList, null);
+		return toTabResponseDtoList(tabList, null, null);
 	}
 
-	default List<TabResponseDTO> toTabResponseDtoList(List<Tab> tabList, Map<Long, Map<String, String>> translations) {
+	default List<TabResponseDTO> toTabResponseDtoList(
+		List<Tab> tabList,
+		Map<Long, Map<String, String>> translations,
+		Map<Long, Map<String, String>> sortingsTranslationMaps) {
+
 		if (translations != null) {
 			return tabList
 				.stream()
@@ -45,10 +66,10 @@ public interface BucketResourceMapper {
 						.stream()
 						.map(this::toTokenTabResponseDto)
 						.toList(),
-					tab.getSortings()
-						.stream()
-						.map(this::toSortingResponseDTO)
-						.toList(),
+					this.toSortingResponseDtoList(
+						tab.getSortings().stream().toList(),
+						sortingsTranslationMaps
+					),
 					translations.get(tab.getId()))
 				)
 				.toList();
--- core/app/datasource/src/main/java/io/openk9/datasource/web/BucketResource.java
+++ core/app/datasource/src/main/java/io/openk9/datasource/web/BucketResource.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2020-present SMC Treviso s.r.l. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.openk9.datasource.web;
 
 
@@ -38,6 +55,9 @@ import io.smallrye.mutiny.Uni;
 import io.vertx.core.http.HttpServerRequest;
 import org.hibernate.reactive.mutiny.Mutiny;
 
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Stream;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 import javax.persistence.Tuple;
@@ -53,8 +73,6 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Context;
-import java.util.List;
-import java.util.stream.Stream;
 
 @ApplicationScoped
 @Path("/buckets")
@@ -371,13 +389,30 @@ public class BucketResource {
 				.getResultList()
 				.chain(tabs -> {
 					if (translated) {
+						var sortings = tabs.stream()
+							.map(Tab::getSortings)
+							.flatMap(Collection::stream)
+							.map(K9Entity::getId)
+							.distinct()
+							.toList();
+
+
 						return translationService
 							.getTranslationMaps(
 								Tab.class,
 								tabs.stream()
 									.map(K9Entity::getId)
 									.toList())
-							.map(maps -> mapper.toTabResponseDtoList(tabs, maps));
+							.flatMap(tabTranslationMaps -> translationService.getTranslationMaps(
+										Sorting.class,
+										sortings
+									)
+									.map(sortingsTranslationMaps -> mapper.toTabResponseDtoList(
+										tabs,
+										tabTranslationMaps,
+										sortingsTranslationMaps
+									))
+							);
 					}
 					else {
 						return Uni
