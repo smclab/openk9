0625b94306c485ab749f427434473c2dc7e630e5
lorenzo venneri <lorenzo.venneri@smc.it>
2024-04-04 16:16:12 +0100

    Issue #916: fix type and pdf detail

--- js-packages/search-frontend/src/renderers/openk9/pdf/PdfDetail.tsx
+++ js-packages/search-frontend/src/renderers/openk9/pdf/PdfDetail.tsx
@@ -75,7 +75,7 @@ export function PdfDetail({ result }: PdfDetailProps) {
         </ResultLinkTwo>
       </div>
       <div style={{ marginTop: "15px" }}>
-        {result.source.resources.binaries.map((binary) => {
+        {result.source.resources?.binaries.map((binary) => {
           const url = `/api/file-manager/v1/download/byte/${binary.resourceId}/${result.source.tenantId}`;
           return (
             <ViewIfUrlOk key={binary.id} url={url}>
