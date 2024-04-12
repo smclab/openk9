2169a051e37a8d6ff57650de55df469994633438
lorenzo venneri <lorenzo.venneri@smc.it>
2024-04-12 11:48:48 +0200

    Issue #928: fix accessibility tabs

--- js-packages/search-frontend/src/components/Tabs.tsx
+++ js-packages/search-frontend/src/components/Tabs.tsx
@@ -71,15 +71,21 @@ function Tabs({
 
   return !scrollMode ? (
     <div css={css`openk9-container-arrow-tabs`}>
-      <nav
+      <h2 id="title-tabs-openk9" className="visually-hidden">
+        filtri e argomenti
+      </h2>
+      <ul
         className="openk9-nav-container-tabs"
         ref={elementRef}
+        role="list"
         css={css`
           display: flex;
+          padding: 0;
           overflow-x: hidden;
           white-space: nowrap;
           width: 90vw;
           margin-left: 15px;
+          list-style-type: none;
         `}
       >
         <div
@@ -158,42 +164,103 @@ function Tabs({
             defaultValue: tab.label,
           });
           return (
-            <button
-              className={`openk9-single-tab-container`}
-              key={index}
-              tabIndex={0}
-              css={css`
-                border: none;
-                background: none;
-              `}
-            >
-              <span
+            <li role="listitem" aria-labelledby="title-tabs-openk9">
+              <button
+                className={`openk9-single-tab-container`}
                 key={index}
-                className={
-                  "openk9-single-tab " +
-                  (isSelected ? "openk9-active-tab" : "openk9-not-active")
-                }
+                tabIndex={0}
                 css={css`
-                  white-space: nowrap;
-                  padding: 8px 12px;
-                  background: ${isSelected
-                    ? "var(--openk9-embeddable-search--primary-background-tab-color)"
-                    : "var(--openk9-embeddable-search--secondary-background-tab-color)"};
-                  border-radius: 8px;
-                  font: Helvetica Neue LT Std;
-                  font-style: normal;
-                  display: block;
-                  color: ${isSelected
-                    ? "var(--openk9-embeddable-search--primary-background-color)"
-                    : "var(--openk9-embeddable-tabs--primary-color)"};
-                  ${isSelected
-                    ? "var(--openk9-embeddable-search--active-color)"
-                    : "transparent"};
-                  cursor: ${isSelected ? "" : "pointer"};
-                  user-select: none;
-                  :hover {
-                    ${isSelected ? "" : "text-decoration: underline;"}
+                  border: none;
+                  background: none;
+                `}
+              >
+                <span
+                  key={index}
+                  className={
+                    "openk9-single-tab " +
+                    (isSelected ? "openk9-active-tab" : "openk9-not-active")
                   }
+                  css={css`
+                    white-space: nowrap;
+                    padding: 8px 12px;
+                    background: ${isSelected
+                      ? "var(--openk9-embeddable-search--primary-background-tab-color)"
+                      : "var(--openk9-embeddable-search--secondary-background-tab-color)"};
+                    border-radius: 8px;
+                    font: Helvetica Neue LT Std;
+                    font-style: normal;
+                    display: block;
+                    color: ${isSelected
+                      ? "var(--openk9-embeddable-search--primary-background-color)"
+                      : "var(--openk9-embeddable-tabs--primary-color)"};
+                    ${isSelected
+                      ? "var(--openk9-embeddable-search--active-color)"
+                      : "transparent"};
+                    cursor: ${isSelected ? "" : "pointer"};
+                    user-select: none;
+                    :hover {
+                      ${isSelected ? "" : "text-decoration: underline;"}
+                    }
+                  `}
+                  onClick={() => {
+                    onSelectedTabIndexChange(index);
+                    if (reset) {
+                      if (reset.filters) resetFilter();
+                      if (reset?.calendar) resetFilterCalendar();
+                      if (reset?.search)
+                        selectionsDispatch({
+                          type: "reset-search",
+                        });
+                      if (reset?.sort) resetSort();
+                    }
+                    if (onAction) onAction();
+                  }}
+                >
+                  {tabTraslation.toUpperCase()}
+                </span>
+              </button>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  ) : (
+    <React.Fragment>
+      <h2 id="title-tabs-openk9" className="visually-hidden">
+        filtri e argomenti
+      </h2>
+      <ul
+        className="openk9-tabs-container-internal"
+        role="list"
+        css={css`
+          display: flex;
+          padding-top: 14px;
+          padding: 8px 12px;
+          width: fit-content;
+          height: fit-content;
+          gap: 16px;
+          list-style-type: none;
+          @media (max-width: 480px) {
+            gap: 10px;
+          }
+        `}
+      >
+        {tabs.map((tab, index) => {
+          const isSelected = index === selectedTabIndex;
+          const tabTraslation = translationTab({
+            language: language,
+            tabLanguages: tab.translationMap,
+            defaultValue: tab.label,
+          });
+          return (
+            <li role="listitem" aria-labelledby="title-tabs-openk9">
+              <button
+                className="openk9-single-tab-container"
+                key={index}
+                tabIndex={0}
+                css={css`
+                  border: none;
+                  background: none;
                 `}
                 onClick={() => {
                   onSelectedTabIndexChange(index);
@@ -209,93 +276,43 @@ function Tabs({
                   if (onAction) onAction();
                 }}
               >
-                {tabTraslation.toUpperCase()}
-              </span>
-            </button>
+                <span
+                  key={index}
+                  className={
+                    "openk9-single-tab " +
+                    (isSelected ? "openk9-active-tab" : "openk9-not-active")
+                  }
+                  css={css`
+                    white-space: nowrap;
+                    padding: 8px 12px;
+                    background: ${isSelected
+                      ? "var(--openk9-embeddable-search--primary-background-tab-color)"
+                      : "var(--openk9-embeddable-search--secondary-background-tab-color)"};
+                    border-radius: 8px;
+                    font: Helvetica Neue LT Std;
+                    font-style: normal;
+                    display: block;
+                    color: ${isSelected
+                      ? "var(--openk9-embeddable-search--primary-background-color)"
+                      : "var(--openk9-embeddable-tabs--primary-color)"};
+                    ${isSelected
+                      ? "var(--openk9-embeddable-search--active-color)"
+                      : "transparent"};
+                    cursor: ${isSelected ? "" : "pointer"};
+                    user-select: none;
+                    :hover {
+                      ${isSelected ? "" : "text-decoration: underline;"}
+                    }
+                  `}
+                >
+                  {tabTraslation.toUpperCase()}
+                </span>
+              </button>
+            </li>
           );
         })}
-      </nav>
-    </div>
-  ) : (
-    <nav
-      className="openk9-tabs-container-internal"
-      css={css`
-        display: flex;
-        padding-top: 14px;
-        padding: 8px 12px;
-        width: fit-content;
-        height: fit-content;
-        gap: 16px;
-        @media (max-width: 480px) {
-          gap: 10px;
-        }
-      `}
-    >
-      {tabs.map((tab, index) => {
-        const isSelected = index === selectedTabIndex;
-        const tabTraslation = translationTab({
-          language: language,
-          tabLanguages: tab.translationMap,
-          defaultValue: tab.label,
-        });
-        return (
-          <button
-            className="openk9-single-tab-container"
-            key={index}
-            tabIndex={0}
-            css={css`
-              border: none;
-              background: none;
-            `}
-            onClick={() => {
-              onSelectedTabIndexChange(index);
-              if (reset) {
-                if (reset.filters) resetFilter();
-                if (reset?.calendar) resetFilterCalendar();
-                if (reset?.search)
-                  selectionsDispatch({
-                    type: "reset-search",
-                  });
-                if (reset?.sort) resetSort();
-              }
-              if (onAction) onAction();
-            }}
-          >
-            <span
-              key={index}
-              className={
-                "openk9-single-tab " +
-                (isSelected ? "openk9-active-tab" : "openk9-not-active")
-              }
-              css={css`
-                white-space: nowrap;
-                padding: 8px 12px;
-                background: ${isSelected
-                  ? "var(--openk9-embeddable-search--primary-background-tab-color)"
-                  : "var(--openk9-embeddable-search--secondary-background-tab-color)"};
-                border-radius: 8px;
-                font: Helvetica Neue LT Std;
-                font-style: normal;
-                display: block;
-                color: ${isSelected
-                  ? "var(--openk9-embeddable-search--primary-background-color)"
-                  : "var(--openk9-embeddable-tabs--primary-color)"};
-                ${isSelected
-                  ? "var(--openk9-embeddable-search--active-color)"
-                  : "transparent"};
-                cursor: ${isSelected ? "" : "pointer"};
-                user-select: none;
-                :hover {
-                  ${isSelected ? "" : "text-decoration: underline;"}
-                }
-              `}
-            >
-              {tabTraslation.toUpperCase()}
-            </span>
-          </button>
-        );
-      })}
-    </nav>
+      </ul>
+    </React.Fragment>
   );
 }
 export const TabsMemo = React.memo(Tabs);
