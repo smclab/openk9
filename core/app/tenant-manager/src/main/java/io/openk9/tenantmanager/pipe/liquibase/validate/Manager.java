4bbd4d344a7ea262e0a7f5b0ef92f2f7aa626375
Mirko Zizzari <mirko.zizzari@smc.it>
2024-05-06 15:51:17 +0200

    issue #968: specializes Liquibase exception
    (cherry picked from commit 9f7f0cf8a5889e4e8fb50e2c5a6c993e745fb6f7)

--- core/app/tenant-manager/src/main/java/io/openk9/tenantmanager/pipe/liquibase/validate/Manager.java
+++ core/app/tenant-manager/src/main/java/io/openk9/tenantmanager/pipe/liquibase/validate/Manager.java
@@ -1,3 +1,20 @@
+/*
+ * Copyright (c) 2020-present SMC Treviso s.r.l. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.openk9.tenantmanager.pipe.liquibase.validate;
 
 import akka.actor.typed.ActorRef;
@@ -74,10 +91,9 @@ public class Manager {
 				if (msg.response() instanceof Liquibase.Success) {
 					return onStart(liquibaseParamsList, replyTo, context);
 				}
-				else if (msg.response() instanceof Liquibase.Error) {
+				else if (msg.response() instanceof Liquibase.Error error) {
 					replyTo.tell(
-						new Error(ExceptionUtil.generateStackTrace(
-							((Liquibase.Error) msg.response()).throwable())));
+						new Error(ExceptionUtil.generateStackTrace(error.exception())));
 					return Behaviors.stopped();
 				}
 				else {
