9e928ca4bc6fc5e7c1c92c7b078958d10cc6b059
Mirko Zizzari <mirko.zizzari@smc.it>
2024-07-09 15:17:04 +0200

    issue #957: defines llm dto fields

--- core/app/datasource/src/main/java/io/openk9/datasource/model/dto/LargeLanguageModelDTO.java
+++ core/app/datasource/src/main/java/io/openk9/datasource/model/dto/LargeLanguageModelDTO.java
@@ -18,6 +18,25 @@
 package io.openk9.datasource.model.dto;
 
 import io.openk9.datasource.model.dto.util.K9EntityDTO;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.experimental.SuperBuilder;
 
+import javax.validation.constraints.NotNull;
+
+
+@NoArgsConstructor
+@SuperBuilder
+@Getter
+@Setter
+@EqualsAndHashCode(callSuper = true)
 public class LargeLanguageModelDTO extends K9EntityDTO {
+
+	@NotNull
+	private String apiUrl;
+	private String apiKey;
+	private String promptTemplate;
+
 }
