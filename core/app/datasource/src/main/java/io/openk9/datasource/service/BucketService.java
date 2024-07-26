10db992f7f64cc6872cd545b673d51d7d4aabde4
Michele Bastianelli <michele.bastianelli@smc.it>
2024-07-26 11:08:30 +0200

    #970 adds Bucket update implementation

--- core/app/datasource/src/main/java/io/openk9/datasource/service/BucketService.java
+++ core/app/datasource/src/main/java/io/openk9/datasource/service/BucketService.java
@@ -157,6 +157,61 @@ public class BucketService extends BaseK9EntityService<Bucket, BucketDTO> {
 		return super.patch(bucketId, bucketDTO);
 	}
 
+	public Uni<Bucket> update(long bucketId, BucketDTO bucketDTO) {
+		if ( bucketDTO instanceof BucketWithListsDTO bucketWithListsDTO ) {
+
+			return sessionFactory.withTransaction(
+				(s, transaction) -> findById(s, bucketId)
+					.call(bucket -> Mutiny.fetch(bucket.getDatasources()))
+					.call(bucket -> Mutiny.fetch(bucket.getSuggestionCategories()))
+					.call(bucket -> Mutiny.fetch(bucket.getTabs()))
+					.flatMap(bucket -> {
+						var transientBucket = mapper.update(bucket, bucketWithListsDTO);
+
+						var datasourceIds = bucketWithListsDTO.getDatasourceIds();
+						transientBucket.getDatasources().clear();
+
+						if (datasourceIds != null) {
+							var datasources = datasourceIds.stream()
+								.map(id -> s.getReference(Datasource.class, id))
+								.collect(Collectors.toSet());
+
+							transientBucket.setDatasources(datasources);
+						}
+
+						var suggestionCategoryIds =
+							bucketWithListsDTO.getSuggestionCategoryIds();
+						transientBucket.getSuggestionCategories().clear();
+
+						if (suggestionCategoryIds != null) {
+							var suggestionCategories =
+								suggestionCategoryIds.stream()
+									.map(id -> s.getReference(SuggestionCategory.class, id))
+									.collect(Collectors.toSet());
+
+							transientBucket.setSuggestionCategories(suggestionCategories);
+						}
+
+						var tabIds = bucketWithListsDTO.getTabIds();
+						transientBucket.getTabs().clear();
+
+						if (tabIds != null) {
+							var tabs = tabIds.stream()
+								.map(id -> s.getReference(Tab.class, id))
+								.collect(Collectors.toList());
+
+							transientBucket.setTabs(tabs);
+						}
+
+						return s.merge(transientBucket)
+							.map(__ -> transientBucket);
+					})
+			);
+		}
+
+		return super.update(bucketId, bucketDTO);
+	}
+
 	public Uni<QueryAnalysis> getQueryAnalysis(long bucketId) {
 		return sessionFactory.withTransaction(s -> findById(s, bucketId)
 			.flatMap(bucket -> s.fetch(bucket.getQueryAnalysis())));
