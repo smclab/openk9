46e00bbb0c913058e50522f8a335152b4f0f8a5b
lucac <luca.callocchia@gmail.com>
2024-07-29 15:22:18 +0200

    add api /api/rag/chat

--- app/server.py
+++ app/server.py
@@ -4,7 +4,7 @@ from fastapi import FastAPI, Request
 from fastapi.responses import RedirectResponse, StreamingResponse
 from pydantic import BaseModel
 
-from app.chain import get_chain
+from app.chain import get_chain, get_chat_chain
 
 app = FastAPI()
 
@@ -49,6 +49,61 @@ async def search_query(search_query: SearchQuery, request: Request):
     virtualHost = "gamahiro.openk9.io"
 
     chain = get_chain(
+        searchQuery,
+        range,
+        afterKey,
+        suggestKeyword,
+        suggestionCategoryId,
+        jwt,
+        extra,
+        sort,
+        sortAfterKey,
+        language,
+        vectorIndices,
+        virtualHost,
+        searchText,
+    )
+    return StreamingResponse(chain, media_type="text/event-stream")
+
+
+class SearchQueryChat(BaseModel):
+    searchText: str
+    chatId: Optional[str] = None
+    searchQuery: list
+    range: list = [0, 7]
+    afterKey: Optional[str] = None
+    suggestKeyword: Optional[str] = None
+    suggestionCategoryId: Optional[int] = None
+    jwt: Optional[str] = None
+    extra: Optional[dict] = None
+    sort: Optional[list] = None
+    sortAfterKey: Optional[str] = None
+    language: Optional[str] = None
+    vectorIndices: bool = False
+    searchText: str
+
+
+@app.post("/api/rag/chat")
+async def search_query(search_query: SearchQueryChat, request: Request):
+    searchText = search_query.searchText
+    chatId = search_query.chatId
+    searchQuery = search_query.searchQuery
+    range = search_query.range
+    afterKey = search_query.afterKey
+    suggestKeyword = search_query.suggestKeyword
+    suggestionCategoryId = search_query.suggestionCategoryId
+    jwt = search_query.jwt
+    extra = search_query.extra
+    sort = search_query.sort
+    sortAfterKey = search_query.sortAfterKey
+    language = search_query.language
+    vectorIndices = search_query.vectorIndices
+
+    # TODO: replace "gamahiro.openk9.io" with virtualHost
+    # virtualHost = request.client.host
+    virtualHost = "gamahiro.openk9.io"
+
+    chain = get_chat_chain(
         searchQuery,
         range,
         afterKey,
