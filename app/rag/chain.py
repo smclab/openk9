e63fdd7e5b317a2f092e65ce6a00898416946629
lucac <luca.callocchia@gmail.com>
2024-08-07 15:27:14 +0200

    fix #5: - add new jsonconig parameters

--- app/rag/chain.py
+++ app/rag/chain.py
@@ -2,13 +2,10 @@ import json
 
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
-from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
 
 from app.external_services.grpc.grpc_client import get_llm_configuration
 from app.rag.retriever import OpenSearchRetriever
-
-OLLAMA_URL = 'http://35.207.116.4:11434'
 
 
 def get_chain(
@@ -32,7 +29,8 @@ def get_chain(
     api_url = configuration["api_url"]
     api_key = configuration["api_key"]
     json_config = configuration["json_config"]
-    chat_model_type = json_config["modelType"]
+    model_type = configuration["type"]
+    model = configuration["model"] if configuration["model"] else 'gpt-3.5-turbo'
 
     documents = OpenSearchRetriever._get_relevant_documents(
         searchQuery,
@@ -51,80 +49,21 @@ def get_chain(
         grpc_host
     )
 
-    # if chat_model_type == 'openai':
-    #     model = ChatOpenAI(openai_api_key=api_key)
-    # elif chat_model_type == 'ollama-external':
-    #     model = OllamaExternalChatModel(OLLAMA_URL)
+    if model_type == 'opeani':
+        llm = ChatOpenAI(model=model,openai_api_key=api_key)
 
-    model = ChatOpenAI(openai_api_key=api_key)
-        
     prompt = ChatPromptTemplate.from_template(
         f"{json_config["prompt"]}"
     )
 
     parser = StrOutputParser()
 
-    chain = prompt | model | parser
+    chain = prompt | llm | parser
 
     for chunk in chain.stream({"question": question, "context": documents}):
             yield json.dumps({"chunk": chunk, "type": "CHUNK"})
 
     yield json.dumps({"chunk": "", "type": "END"})
-
-
-# def get_chat_chain(
-#     searchQuery,
-#     range,
-#     afterKey,
-#     suggestKeyword,
-#     suggestionCategoryId,
-#     jwt,
-#     extra,
-#     sort,
-#     sortAfterKey,
-#     language,
-#     vectorIndices,
-#     virtualHost,
-#     searchText
-# ):
-#     configuration = get_llm_configuration(virtualHost)
-#     api_url = configuration["api_url"]
-#     api_key = configuration["api_key"]
-#     json_config = configuration["json_config"]
-#     chat_model_type = json_config["modelType"]
-
-#     documents = OpenSearchRetriever._get_relevant_documents(
-#         searchQuery,
-#         range,
-#         afterKey,
-#         suggestKeyword,
-#         suggestionCategoryId,
-#         virtualHost,
-#         jwt,
-#         extra,
-#         sort,
-#         sortAfterKey,
-#         language,
-#         vectorIndices,
-#     )
-
-#     if chat_model_type == 'openai':
-#         model = ChatOpenAI(openai_api_key=api_key)
-#     elif chat_model_type == 'ollama-external':
-#         model = OllamaExternalChatModel(OLLAMA_URL)
-
-#     prompt = ChatPromptTemplate.from_template(
-#         f"{json_config["prompt"]}"
-#     )
-
-#     parser = StrOutputParser()
-
-#     chain = prompt | model | parser
-
-#     for chunk in chain.stream({"question": searchText, "context": documents}):
-#         yield json.dumps({"chunk": chunk, "type": "CHUNK"})
-
-#     yield json.dumps({"chunk": "", "type": "END"})    
 
 
 def get_chat_chain(
@@ -148,7 +87,6 @@ def get_chat_chain(
     api_url = configuration["api_url"]
     api_key = configuration["api_key"]
     json_config = configuration["json_config"]
-    # chat_model_type = json_config["modelType"]
 
     documents = OpenSearchRetriever._get_relevant_documents(
         searchQuery,
@@ -167,22 +105,17 @@ def get_chat_chain(
         grpc_host
     )
 
-    model = ChatOpenAI(openai_api_key=api_key)
+    llm = ChatOpenAI(openai_api_key=api_key)
 
     prompt = ChatPromptTemplate.from_template(
         f"{json_config["prompt"]}"
     )
-    model = ChatOllama(model="llama3", base_url=OLLAMA_URL)
 
-    # prompt = ChatPromptTemplate.from_template(
-    #     f"{json_config["prompt"]}"
-    # )
-    
     parser = StrOutputParser()
 
-    chain = model | parser
+    chain = prompt | llm | parser
 
-    for chunk in chain.stream(searchText):
+    for chunk in chain.stream({"question": searchText, "context": documents}):
         yield json.dumps({"chunk": chunk, "type": "CHUNK"})
 
     for element in documents:
