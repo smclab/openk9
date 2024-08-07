e9ba45fb0bed7ad9e7f78d8e835b4a09e5e1f80e
lucac <luca.callocchia@gmail.com>
2024-08-07 09:50:01 +0200

    add ChatOllama model

--- app/rag/chain.py
+++ app/rag/chain.py
@@ -2,10 +2,13 @@ import json
 
 from langchain.prompts import ChatPromptTemplate
 from langchain_core.output_parsers import StrOutputParser
+from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
 
 from app.external_services.grpc.grpc_client import get_llm_configuration
 from app.rag.retriever import OpenSearchRetriever
+
+OLLAMA_URL = 'http://35.207.116.4:11434'
 
 
 def get_chain(
@@ -29,6 +32,7 @@ def get_chain(
     api_url = configuration["api_url"]
     api_key = configuration["api_key"]
     json_config = configuration["json_config"]
+    chat_model_type = json_config["modelType"]
 
     documents = OpenSearchRetriever._get_relevant_documents(
         searchQuery,
@@ -47,8 +51,13 @@ def get_chain(
         grpc_host
     )
 
-    model = ChatOpenAI(openai_api_key=api_key)
+    # if chat_model_type == 'openai':
+    #     model = ChatOpenAI(openai_api_key=api_key)
+    # elif chat_model_type == 'ollama-external':
+    #     model = OllamaExternalChatModel(OLLAMA_URL)
 
+    model = ChatOpenAI(openai_api_key=api_key)
+        
     prompt = ChatPromptTemplate.from_template(
         f"{json_config["prompt"]}"
     )
@@ -61,6 +70,61 @@ def get_chain(
             yield json.dumps({"chunk": chunk, "type": "CHUNK"})
 
     yield json.dumps({"chunk": "", "type": "END"})
+
+
+# def get_chat_chain(
+#     searchQuery,
+#     range,
+#     afterKey,
+#     suggestKeyword,
+#     suggestionCategoryId,
+#     jwt,
+#     extra,
+#     sort,
+#     sortAfterKey,
+#     language,
+#     vectorIndices,
+#     virtualHost,
+#     searchText
+# ):
+#     configuration = get_llm_configuration(virtualHost)
+#     api_url = configuration["api_url"]
+#     api_key = configuration["api_key"]
+#     json_config = configuration["json_config"]
+#     chat_model_type = json_config["modelType"]
+
+#     documents = OpenSearchRetriever._get_relevant_documents(
+#         searchQuery,
+#         range,
+#         afterKey,
+#         suggestKeyword,
+#         suggestionCategoryId,
+#         virtualHost,
+#         jwt,
+#         extra,
+#         sort,
+#         sortAfterKey,
+#         language,
+#         vectorIndices,
+#     )
+
+#     if chat_model_type == 'openai':
+#         model = ChatOpenAI(openai_api_key=api_key)
+#     elif chat_model_type == 'ollama-external':
+#         model = OllamaExternalChatModel(OLLAMA_URL)
+
+#     prompt = ChatPromptTemplate.from_template(
+#         f"{json_config["prompt"]}"
+#     )
+
+#     parser = StrOutputParser()
+
+#     chain = prompt | model | parser
+
+#     for chunk in chain.stream({"question": searchText, "context": documents}):
+#         yield json.dumps({"chunk": chunk, "type": "CHUNK"})
+
+#     yield json.dumps({"chunk": "", "type": "END"})    
 
 
 def get_chat_chain(
@@ -84,6 +148,7 @@ def get_chat_chain(
     api_url = configuration["api_url"]
     api_key = configuration["api_key"]
     json_config = configuration["json_config"]
+    # chat_model_type = json_config["modelType"]
 
     documents = OpenSearchRetriever._get_relevant_documents(
         searchQuery,
@@ -107,12 +172,17 @@ def get_chat_chain(
     prompt = ChatPromptTemplate.from_template(
         f"{json_config["prompt"]}"
     )
+    model = ChatOllama(model="llama3", base_url=OLLAMA_URL)
 
+    # prompt = ChatPromptTemplate.from_template(
+    #     f"{json_config["prompt"]}"
+    # )
+    
     parser = StrOutputParser()
 
-    chain = prompt | model | parser
+    chain = model | parser
 
-    for chunk in chain.stream({"question": searchText, "context": documents}):
+    for chunk in chain.stream(searchText):
         yield json.dumps({"chunk": chunk, "type": "CHUNK"})
 
     for element in documents:
