2590684d14bd5cdd162a4e3ec4a8b9f44707ad33
lucac <luca.callocchia@gmail.com>
2024-08-26 14:25:32 +0200

    fix #15: - keycloak info by grpc

--- app/utils/keycloak.py
+++ app/utils/keycloak.py
@@ -1,25 +1,27 @@
 import requests
 from keycloak import KeycloakOpenID
 
+from app.external_services.grpc.grpc_client import get_tenant_manager_configuration
+
 
 class Keycloak:
 
-    def verify_token(keykloak_info_api_url, token) -> dict:
+    def verify_token(grpc_host, virtualHost, token) -> dict:
         try:
-            keykloak_request = requests.get(keykloak_info_api_url)
-            keykloak_info = keykloak_request.json()
+            keycloak_info = get_tenant_manager_configuration(grpc_host, virtualHost)
 
-            keykloak_url = keykloak_info["url"]
-            keykloak_client_id = keykloak_info["clientId"]
-            keykloak_realm = keykloak_info["realm"]
+            keycloak_url = keycloak_info["server_url"]
+            keycloak_client_id = keycloak_info["client_id"]
+            keycloak_realm = keycloak_info["realm_name"]
 
             keycloak_openid = KeycloakOpenID(
-                server_url=keykloak_url,
-                client_id=keykloak_client_id,
-                realm_name=keykloak_realm,
+                server_url=keycloak_url,
+                client_id=keycloak_client_id,
+                realm_name=keycloak_realm,
             )
 
             userinfo = keycloak_openid.userinfo(token=token)
+
             return userinfo
         except:
             return {}
