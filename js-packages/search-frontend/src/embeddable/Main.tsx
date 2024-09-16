13a12cd1ef356162f2f912e910c27614e5fc7579
Lorenzo Venneri <lorenzov96@live.it>
2024-09-16 15:55:14 +0200

    fix loading search

--- js-packages/search-frontend/src/embeddable/Main.tsx
+++ js-packages/search-frontend/src/embeddable/Main.tsx
@@ -246,29 +246,26 @@ export function Main({
       )}
       {renderPortal(
         <I18nextProvider i18n={i18next}>
-          {isSearchLoading ? null : (
-            <Search
-              configuration={configuration}
-              spans={spans}
-              selectionsState={selectionsState}
-              selectionsDispatch={selectionsDispatch}
-              showSyntax={isQueryAnalysisComplete}
-              btnSearch={configuration.searchConfigurable?.btnSearch ?? false}
-              htmlKey={configuration.searchConfigurable?.htmlKey}
-              viewColor={configuration.showSyntax}
-              messageSearchIsVisible={
-                configuration?.searchConfigurable?.messageSearchIsVisible ??
-                true
-              }
-              customMessageSearch={
-                configuration?.searchConfigurable?.customMessageSearch
-              }
-              actionOnClick={configuration?.searchConfigurable?.actionOnClick}
-              callbackClickSearch={
-                configuration?.searchConfigurable?.callbackClickSearch
-              }
-            />
-          )}
+          <Search
+            configuration={configuration}
+            spans={spans}
+            selectionsState={selectionsState}
+            selectionsDispatch={selectionsDispatch}
+            showSyntax={isQueryAnalysisComplete}
+            btnSearch={configuration.searchConfigurable?.btnSearch ?? false}
+            htmlKey={configuration.searchConfigurable?.htmlKey}
+            viewColor={configuration.showSyntax}
+            messageSearchIsVisible={
+              configuration?.searchConfigurable?.messageSearchIsVisible ?? true
+            }
+            customMessageSearch={
+              configuration?.searchConfigurable?.customMessageSearch
+            }
+            actionOnClick={configuration?.searchConfigurable?.actionOnClick}
+            callbackClickSearch={
+              configuration?.searchConfigurable?.callbackClickSearch
+            }
+          />
         </I18nextProvider>,
         configuration.searchConfigurable
           ? configuration.searchConfigurable.element
