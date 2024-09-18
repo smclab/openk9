2593953079c35f5cb5d9f48989c23a424f4f637b
lucac <luca.callocchia@gmail.com>
2024-09-18 18:00:26 +0200

    fix prompt string

--- app/rag/chain.py
+++ app/rag/chain.py
@@ -6,11 +6,11 @@ from langchain_ollama import ChatOllama
 from langchain_openai import ChatOpenAI
 
 from app.external_services.grpc.grpc_client import get_llm_configuration
-from app.rag.retriever import OpenSearchRetriever
 from app.rag.custom_hugging_face_model import CustomChatHuggingFaceModel
+from app.rag.retriever import OpenSearchRetriever
 
-DEFAULT_MODEL_TYPE = 'openai'
-DEFAULT_MODEL = 'gpt-3.5-turbo'
+DEFAULT_MODEL_TYPE = "openai"
+DEFAULT_MODEL = "gpt-3.5-turbo"
 
 
 def get_chain(
@@ -28,12 +28,16 @@ def get_chain(
     virtualHost,
     question,
     opensearch_host,
-    grpc_host
+    grpc_host,
 ):
     configuration = get_llm_configuration(grpc_host, virtualHost)
     api_url = configuration["api_url"]
     api_key = configuration["api_key"]
-    model_type = configuration["model_type"] if configuration["model_type"]  else DEFAULT_MODEL_TYPE
+    model_type = (
+        configuration["model_type"]
+        if configuration["model_type"]
+        else DEFAULT_MODEL_TYPE
+    )
     model = configuration["model"] if configuration["model"] else DEFAULT_MODEL
 
     documents = OpenSearchRetriever._get_relevant_documents(
@@ -50,26 +54,24 @@ def get_chain(
         language,
         vectorIndices,
         opensearch_host,
-        grpc_host
+        grpc_host,
     )
 
-    if model_type == 'openai':
+    if model_type == "openai":
         llm = ChatOpenAI(model=model, openai_api_key=api_key)
-    elif model_type == 'ollama':
+    elif model_type == "ollama":
         llm = ChatOllama(model=model, base_url=api_url)
-    elif model_type == 'hugging-face-custom':
+    elif model_type == "hugging-face-custom":
         llm = CustomChatHuggingFaceModel(base_url=api_url)
 
-    prompt = ChatPromptTemplate.from_template(
-        f"{configuration["prompt"]}"
-    )
+    prompt = ChatPromptTemplate.from_template(configuration["prompt"])
 
     parser = StrOutputParser()
 
     chain = prompt | llm | parser
 
     for chunk in chain.stream({"question": question, "context": documents}):
-            yield json.dumps({"chunk": chunk, "type": "CHUNK"})
+        yield json.dumps({"chunk": chunk, "type": "CHUNK"})
 
     yield json.dumps({"chunk": "", "type": "END"})
 
@@ -89,12 +91,16 @@ def get_chat_chain(
     virtualHost,
     searchText,
     opensearch_host,
-    grpc_host
+    grpc_host,
 ):
     configuration = get_llm_configuration(grpc_host, virtualHost)
     api_url = configuration["api_url"]
     api_key = configuration["api_key"]
-    model_type = configuration["model_type"] if configuration["model_type"]  else DEFAULT_MODEL_TYPE
+    model_type = (
+        configuration["model_type"]
+        if configuration["model_type"]
+        else DEFAULT_MODEL_TYPE
+    )
     model = configuration["model"] if configuration["model"] else DEFAULT_MODEL
 
     documents = OpenSearchRetriever._get_relevant_documents(
@@ -111,19 +117,17 @@ def get_chat_chain(
         language,
         vectorIndices,
         opensearch_host,
-        grpc_host
+        grpc_host,
     )
 
-    if model_type == 'openai':
+    if model_type == "openai":
         llm = ChatOpenAI(model=model, openai_api_key=api_key)
-    elif model_type == 'ollama':
+    elif model_type == "ollama":
         llm = ChatOllama(model=model, base_url=api_url)
-    elif model_type == 'hugging-face-custom':
+    elif model_type == "hugging-face-custom":
         llm = CustomChatHuggingFaceModel(base_url=api_url)
 
-    prompt = ChatPromptTemplate.from_template(
-        f"{configuration["prompt"]}"
-    )
+    prompt = ChatPromptTemplate.from_template(configuration["prompt"])
 
     parser = StrOutputParser()
 
