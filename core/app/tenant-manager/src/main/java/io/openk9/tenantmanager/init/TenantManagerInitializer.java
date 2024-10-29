72e2412719f664ea67826f0aa78075763835f267
Mirko Zizzari <mirko.zizzari@smc.it>
2024-10-29 15:56:39 +0100

    issue #599: emits and subscribes all with workerPool

--- core/app/tenant-manager/src/main/java/io/openk9/tenantmanager/init/TenantManagerInitializer.java
+++ core/app/tenant-manager/src/main/java/io/openk9/tenantmanager/init/TenantManagerInitializer.java
@@ -22,6 +22,7 @@ import io.openk9.tenantmanager.pipe.liquibase.validate.LiquibaseValidatorActorSy
 import io.openk9.tenantmanager.pipe.liquibase.validate.util.Params;
 import io.openk9.tenantmanager.service.DatasourceLiquibaseService;
 import io.openk9.tenantmanager.service.TenantService;
+import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.infrastructure.Infrastructure;
 import io.vertx.mutiny.core.eventbus.EventBus;
 import jakarta.enterprise.event.Observes;
@@ -56,10 +57,11 @@ public class TenantManagerInitializer {
 
 	public void onStart(@Observes Startup startup) {
 
-		tenantService.findAllSchemaNameAndLiquibaseSchemaName()
-			.runSubscriptionOn(Infrastructure.getDefaultWorkerPool())
+		Uni.createFrom()
+			.voidItem()
 			.emitOn(Infrastructure.getDefaultWorkerPool())
-			.flatMap((schemas) -> {
+			.flatMap((nothing) -> tenantService.findAllSchemaNameAndLiquibaseSchemaName()
+				.flatMap((schemas) -> {
 					LinkedList<Params> schemaParamList = new LinkedList<>();
 
 					for (SchemaTuple schema : schemas) {
@@ -77,9 +79,10 @@ public class TenantManagerInitializer {
 						);
 					}
 
-				return liquibaseValidatorActorSystem.validateSchemas(schemaParamList);
-				}
+					return liquibaseValidatorActorSystem.validateSchemas(schemaParamList);
+				})
 			)
+			.runSubscriptionOn(Infrastructure.getDefaultWorkerPool())
 			.subscribe()
 			.with(
 				nothing -> {
