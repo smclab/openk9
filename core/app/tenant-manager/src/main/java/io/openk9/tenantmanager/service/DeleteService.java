1075111f97843972bf1910e3d49bf0f331b86f34
Mirko Zizzari <mirko.zizzari@smc.it>
2024-11-20 18:24:32 +0100

    issue #599: executes blocking on vertx

--- core/app/tenant-manager/src/main/java/io/openk9/tenantmanager/service/DeleteService.java
+++ core/app/tenant-manager/src/main/java/io/openk9/tenantmanager/service/DeleteService.java
@@ -21,6 +21,7 @@ import io.openk9.tenantmanager.model.Tenant;
 import io.quarkus.vertx.ConsumeEvent;
 import io.smallrye.common.annotation.Blocking;
 import io.smallrye.mutiny.Uni;
+import io.vertx.mutiny.core.Vertx;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.control.ActivateRequestContext;
 import jakarta.inject.Inject;
@@ -40,11 +41,13 @@ public class DeleteService {
 	DatasourceLiquibaseService datasourceLiquibaseService;
 	@Inject
 	TenantService tenantService;
+	@Inject
+	Vertx vertx;
 
 	@ConsumeEvent(FIND_TENANT_BY_VIRTUAL_HOST)
-	@Blocking
 	public Uni<Tenant> findTenant(String virtualHost) {
-		return tenantService.findTenantByVirtualHost(virtualHost);
+		return vertx.executeBlocking(
+			tenantService.findTenantByVirtualHost(virtualHost));
 	}
 
 	@ActivateRequestContext
@@ -67,10 +70,9 @@ public class DeleteService {
 	}
 
 	@ConsumeEvent(DELETE_TENANT)
-	@Blocking
 	public Uni<Void> deleteTenant(long tenantId) {
 
-		return tenantService.deleteTenant(tenantId);
+		return vertx.executeBlocking(tenantService.deleteTenant(tenantId));
 
 	}
 
