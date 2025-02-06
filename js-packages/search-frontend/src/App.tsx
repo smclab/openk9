54236f49a77b3bf9306ca6b61a83fcc1ce6ed088
Lorenzo Venneri <lorenzov96@live.it>
2025-02-06 14:42:21 +0100

    Issue #1131: removed border to detail box

--- js-packages/search-frontend/src/App.tsx
+++ js-packages/search-frontend/src/App.tsx
@@ -710,7 +710,6 @@ export function App() {
           );
           height: auto;
           border-radius: 8px;
-          border: 1px solid var(--openk9-embeddable-search--border-color);
           margin-top: ${searchText !== undefined ? "20px" : "unset"};
 
           @media (max-width: 480px) {
