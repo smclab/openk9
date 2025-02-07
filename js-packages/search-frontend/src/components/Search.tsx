91a4548dfd723c287726dffe1de989f47da3e002
Lorenzo Venneri <lorenzov96@live.it>
2025-02-07 11:12:48 +0100

    Issue #1085: added callback

--- js-packages/search-frontend/src/components/Search.tsx
+++ js-packages/search-frontend/src/components/Search.tsx
@@ -433,6 +433,7 @@ export function Search({
                 inputRef &&
                   inputRef.current &&
                   search(inputRef?.current?.value || "");
+                if (callbackClickSearch) callbackClickSearch();
               }}
             >
               <svg
