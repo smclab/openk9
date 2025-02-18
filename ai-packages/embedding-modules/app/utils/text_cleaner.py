181ca689c2c91a6563da46994186b2cdca5dae39
Luca Callocchia <luca.callocchia@smc.it>
2025-02-18 15:31:10 +0100

    issue #1140: fix variable name

--- ai-packages/embedding-modules/app/utils/text_cleaner.py
+++ ai-packages/embedding-modules/app/utils/text_cleaner.py
@@ -17,18 +17,20 @@ def clean_text(raw_text):
     str: The cleaned text with unwanted elements removed and formatting corrected.
     """
     # 1. Remove HTML tags like <br>, <p>, etc.
-    clean_text = re.sub(r"<[^>]+>", "", raw_text)
+    cleaned_text = re.sub(r"<[^>]+>", "", raw_text)
 
     # 2. Correct word breaks like "Organiz- \nzation" -> "Organization"
-    clean_text = re.sub(r"-\s*\n", "", clean_text)
+    cleaned_text = re.sub(r"-\s*\n", "", cleaned_text)
 
     # 3. Remove sequences of long dots (more than 3) like "..........."
-    clean_text = re.sub(r"\.{4,}", "", clean_text)
+    cleaned_text = re.sub(r"\.{4,}", "", cleaned_text)
 
     # 4. Remove unwanted characters while keeping numbers with / (e.g., 3/27)
-    clean_text = re.sub(r"[^a-zA-Z0-9\s.,;:?!()/%-]|(?<!\d)/|/(?!\d)", "", clean_text)
+    cleaned_text = re.sub(
+        r"[^a-zA-Z0-9\s.,;:?!()/%-]|(?<!\d)/|/(?!\d)", "", cleaned_text
+    )
 
     # 5. Replace newlines or multiple spaces with a single space
-    clean_text = re.sub(r"\s+", " ", clean_text)
+    cleaned_text = re.sub(r"\s+", " ", cleaned_text)
 
-    return clean_text.strip()
+    return cleaned_text.strip()
