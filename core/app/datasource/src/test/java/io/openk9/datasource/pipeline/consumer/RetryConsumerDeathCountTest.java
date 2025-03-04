8efe947bcfded9fc9c67d19062bb5ef6bc3ea1f0
Mirko Zizzari <mirko.zizzari@smc.it>
2025-03-04 16:00:36 +0100

    issue #1247: implements incrementDeathCount and tests it

--- core/app/datasource/src/test/java/io/openk9/datasource/pipeline/consumer/RetryConsumerDeathCountTest.java
+++ core/app/datasource/src/test/java/io/openk9/datasource/pipeline/consumer/RetryConsumerDeathCountTest.java
@@ -15,7 +15,7 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-package io.openk9.datasource;
+package io.openk9.datasource.pipeline.consumer;
 
 import java.io.IOException;
 import java.util.Map;
@@ -33,7 +33,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
-public class QueueTest {
+public class RetryConsumerDeathCountTest {
 
 	private static final String AMQ_TOPIC_EXCHANGE = "amq.topic";
 	private static final String X_DEAD_LETTER_EXCHANGE = "x-dead-letter-exchange";
@@ -42,10 +42,12 @@ public class QueueTest {
 	private static final String RETRY_QUEUE = "RETRY_QUEUE";
 	private static final String ERROR_QUEUE = "ERROR_QUEUE";
 	private static final byte[] MESSAGE_BODY = "Hello World!".getBytes();
-	private final Logger log = Logger.getLogger(QueueTest.class);
+	private static final Logger log = Logger.getLogger(RetryConsumerDeathCountTest.class);
+
 	@Inject
 	RabbitMQClient rabbitMQClient;
 
+	private Channel channel;
 	private Connection connection;
 
 	@BeforeEach
@@ -53,6 +55,8 @@ public class QueueTest {
 
 		try {
 			this.connection = rabbitMQClient.connect();
+			this.channel = connection.createChannel();
+			bindQueues(channel);
 		}
 		catch (Exception e) {
 			throw new RuntimeException(e);
@@ -61,140 +65,102 @@ public class QueueTest {
 	}
 
 	@Test
-	void should_consume_without_error() throws Exception {
-
-		try (Channel channel = this.connection.createChannel()) {
-
-			bindQueues(channel);
-
-			// publish a message
-			channel.basicPublish(
-				AMQ_TOPIC_EXCHANGE,
-				MAIN_QUEUE,
-				MessageProperties.PERSISTENT_BASIC,
-				MESSAGE_BODY
-			);
-
-			var latch = new CountDownLatch(1);
-
-			// consume message
-			channel.basicConsume(
-				MAIN_QUEUE,
-				(consumerTag, message) -> {
-					log.infof("Received: %s", message.getBody());
-					latch.countDown();
-				},
-				(consumerTag, sig) -> {
-					log.info("consumer closed");
-					latch.countDown();
-				}
-			);
-
-			latch.await();
-		}
-
-	}
-
-	@Test
 	void should_move_to_error() throws Exception {
 
-		try (Channel channel = this.connection.createChannel()) {
+		// publish a message
+		channel.basicPublish(
+			AMQ_TOPIC_EXCHANGE,
+			MAIN_QUEUE,
+			MessageProperties.PERSISTENT_BASIC,
+			MESSAGE_BODY
+		);
 
-			bindQueues(channel);
+		var latch = new CountDownLatch(1);
 
-			// publish a message
-			channel.basicPublish(
-				AMQ_TOPIC_EXCHANGE,
-				MAIN_QUEUE,
-				MessageProperties.PERSISTENT_BASIC,
-				MESSAGE_BODY
-			);
+		// consume message
+		channel.basicConsume(
+			MAIN_QUEUE,
+			(consumerTag, message) -> {
+				var deliveryTag = message.getEnvelope().getDeliveryTag();
+				log.infof("MainQueue received: %s", message.getBody());
 
-			var latch = new CountDownLatch(1);
+				channel.basicNack(
+					deliveryTag,
+					false,
+					false
+				);
 
-			// consume message
-			channel.basicConsume(
-				MAIN_QUEUE,
-				(consumerTag, message) -> {
-					var deliveryTag = message.getEnvelope().getDeliveryTag();
-					log.infof("MainQueue received: %s", message.getBody());
+				log.infof("nack %s", deliveryTag);
+			},
+			(consumerTag, sig) -> log.info("consumer closed")
+		);
 
-					channel.basicNack(
-						deliveryTag,
-						false,
-						false
+		// retry message three times
+		channel.basicConsume(
+			RETRY_QUEUE,
+			(consumerTag, message) -> {
+				var deliveryTag = message.getEnvelope().getDeliveryTag();
+				log.infof("RetryQueue received: %s", message.getBody());
+				var properties = message.getProperties();
+				var deathCount = RetryConsumer.incrementDeathCount(properties);
+
+				if (deathCount < 3) {
+
+					channel.basicPublish(
+						AMQ_TOPIC_EXCHANGE,
+						MAIN_QUEUE,
+						properties,
+						message.getBody()
 					);
 
-					log.infof("nack %s", deliveryTag);
-				},
-				(consumerTag, sig) -> log.info("consumer closed")
-			);
+					channel.basicAck(
+						message.getEnvelope().getDeliveryTag(), false);
 
-			// retry message
-			channel.basicConsume(
-				RETRY_QUEUE,
-				(consumerTag, message) -> {
-					var deliveryTag = message.getEnvelope().getDeliveryTag();
-					log.infof("RetryQueue received: %s", message.getBody());
-					var properties = message.getProperties();
-					var headers = properties.getHeaders();
-					int count = (int) headers.getOrDefault("retry", 0);
-
-					if (count < 3) {
-						headers.put("retry", ++count);
-
-						channel.basicPublish(
-							AMQ_TOPIC_EXCHANGE,
-							MAIN_QUEUE,
-							properties,
-							message.getBody()
-						);
-
-						channel.basicAck(
-							message.getEnvelope().getDeliveryTag(), false);
-
-						log.infof("re-publish %s", deliveryTag);
-					}
-					else {
-
-						channel.basicNack(
-							message.getEnvelope().getDeliveryTag(), false, false);
-
-						log.infof("dlq error %s", deliveryTag);
-					}
-				},
-				(consumerTag, sig) -> log.info("consumer closed")
-			);
-
-			// consume with ack on error
-			channel.basicConsume(
-				ERROR_QUEUE,
-				(consumerTag, message) -> {
-					var deliveryTag = message.getEnvelope().getDeliveryTag();
-					log.infof("ErrorQueue received: %s", message.getBody());
-
-					latch.countDown();
-
-					channel.basicAck(deliveryTag, false);
-
-					log.infof("ack %s", deliveryTag);
-				},
-				(consumerTag, sig) -> {
-					log.info("consumer closed");
-					latch.countDown();
+					log.infof("re-publish %s", deliveryTag);
 				}
-			);
+				else {
 
-			latch.await();
-		}
+					channel.basicNack(
+						message.getEnvelope().getDeliveryTag(), false, false);
+
+					log.infof("dlq error %s", deliveryTag);
+				}
+			},
+			(consumerTag, sig) -> log.info("consumer closed")
+		);
+
+		// consume with ack on error
+		channel.basicConsume(
+			ERROR_QUEUE,
+			(consumerTag, message) -> {
+				var deliveryTag = message.getEnvelope().getDeliveryTag();
+				log.infof("ErrorQueue received: %s", message.getBody());
+
+				latch.countDown();
+
+				channel.basicAck(deliveryTag, false);
+
+				log.infof("ack %s", deliveryTag);
+			},
+			(consumerTag, sig) -> {
+				log.info("consumer closed");
+				latch.countDown();
+			}
+		);
+
+		latch.await();
 
 	}
 
 	@AfterEach
 	void tearDown() {
-		if (this.connection != null) {
+		if (connection != null) {
 			try {
+				unbindQueues(channel);
+				channel.close();
 				connection.close();
+				channel = null;
+				connection = null;
 			}
 			catch (Exception e) {
 				log.error("Channel cannot be close");
@@ -231,14 +197,6 @@ public class QueueTest {
 		channel.queueBind(MAIN_QUEUE, AMQ_TOPIC_EXCHANGE, MAIN_QUEUE);
 	}
 
-	private static void bindQueues(Channel channel) throws IOException {
-		bindMainQueue(channel);
-
-		bindRetryQueue(channel);
-
-		bindErrorQueue(channel);
-	}
-
 	private static void bindRetryQueue(Channel channel) throws IOException {
 		// bind retry queue
 		channel.queueDeclare(
@@ -253,6 +211,26 @@ public class QueueTest {
 		);
 
 		channel.queueBind(RETRY_QUEUE, AMQ_TOPIC_EXCHANGE, RETRY_QUEUE);
+	}
+
+	private static void bindQueues(Channel channel) throws IOException {
+		bindMainQueue(channel);
+
+		bindRetryQueue(channel);
+
+		bindErrorQueue(channel);
+	}
+
+	private static void unbindQueues(Channel channel) throws IOException {
+
+		channel.queueUnbind(MAIN_QUEUE, AMQ_TOPIC_EXCHANGE, MAIN_QUEUE);
+		channel.queueUnbind(RETRY_QUEUE, AMQ_TOPIC_EXCHANGE, RETRY_QUEUE);
+		channel.queueUnbind(ERROR_QUEUE, AMQ_TOPIC_EXCHANGE, ERROR_QUEUE);
+
+		channel.queueDelete(MAIN_QUEUE);
+		channel.queueDelete(RETRY_QUEUE);
+		channel.queueDelete(ERROR_QUEUE);
+
 	}
 
 }
