d78e699c9c176ec1f1b7eec9abc57860d22a0f2c
Michele Bastianelli <michele.bastianelli@smc.it>
2025-03-24 11:47:39 +0100

    issue #1278: [test] adds test for GraphQL APIs of update/patch for the EmbeddingModel entity

--- core/app/datasource/src/test/java/io/openk9/datasource/graphql/EmbeddingModelGraphqlTest.java
+++ core/app/datasource/src/test/java/io/openk9/datasource/graphql/EmbeddingModelGraphqlTest.java
@@ -10,6 +10,8 @@ import io.smallrye.graphql.client.dynamic.api.DynamicGraphQLClient;
 import jakarta.inject.Inject;
 import org.hibernate.reactive.mutiny.Mutiny;
 import org.jboss.logging.Logger;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.concurrent.ExecutionException;
@@ -29,8 +31,15 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 @QuarkusTest
 public class EmbeddingModelGraphqlTest {
 
+	private static final String API_URL = "apiUrl";
+	private static final String API_KEY = "apiKey";
+	private static final String EMBEDDING_MODEL = "embeddingModel";
+	private static final String EMBEDDING_MODEL_DTO = "embeddingModelDTO";
+	private static final String EMBEDDING_MODEL_LOCAL = "embedding-model.local";
+	private static final String EMBEDDING_MODEL_LOCAL_UPDATED = "embedding-model.local_updated";
 	private static final String ENTITY_NAME_PREFIX = "EmbeddingModelGraphqlTest - ";
 	private static final String EMBEDDING_MODEL_ONE_NAME = ENTITY_NAME_PREFIX + "Embedding model 1 ";
+	private static final String EMBEDDING_MODEL_TWO_NAME = ENTITY_NAME_PREFIX + "Embedding model 2 ";
 	private static final String ENTITY = "entity";
 	private static final String FIELD = "field";
 	private static final String FIELD_VALIDATORS = "fieldValidators";
@@ -38,9 +47,17 @@ public class EmbeddingModelGraphqlTest {
 	private static final String MESSAGE = "message";
 	private static final String MODEL = "model";
 	private static final String MODEL_VALUE = "model";
+	private static final String MODEL_VALUE_UPDATED = "model_updated";
 	private static final String NAME = "name";
+	private static final String PATCH = "patch";
+	private static final String SECRET_KEY = "secret-key";
+	private static final String SECRET_KEY_UPDATED = "secret-key_updated";
 	private static final String TYPE = "type";
 	private static final String TYPE_VALUE = "type";
+	private static final String TYPE_VALUE_UPDATED = "type_updated";
+	private static final String VECTOR_SIZE = "vectorSize";
+	private static final int VECTOR_SIZE_DEFAULT_VALUE = 1500;
+	private static final int VECTOR_SIZE_VALUE_UPDATED = 3000;
 	private static final Logger log = Logger.getLogger(EmbeddingModelGraphqlTest.class);
 
 	@Inject
@@ -53,24 +70,175 @@ public class EmbeddingModelGraphqlTest {
 	@Inject
 	Mutiny.SessionFactory sessionFactory;
 
+	@BeforeEach
+	void setup() {
+		createEmbeddingModelTwo();
+	}
+
 	@Test
-	void should_get_embedding_model_one() throws ExecutionException, InterruptedException {
+	void should_create_embedding_model_one() throws ExecutionException, InterruptedException {
 
 		var mutation = document(
 			operation(
 				OperationType.MUTATION,
 				field(
-					"embeddingModel",
+					EMBEDDING_MODEL,
 					args(
 						arg(
-							"embeddingModelDTO",
+							EMBEDDING_MODEL_DTO,
 							inputObject(
 								prop(NAME, EMBEDDING_MODEL_ONE_NAME),
-								prop("apiUrl", "embedding-model.local"),
-								prop("apiKey", "secret-key"),
-								prop("vectorSize", 1500),
+								prop(API_URL, EMBEDDING_MODEL_LOCAL),
+								prop(API_KEY, SECRET_KEY),
+								prop(VECTOR_SIZE, VECTOR_SIZE_DEFAULT_VALUE),
 								prop(TYPE, TYPE_VALUE),
 								prop(MODEL, MODEL_VALUE)
+							)
+						)
+					),
+					field(ENTITY,
+						field(ID),
+						field(NAME),
+						field(API_URL),
+						field(API_KEY),
+						field(VECTOR_SIZE),
+						field(TYPE),
+						field(MODEL)
+					),
+					field(FIELD_VALIDATORS,
+						field(FIELD),
+						field(MESSAGE)
+					)
+				)
+			)
+		);
+
+		var response = graphQLClient.executeSync(mutation);
+
+		log.info(String.format("Response: %s", response));
+
+		assertFalse(response.hasError());
+		assertTrue(response.hasData());
+
+		var embeddingModelResponse =
+			response.getData().getJsonObject(EMBEDDING_MODEL);
+
+		assertNotNull(embeddingModelResponse);
+
+		assertTrue(embeddingModelResponse.isNull(FIELD_VALIDATORS));
+
+		// check if the embeddingModel have the type and model fields
+		var embeddingModelOne = getEmbeddingModelOne();
+
+		assertEquals(TYPE_VALUE, embeddingModelOne.getType());
+		assertEquals(MODEL_VALUE, embeddingModelOne.getModel());
+
+		// remove the embeddingModelOne
+		removeEmbeddingModelOne();
+	}
+
+	@Test
+	void should_update_embedding_model_two() throws ExecutionException, InterruptedException {
+
+		var embeddingModelTwo = getEmbeddingModelTwo();
+
+		// check initial state
+		assertEquals(EMBEDDING_MODEL_LOCAL, embeddingModelTwo.getApiUrl());
+		assertEquals(SECRET_KEY, embeddingModelTwo.getApiKey());
+		assertEquals(VECTOR_SIZE_DEFAULT_VALUE, embeddingModelTwo.getVectorSize());
+		assertEquals(TYPE_VALUE, embeddingModelTwo.getType());
+		assertEquals(MODEL_VALUE, embeddingModelTwo.getModel());
+
+		var mutation = document(
+			operation(
+				OperationType.MUTATION,
+				field(
+					EMBEDDING_MODEL,
+					args(
+						arg(ID, embeddingModelTwo.getId()),
+						arg(PATCH, false),
+						arg(
+							EMBEDDING_MODEL_DTO,
+							inputObject(
+								prop(NAME, EMBEDDING_MODEL_TWO_NAME),
+								prop(API_URL, EMBEDDING_MODEL_LOCAL_UPDATED),
+								prop(API_KEY, SECRET_KEY_UPDATED),
+								prop(VECTOR_SIZE, VECTOR_SIZE_VALUE_UPDATED),
+								prop(TYPE, TYPE_VALUE_UPDATED),
+								prop(MODEL, MODEL_VALUE_UPDATED)
+							)
+						)
+					),
+					field(ENTITY,
+						field(ID),
+						field(NAME),
+						field(API_URL),
+						field(API_KEY),
+						field(VECTOR_SIZE),
+						field(TYPE),
+						field(MODEL)
+					),
+					field(FIELD_VALIDATORS,
+						field(FIELD),
+						field(MESSAGE)
+					)
+				)
+			)
+		);
+
+		var response = graphQLClient.executeSync(mutation);
+
+		log.info(String.format("Response: %s", response));
+
+		assertFalse(response.hasError());
+		assertTrue(response.hasData());
+
+		var embeddingModelResponse =
+			response.getData().getJsonObject(EMBEDDING_MODEL);
+
+		assertNotNull(embeddingModelResponse);
+
+		assertTrue(embeddingModelResponse.isNull(FIELD_VALIDATORS));
+
+		// check if the embeddingModel has been correctly updated
+		var embeddingModelUpdated = getEmbeddingModelTwo();
+
+		assertEquals(EMBEDDING_MODEL_LOCAL_UPDATED, embeddingModelUpdated.getApiUrl());
+		assertEquals(SECRET_KEY_UPDATED, embeddingModelUpdated.getApiKey());
+		assertEquals(VECTOR_SIZE_VALUE_UPDATED, embeddingModelUpdated.getVectorSize());
+		assertEquals(TYPE_VALUE_UPDATED, embeddingModelUpdated.getType());
+		assertEquals(MODEL_VALUE_UPDATED, embeddingModelUpdated.getModel());
+	}
+
+	@Test
+	void should_patched_embedding_model_two() throws ExecutionException, InterruptedException {
+
+		var embeddingModelTwo = getEmbeddingModelTwo();
+
+		// check initial state
+		assertEquals(EMBEDDING_MODEL_LOCAL, embeddingModelTwo.getApiUrl());
+		assertEquals(SECRET_KEY, embeddingModelTwo.getApiKey());
+		assertEquals(VECTOR_SIZE_DEFAULT_VALUE, embeddingModelTwo.getVectorSize());
+		assertEquals(TYPE_VALUE, embeddingModelTwo.getType());
+		assertEquals(MODEL_VALUE, embeddingModelTwo.getModel());
+
+		var mutation = document(
+			operation(
+				OperationType.MUTATION,
+				field(
+					EMBEDDING_MODEL,
+					args(
+						arg(ID, embeddingModelTwo.getId()),
+						arg(PATCH, true),
+						arg(
+							EMBEDDING_MODEL_DTO,
+							inputObject(
+								prop(NAME, EMBEDDING_MODEL_TWO_NAME),
+								prop(API_URL, EMBEDDING_MODEL_LOCAL_UPDATED),
+								prop(API_KEY, SECRET_KEY_UPDATED),
+								prop(VECTOR_SIZE, VECTOR_SIZE_VALUE_UPDATED),
+								prop(TYPE, TYPE_VALUE_UPDATED),
+								prop(MODEL, MODEL_VALUE_UPDATED)
 							)
 						)
 					),
@@ -95,21 +263,42 @@ public class EmbeddingModelGraphqlTest {
 		assertFalse(response.hasError());
 		assertTrue(response.hasData());
 
-		var datasource =
-			response.getData().getJsonObject("embeddingModel");
+		var embeddingModelResponse =
+			response.getData().getJsonObject(EMBEDDING_MODEL);
 
-		assertNotNull(datasource);
+		assertNotNull(embeddingModelResponse);
 
-		assertTrue(datasource.isNull(FIELD_VALIDATORS));
+		assertTrue(embeddingModelResponse.isNull(FIELD_VALIDATORS));
 
-		// check if the embeddingModel have the type and model fields
-		var embeddingModelOne = getEmbeddingModelOne();
+		// check if the embeddingModel has been correctly updated
+		var embeddingModelPatched = getEmbeddingModelTwo();
 
-		assertEquals(TYPE_VALUE, embeddingModelOne.getType());
-		assertEquals(MODEL_VALUE, embeddingModelOne.getModel());
+		assertEquals(EMBEDDING_MODEL_LOCAL_UPDATED, embeddingModelPatched.getApiUrl());
+		assertEquals(SECRET_KEY_UPDATED, embeddingModelPatched.getApiKey());
+		assertEquals(VECTOR_SIZE_VALUE_UPDATED, embeddingModelPatched.getVectorSize());
+		assertEquals(TYPE_VALUE_UPDATED, embeddingModelPatched.getType());
+		assertEquals(MODEL_VALUE_UPDATED, embeddingModelPatched.getModel());
+	}
 
-		// remove the embeddingModelOne
-		removeEmbeddingModelOne();
+	@AfterEach
+	void tearDown() {
+		removeEmbeddingModelTwo();
+	}
+
+	private void createEmbeddingModelTwo() {
+		var dto = EmbeddingModelDTO
+			.builder()
+			.name(EMBEDDING_MODEL_TWO_NAME)
+			.apiUrl(EMBEDDING_MODEL_LOCAL)
+			.apiKey(SECRET_KEY)
+			.vectorSize(VECTOR_SIZE_DEFAULT_VALUE)
+			.type(TYPE)
+			.model(MODEL)
+			.build();
+
+		embeddingModelService.create(dto)
+			.await()
+			.indefinitely();
 	}
 
 	private EmbeddingModel getEmbeddingModelOne() {
@@ -120,11 +309,29 @@ public class EmbeddingModelGraphqlTest {
 		.indefinitely();
 	}
 
+	private EmbeddingModel getEmbeddingModelTwo() {
+		return sessionFactory.withTransaction(
+				s -> embeddingModelService.findByName(s, EMBEDDING_MODEL_TWO_NAME)
+			)
+			.await()
+			.indefinitely();
+	}
+
 	private void removeEmbeddingModelOne() {
-		var embeddingModelOne = getEmbeddingModelOne();
+		var embeddingModel = getEmbeddingModelOne();
 
 		sessionFactory.withTransaction(
-				session -> embeddingModelService.deleteById(embeddingModelOne.getId())
+				session -> embeddingModelService.deleteById(embeddingModel.getId())
+			)
+			.await()
+			.indefinitely();
+	}
+
+	private void removeEmbeddingModelTwo() {
+		var embeddingModel = getEmbeddingModelTwo();
+
+		sessionFactory.withTransaction(
+				session -> embeddingModelService.deleteById(embeddingModel.getId())
 			)
 			.await()
 			.indefinitely();
