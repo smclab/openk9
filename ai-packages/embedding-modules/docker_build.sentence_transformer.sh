84cc5bb730db34e2d518543349c294a85567816e
Luca Callocchia <luca.callocchia@smc.it>
2025-03-26 15:17:52 +0100

    issue #1274: add build bash scripts to build docker images

---
+++ ai-packages/embedding-modules/docker_build.sentence_transformer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Get the absolute path to the Git repository root
+GIT_REPO_ROOT=$(git rev-parse --show-toplevel)
+
+# Read centralized python_modules_info.txt file
+CONFIG_FILE="$GIT_REPO_ROOT/python_modules_info.txt"
+if ! source "$CONFIG_FILE"; then
+    echo "Failed to source python_modules_info.txt"
+    exit 1
+fi
+
+# Check if required variables are set
+if [[ -z "$PYTHON_BASE_DOCKER_IMAGE" || -z "$OPENK9_VERSION" ]]; then
+    echo "PYTHON_BASE_DOCKER_IMAGE or OPENK9_VERSION is not set."
+    exit 1
+fi
+
+# Build the Docker image
+docker buildx build --build-arg PYTHON_BASE_DOCKER_IMAGE="$PYTHON_BASE_DOCKER_IMAGE" . -t openk9-embedding-module-sentence-transformer:"$OPENK9_VERSION" -f Dockerfile.sentence_transformer
