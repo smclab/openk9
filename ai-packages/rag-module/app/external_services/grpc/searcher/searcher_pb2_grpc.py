ab207f523fc0272bec20f2145d509a6a496d584e
Luca Callocchia <luca.callocchia@smc.it>
2025-04-15 16:30:50 +0200

    issue #1309: updated searcher.proto

--- ai-packages/rag-module/app/external_services/grpc/searcher/searcher_pb2_grpc.py
+++ ai-packages/rag-module/app/external_services/grpc/searcher/searcher_pb2_grpc.py
@@ -5,10 +5,8 @@ import warnings
 
 from app.external_services.grpc.searcher import searcher_pb2 as app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2
 
-GRPC_GENERATED_VERSION = '1.65.1'
+GRPC_GENERATED_VERSION = '1.71.0'
 GRPC_VERSION = grpc.__version__
-EXPECTED_ERROR_RELEASE = '1.66.0'
-SCHEDULED_RELEASE_DATE = 'August 6, 2024'
 _version_not_supported = False
 
 try:
@@ -18,15 +16,12 @@ except ImportError:
     _version_not_supported = True
 
 if _version_not_supported:
-    warnings.warn(
+    raise RuntimeError(
         f'The grpc package installed is at version {GRPC_VERSION},'
         + f' but the generated code in app/external_services/grpc/searcher/searcher_pb2_grpc.py depends on'
         + f' grpcio>={GRPC_GENERATED_VERSION}.'
         + f' Please upgrade your grpc module to grpcio>={GRPC_GENERATED_VERSION}'
         + f' or downgrade your generated code using grpcio-tools<={GRPC_VERSION}.'
-        + f' This warning will become an error in {EXPECTED_ERROR_RELEASE},'
-        + f' scheduled for release on {SCHEDULED_RELEASE_DATE}.',
-        RuntimeWarning
     )
 
 
@@ -59,6 +54,16 @@ class SearcherStub(object):
                 request_serializer=app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetLLMConfigurationsRequest.SerializeToString,
                 response_deserializer=app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetLLMConfigurationsResponse.FromString,
                 _registered_method=True)
+        self.GetEmbeddingModelConfigurations = channel.unary_unary(
+                '/grpc.Searcher/GetEmbeddingModelConfigurations',
+                request_serializer=app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetEmbeddingModelConfigurationsRequest.SerializeToString,
+                response_deserializer=app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetEmbeddingModelConfigurationsResponse.FromString,
+                _registered_method=True)
+        self.GetRAGConfigurations = channel.unary_unary(
+                '/grpc.Searcher/GetRAGConfigurations',
+                request_serializer=app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetRAGConfigurationsRequest.SerializeToString,
+                response_deserializer=app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetRAGConfigurationsResponse.FromString,
+                _registered_method=True)
 
 
 class SearcherServicer(object):
@@ -88,6 +93,18 @@ class SearcherServicer(object):
         context.set_details('Method not implemented!')
         raise NotImplementedError('Method not implemented!')
 
+    def GetEmbeddingModelConfigurations(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
+    def GetRAGConfigurations(self, request, context):
+        """Missing associated documentation comment in .proto file."""
+        context.set_code(grpc.StatusCode.UNIMPLEMENTED)
+        context.set_details('Method not implemented!')
+        raise NotImplementedError('Method not implemented!')
+
 
 def add_SearcherServicer_to_server(servicer, server):
     rpc_method_handlers = {
@@ -110,6 +127,16 @@ def add_SearcherServicer_to_server(servicer, server):
                     servicer.GetLLMConfigurations,
                     request_deserializer=app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetLLMConfigurationsRequest.FromString,
                     response_serializer=app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetLLMConfigurationsResponse.SerializeToString,
+            ),
+            'GetEmbeddingModelConfigurations': grpc.unary_unary_rpc_method_handler(
+                    servicer.GetEmbeddingModelConfigurations,
+                    request_deserializer=app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetEmbeddingModelConfigurationsRequest.FromString,
+                    response_serializer=app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetEmbeddingModelConfigurationsResponse.SerializeToString,
+            ),
+            'GetRAGConfigurations': grpc.unary_unary_rpc_method_handler(
+                    servicer.GetRAGConfigurations,
+                    request_deserializer=app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetRAGConfigurationsRequest.FromString,
+                    response_serializer=app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetRAGConfigurationsResponse.SerializeToString,
             ),
     }
     generic_handler = grpc.method_handlers_generic_handler(
@@ -220,6 +247,60 @@ class Searcher(object):
             '/grpc.Searcher/GetLLMConfigurations',
             app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetLLMConfigurationsRequest.SerializeToString,
             app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetLLMConfigurationsResponse.FromString,
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+            _registered_method=True)
+
+    @staticmethod
+    def GetEmbeddingModelConfigurations(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(
+            request,
+            target,
+            '/grpc.Searcher/GetEmbeddingModelConfigurations',
+            app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetEmbeddingModelConfigurationsRequest.SerializeToString,
+            app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetEmbeddingModelConfigurationsResponse.FromString,
+            options,
+            channel_credentials,
+            insecure,
+            call_credentials,
+            compression,
+            wait_for_ready,
+            timeout,
+            metadata,
+            _registered_method=True)
+
+    @staticmethod
+    def GetRAGConfigurations(request,
+            target,
+            options=(),
+            channel_credentials=None,
+            call_credentials=None,
+            insecure=False,
+            compression=None,
+            wait_for_ready=None,
+            timeout=None,
+            metadata=None):
+        return grpc.experimental.unary_unary(
+            request,
+            target,
+            '/grpc.Searcher/GetRAGConfigurations',
+            app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetRAGConfigurationsRequest.SerializeToString,
+            app_dot_external__services_dot_grpc_dot_searcher_dot_searcher__pb2.GetRAGConfigurationsResponse.FromString,
             options,
             channel_credentials,
             insecure,
