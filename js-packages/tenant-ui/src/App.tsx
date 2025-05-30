eea8730e0ac94b32dda9a4b079a4bfc43a333ba6
Lorenzo Venneri <lorenzov96@live.it>
2025-05-30 11:39:46 +0200

    Issue #1170: create themes

--- js-packages/tenant-ui/src/App.tsx
+++ js-packages/tenant-ui/src/App.tsx
@@ -1,25 +1,25 @@
 import { ApolloProvider } from "@apollo/client";
 import { Clear, Search } from "@mui/icons-material";
-import { AppBar, Box, createTheme, InputAdornment, List, TextField, ThemeProvider, Toolbar, Typography } from "@mui/material";
-import { red } from "@mui/material/colors";
+import { AppBar, Box, InputAdornment, List, TextField, ThemeProvider, Toolbar, Typography } from "@mui/material";
 import { QueryClientProvider } from "@tanstack/react-query";
 import React, { useMemo, useState } from "react";
-import { BrowserRouter, NavLink, Route, Routes } from "react-router-dom";
-import { AuthenticationProvider } from "./components/client/authentication";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { BrandLogo } from "./components/BrandLogo";
+import { apolloClient } from "./components/client/apolloClient";
+import { AuthenticationProvider } from "./components/client/authentication";
+import { queryClient } from "./components/client/queryClient";
 import { ModalProvider } from "./components/Modals";
 import { useFilteredMenuItems } from "./components/Navigation/menuItems";
 import { SideNavigationItem } from "./components/Navigation/SideNavigationItem";
-import { SideNavigationContextProvider } from "./components/sideNavigationContext";
+import { DashBoard } from "./components/Page/Dashboard";
 import { Tenant } from "./components/Page/Tenant";
-import { TenantCreate } from "./components/TenantCreate";
 import { Tenants } from "./components/Page/Tenants";
+import { SideNavigationContextProvider } from "./components/sideNavigationContext";
+import { TenantCreate } from "./components/TenantCreate";
+import { darkTheme, lightTheme } from "./components/Themes";
 import ThemeSwitcher from "./components/ThemeSwitcher";
 import { ToastProvider } from "./components/ToastProvider";
 import "./index.css";
-import { DashBoard } from "./components/Page/Dashboard";
-import { queryClient } from "./components/client/queryClient";
-import { apolloClient } from "./components/client/apolloClient";
 
 export default function App() {
   const savedTheme = localStorage.getItem("isDarkMode");
@@ -77,8 +77,6 @@ export default function App() {
                                 K9
                               </Typography>
                             </Box>
-
-                            {/* Theme switcher */}
                             <Box marginBottom={0.8}>
                               <ThemeSwitcher isDarkMode={isDarkMode} toggleTheme={toggleTheme} />
                             </Box>
@@ -159,8 +157,6 @@ export default function App() {
                             </List>
                           </Box>
                         </Box>
-
-                        {/* Main content */}
                         <Box
                           component="main"
                           sx={{
@@ -188,289 +184,6 @@ export default function App() {
     </AuthenticationProvider>
   );
 }
-
-function NavTabs({ tabs }: { tabs: Array<{ label: string; path: string }> }) {
-  return (
-    <div className="navbar navbar-underline navigation-bar navigation-bar-secondary navbar-expand-md" style={{ position: "sticky" }}>
-      <div className="container-fluid container-fluid-max-xl">
-        <ul className="navbar-nav">
-          {tabs.map(({ label, path }, index) => {
-            return (
-              <li className="nav-item" key={index}>
-                <NavLink className={({ isActive }) => `nav-link ${isActive ? "active" : ""}`} to={path} end={true}>
-                  <span className="navbar-text-truncate">{label}</span>
-                </NavLink>
-              </li>
-            );
-          })}
-        </ul>
-      </div>
-    </div>
-  );
-}
-
-export const themeColor = {
-  light: {
-    primary: "#FFFFFF",
-    secondary: "#FAFAFA",
-  },
-  dark: {
-    primary: "#2c2a29",
-    secondary: "#303030",
-  },
-  main: {
-    default: red[500],
-    lighter: red[100],
-    darker: "#7f1818bf",
-  },
-};
-
-const lightTheme = createTheme({
-  shape: { borderRadius: 8 },
-  components: {
-    MuiTable: {
-      styleOverrides: {
-        root: { background: themeColor.light.secondary },
-      },
-    },
-    MuiTableHead: {
-      styleOverrides: {
-        root: {
-          backgroundColor: themeColor.light.secondary,
-        },
-      },
-    },
-    MuiCard: {
-      defaultProps: {
-        variant: "outlined",
-      },
-    },
-    MuiListItemButton: {
-      styleOverrides: {
-        root: {
-          "&:hover": {
-            backgroundColor: themeColor.main.lighter,
-            color: themeColor.dark.primary,
-          },
-          "&.active .MuiListItemText-root": {
-            backgroundColor: themeColor.main.default,
-            color: themeColor.light.primary,
-            paddingLeft: "10px",
-            borderRadius: "8px",
-          },
-        },
-      },
-    },
-    MuiDrawer: {
-      styleOverrides: {
-        paper: { background: themeColor.light.primary },
-      },
-    },
-    MuiTooltip: {
-      styleOverrides: {
-        tooltip: { background: "#333", padding: "10px" },
-      },
-    },
-  },
-  zIndex: { modal: 500, drawer: 100, snackbar: 510 },
-  palette: {
-    error: {
-      main: red[500],
-    },
-    success: {
-      main: "#2EC071",
-    },
-    warning: {
-      main: "#F39C12",
-    },
-    info: {
-      main: "#2980B9",
-    },
-    background: {
-      paper: themeColor.light.secondary,
-      default: themeColor.light.primary,
-    },
-    primary: {
-      light: red[100],
-      main: red[500],
-      dark: red[800],
-      contrastText: themeColor.light.primary,
-    },
-    secondary: {
-      main: themeColor.light.primary,
-    },
-    mode: "light",
-  },
-  typography: {
-    fontFamily: "Lato, sans-serif",
-    h1: {
-      fontSize: "2.125rem",
-    },
-    h2: {
-      fontSize: "1.5rem",
-      fontWeight: "bold",
-    },
-    h3: { fontSize: "1.17rem" },
-    h4: { fontSize: "1rem" },
-  },
-});
-
-const darkTheme = createTheme({
-  shape: { borderRadius: 8 },
-  components: {
-    MuiSelect: {
-      styleOverrides: {
-        root: { background: themeColor.dark.secondary },
-      },
-    },
-    MuiDialogContent: {
-      styleOverrides: {
-        root: {
-          backgroundColor: themeColor.dark.secondary,
-        },
-      },
-    },
-    MuiDialogActions: {
-      styleOverrides: {
-        root: {
-          backgroundColor: themeColor.dark.secondary,
-          borderRadius: "0 0 8px 8px",
-        },
-      },
-    },
-    MuiTable: {
-      styleOverrides: {
-        root: {},
-      },
-    },
-    MuiTableCell: {
-      styleOverrides: {
-        root: {
-          borderBottom: "1px solid #555",
-        },
-      },
-    },
-    MuiTableHead: {
-      styleOverrides: {
-        root: {
-          backgroundColor: themeColor.dark.secondary,
-        },
-      },
-    },
-    MuiTableRow: {
-      styleOverrides: {
-        root: {
-          backgroundColor: themeColor.dark.secondary,
-        },
-      },
-    },
-    MuiTableBody: {
-      styleOverrides: {
-        root: {},
-      },
-    },
-    MuiCard: {
-      defaultProps: {
-        variant: "outlined",
-      },
-    },
-    MuiListItemButton: {
-      styleOverrides: {
-        root: {
-          "&:hover": {
-            backgroundColor: themeColor.main.darker,
-            color: themeColor.light.primary,
-          },
-          "&.active .MuiListItemText-root": {
-            paddingLeft: "10px",
-            color: "#FFFFFF",
-            borderRadius: "8px",
-          },
-          ".MuiListItemText-root": {
-            backgrundColor: "transparent",
-          },
-        },
-      },
-    },
-    MuiDrawer: {
-      styleOverrides: {
-        paper: { background: themeColor.dark.primary },
-      },
-    },
-    MuiTextField: {
-      styleOverrides: {
-        root: {
-          background: themeColor.dark.secondary,
-        },
-      },
-    },
-    MuiTooltip: {
-      styleOverrides: {
-        tooltip: { background: "#FFFFFF", color: themeColor.dark.secondary, padding: "10px" },
-      },
-    },
-  },
-  zIndex: { modal: 500, drawer: 100, snackbar: 510 },
-  palette: {
-    mode: "dark",
-    error: {
-      main: red[500],
-    },
-    success: {
-      main: "#2EC071",
-    },
-    warning: {
-      main: "#F39C12",
-    },
-    info: {
-      main: "#2980B9",
-    },
-    background: {
-      paper: "#303030",
-      default: "#2c2a29",
-    },
-    primary: {
-      light: red[100],
-      main: red[500],
-      dark: red[800],
-      contrastText: themeColor.light.primary,
-    },
-    secondary: {
-      main: themeColor.light.primary,
-    },
-    text: {
-      primary: "#FFFFFF",
-      secondary: "#CCC",
-      disabled: "#666",
-    },
-  },
-  typography: {
-    fontFamily: "Lato, sans-serif",
-    subtitle1: {
-      color: "#FFFFFF",
-    },
-    h1: {
-      fontSize: "2.125rem",
-      fontWeight: "bold",
-      color: "#FFFFFF",
-    },
-    h2: {
-      fontSize: "1.5rem",
-      fontWeight: "bold",
-      color: "#FFFFFF",
-    },
-    h3: { fontSize: "1.17rem", color: "#FFFFFF" },
-    h4: { fontSize: "1rem", color: "#FFFFFF" },
-    h5: { color: "#FFFFFF" },
-    h6: { color: "#FFFFFF" },
-    body1: {
-      color: "#FFFFFF",
-    },
-    body2: {
-      color: "#CCC",
-    },
-  },
-});
 
 const AppRoutes = () => (
   <Routes>
