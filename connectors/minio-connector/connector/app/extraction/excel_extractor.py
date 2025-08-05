3d206f53293e95f50d06fd4749416d019837a100
daniele-caldarini <daniele.caldarini@smc.it>
2025-08-05 13:46:53 +0200

    Issue #1225: fixes minio connector adding excel extraction logic

---
+++ connectors/minio-connector/connector/app/extraction/excel_extractor.py
@@ -0,0 +1,131 @@
+"""
+Copyright (c) 2020-present SMC Treviso s.r.l. All rights reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import json
+import base64
+import logging
+import pandas as pd
+import requests
+from minio import Minio
+from datetime import datetime
+from logging.config import dictConfig
+
+from minio.error import MinioException
+from .log_config import LogConfig
+
+dictConfig(LogConfig().dict())
+
+def get_as_base64(response):
+    data = base64.b64encode(response).decode('utf-8')
+    return data
+
+
+logger = logging.getLogger("status-logger")
+
+
+def post_message(url, payload):
+    try:
+        r = requests.post(url, json=payload, timeout=20)
+        if r.status_code == 200:
+            return True
+        else:
+            r.raise_for_status()
+    except Exception as e:
+        logger.error(str(e) + " during request at url: " + str(url))
+        return False
+
+
+class ExcelMinioExtractor:
+
+    def __init__(self, host, port, access_key, secret_key, bucket_name, columns, datasource_id, timestamp,
+                 schedule_id, tenant_id, ingestion_url):
+
+        super(ExcelMinioExtractor, self).__init__()
+        self.datasource_id = datasource_id
+        self.ingestion_url = ingestion_url
+        self.schedule_id = schedule_id
+        self.tenant_id = tenant_id
+        self.access_key = access_key
+        self.secret_key = secret_key
+        self.bucket_name = bucket_name
+        self.columns = columns
+        self.timestamp = timestamp
+        self.url = str(host) + ":" + str(port)
+
+        self.status_logger = logging.getLogger("status-logger")
+
+    def extract_data(self):
+
+        try:
+
+            client = Minio(self.url, self.access_key, self.secret_key, secure=False)
+
+            end_timestamp = datetime.utcnow().timestamp() * 1000
+
+            objects = client.list_objects(self.bucket_name)
+
+        except MinioException:
+
+            return
+
+        for obj in objects:
+
+            metadata = client.stat_object(self.bucket_name, obj.object_name)
+
+            binaries = []
+
+            y = client.get_object(self.bucket_name, obj.object_name)
+
+            data = y.data
+
+            df = pd.read_excel(data)
+
+            df = df.drop(columns=df.select_dtypes(include=['datetime64']).columns.tolist())
+            df = df.drop(columns=df.select_dtypes(include=['float64']).columns.tolist())
+
+            df = df.astype(str)
+
+            if len(self.columns) > 0:
+                df = df[self.columns]
+
+            df_dict = df.to_dict('records')
+
+            self.status_logger.info("Sending " + str(len(df_dict)) + "elements")
+
+            for i, element in enumerate(df_dict):
+
+                datasource_payload = {
+                    "ticket": element
+                }
+
+                payload = {
+                    "datasourceId": self.datasource_id,
+                    "contentId": i,
+                    "parsingDate": int(end_timestamp),
+                    "rawContent": str(element).lower(),
+                    "datasourcePayload": datasource_payload,
+                    "resources": {
+                        "binaries": []
+                    },
+                    "scheduleId": self.schedule_id,
+                    "tenantId": self.tenant_id
+                }
+
+                self.status_logger.info("post")
+
+                post_message(self.ingestion_url, payload)
+                self.status_logger.info("Sent " + str(i+1) + " element of " + str(len(df_dict)) + " elements")
--- connectors/minio-connector/connector/app/main.py
+++ connectors/minio-connector/connector/app/main.py
@@ -22,6 +22,8 @@ from pydantic import BaseModel
 from typing import Optional
 import logging
 import os
+import requests
+import json
 import threading
 from extraction.extractor import MinioExtractor
 from extraction.excel_extractor import ExcelMinioExtractor
@@ -50,6 +52,7 @@ class MinioRequest(BaseModel):
     timestamp: int
     tenantId: str
     prefix: Optional[str] = None
+    columns: Optional[list] = []
     additionalMetadata: Optional[dict] = {}
 
 
@@ -85,3 +88,96 @@ def execute(request: MinioRequest):
     thread.start()
 
     return "Extraction of files started"
+
+
+@app.post("/extract-excel")
+def execute(request: MinioRequest):
+    request = request.dict()
+
+    datasource_id = request['datasourceId']
+    schedule_id = request['scheduleId']
+    timestamp = request["timestamp"]
+    tenant_id = request["tenantId"]
+
+    host = request["host"]
+    port = request["port"]
+    access_key = request["accessKey"]
+    secret_key = request["secretKey"]
+    bucket_name = request["bucketName"]
+    columns = request["columns"]
+
+    extractor = ExcelMinioExtractor(host, port, access_key, secret_key, bucket_name, columns, datasource_id,
+                               timestamp, schedule_id, tenant_id, ingestion_url)
+
+    thread = threading.Thread(target=extractor.extract_data)
+    thread.start()
+
+    return "Extraction of files started"
+
+
+class HealthCheck(BaseModel):
+    """Response model to validate and return when performing a health check."""
+
+    status: str = "UP"
+
+
+@app.get(
+    "/health",
+    tags=["healthcheck"],
+    summary="Perform a Health Check",
+    response_description="Return HTTP Status Code 200 (OK)",
+    status_code=status.HTTP_200_OK,
+    response_model=HealthCheck,
+)
+def get_health() -> HealthCheck:
+    """
+    ## Perform a Health Check
+    Endpoint to perform a healthcheck on. This endpoint can primarily be used Docker
+    to ensure a robust container orchestration and management is in place. Other
+    services which rely on proper functioning of the API service will not deploy if this
+    endpoint returns any other HTTP status code except 200 (OK).
+    Returns:
+        HealthCheck: Returns a JSON response with the health status
+    """
+
+    try:
+        fastapi_response = requests.get("http://localhost:5000/docs")
+        if fastapi_response.status_code == 200:
+            return HealthCheck(status="UP")
+        else:
+            return HealthCheck(status="DOWN")
+    except requests.RequestException as e:
+        logger.error(str(e) + " during request for health check")
+        raise e
+
+
+@app.get("/sample",
+        tags=["sample"],
+        summary="Get a sample of result",
+        response_description="Return json sample result", )
+def get_sample():
+    f = open('data/sample.json')
+
+    # returns JSON object as
+    # a dictionary
+    data = json.load(f)
+
+    f.close()
+
+    return data
+
+
+@app.get("/form",
+        tags=["form"],
+        summary="Get form structure of request",
+        response_description="Return json form structure", )
+def get_sitemap_form():
+    f = open('data/form.json')
+
+    # returns JSON object as
+    # a dictionary
+    data = json.load(f)
+
+    f.close()
+
+    return data
