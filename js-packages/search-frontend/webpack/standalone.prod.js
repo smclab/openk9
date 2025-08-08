b8978bf08e9a73b3e6bf3a0ca3145590c01446f2
Giorgio Bartolomeo <giorgio.bartolomeo@smc.it>
2025-08-08 17:55:01 +0200

    issue #1486 - Added configuration for prod environment config and package.json build scripts

--- js-packages/search-frontend/webpack/standalone.prod.js
+++ js-packages/search-frontend/webpack/standalone.prod.js
@@ -2,51 +2,68 @@
 
 const path = require("path");
 
-module.exports = {
-  mode: "production",
-  entry: {
-    index: "./src/index.tsx",
-  },
-  devtool: "source-map",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            plugins: [
-              "macros",
-              [
-                "@babel/plugin-transform-runtime",
-                {
-                  regenerator: true,
-                },
+module.exports = (env = {}) => {
+  const isKeycloakEnabled = env.keycloak === "true";
+  const isChatbotEnabled = env.chatbot === "true";
+
+  return {
+    mode: "production",
+    entry: {
+      index: "./src/index.tsx",
+    },
+    devtool: "source-map",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              plugins: [
+                "macros",
+                [
+                  "@babel/plugin-transform-runtime",
+                  {
+                    regenerator: true,
+                  },
+                ],
               ],
-            ],
-            presets: ["@babel/preset-env", "@babel/react", "@babel/typescript"],
+              presets: [
+                "@babel/preset-env",
+                "@babel/react",
+                "@babel/typescript",
+              ],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/inline",
-      },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/inline",
+        },
+      ],
+    },
+    // decomment this to analyze bundle size
+    // plugins: [new require("webpack-bundle-analyzer").BundleAnalyzerPlugin()],
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+      path: path.resolve(__dirname, "../dist"),
+      filename: "[name].js",
+      libraryTarget: "umd",
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        "process.env.REACT_APP_KEYCLOAK_ENABLED":
+          JSON.stringify(isKeycloakEnabled),
+        "process.env.REACT_APP_CHATBOT_ENABLED":
+          JSON.stringify(isChatbotEnabled),
+      }),
     ],
-  },
-  // decomment this to analyze bundle size
-  // plugins: [new require("webpack-bundle-analyzer").BundleAnalyzerPlugin()],
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    path: path.resolve(__dirname, "../dist"),
-    filename: "[name].js",
-    libraryTarget: "umd",
-  },
+  };
 };
