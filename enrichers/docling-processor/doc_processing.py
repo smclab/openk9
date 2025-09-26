48bc922b13605dc8502539c238aa5f07d4db5b93
luca.siligato <luca.siligato@smc.it>
2025-09-26 18:34:12 +0200

    Issue #1564: Added thread and response draft

--- enrichers/docling-processor/doc_processing.py
+++ enrichers/docling-processor/doc_processing.py
@@ -20,10 +20,25 @@ async def start_task(input:dict= Body(...)):#payload="pl",enrichItemConfig="Conf
     thread.start()
     return {"status": "ok", "message": f"Processo avviato con parametro {b64_str}"}
 
-def operation(document):
-    print(f"Processo {document} iniziato")
-    time.sleep(10)
+def operation(b64_str,configs,token):
+    host="http://127.0.0.1:8001"
+
+    decoded_bytes = base64.b64decode(b64_str)
+
+    bites_io = BytesIO(decoded_bytes)
+    print(f"Processo {b64_str} iniziato")
+    source= DocumentStream(name="doc.pdf",stream=bites_io)
+
     # converter = DocumentConverter()
-    # result = converter.convert_string(string=document)
-    # print(result.document.export_to_markdown())
-    print(f"Processo {document} terminato")
+    # result = converter.convert(source)
+    # markdown=result.document.export_to_markdown()
+    # print(markdown)
+    # print(f"Processo {b64_str} terminato")
+
+    markdown=decoded_bytes
+
+    headers = {"Content-Type": "text/markdown"}
+    response = requests.post(f"{host}/api/datasource/pipeline/callback/{token}", params={"markdown_response": markdown}, headers=headers)
+
+    print("Status:", response.status_code)
+    print("Risultato:", response.json())
