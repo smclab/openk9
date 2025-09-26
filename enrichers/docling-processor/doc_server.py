a0de4f2726766ac443d589f7e209e2ef7a6605fa
luca.siligato <luca.siligato@smc.it>
2025-09-26 18:37:15 +0200

    Issue #1564:  Added mockup callback

--- enrichers/docling-processor/doc_server.py
+++ enrichers/docling-processor/doc_server.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+from io import BytesIO
+
 import asyncio
 import base64
 import random
+import requests
 
 app = FastAPI()
 
@@ -10,12 +13,27 @@ STRINGS = [
     "FastAPI è fantastico!",
     "Stringa casuale",
     "OpenAI rocks",
-    "Python ❤️"
+    "Python"
 ]
 
-@app.get("/random-string")
+@app.get("/payload/")
 def get_random_string():
     chosen = random.choice(STRINGS)
-    print(f"Chosen: {chosen}")
-    encoded_str = base64.b64encode(chosen.encode("utf-8")).decode("utf-8")
-    return {"string": encoded_str}
+    encode_bytes= base64.b64encode(chosen.encode("utf-8"))
+    decoded_bytes = encode_bytes.decode("utf-8")
+
+    input ={"payload": 
+                {"doc":decoded_bytes},
+            "enrichItemConfig": 
+                {"configs":"Config passata"},
+            "replyTo":"fake-token"
+            }
+    response = requests.post("http://127.0.0.1:8000/start-task/", json=input)
+    print("Status:", response.status_code)
+    print("Risposta:", response.json())
+
+
+@app.post("/api/datasource/pipeline/callback/{token}")
+def cose(token:str,markdown_response):
+    print(token)
+    print(markdown_response)
