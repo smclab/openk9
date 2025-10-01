cdfa5ccd8bc2d14c2120a649d3a048f5eccef261
luca.siligato <luca.siligato@smc.it>
2025-10-01 11:54:54 +0200

    Issue #1565: Added more interfaces to class interface

--- enrichers/docling-processor/app/utils/fm_helper.py
+++ enrichers/docling-processor/app/utils/fm_helper.py
@@ -1,9 +1,31 @@
 import requests
+import base64
+from io import BytesIO
+
+
 
 class FileManagerHelper():
     def __init__(self,host):
         self.host=host
 
+    def delete(self,tenant,resource):
+        response = requests.post(f"{self.host}/api/file-manager/v1/delete/{resource}/{tenant}")
+        pass
+
     def getBase64(self,tenant,resource):
         response = requests.get(f"{self.host}/api/file-manager/v1/download/base64/{resource}/{tenant}")
         return response.text
+    
+    def getBytes(self,tenant,resource):
+        response = requests.get(f"{self.host}/api/file-manager/v1/download/byte/{resource}/{tenant}")
+        decoded_bytes = base64.b64decode(response.text)
+        bites_io = BytesIO(decoded_bytes)
+        return bites_io
+
+    def download(self,tenant,resource):
+        response = requests.get(f"{self.host}/api/file-manager/v1/download/{resource}/{tenant}")
+        pass
+
+    def upload(self,tenant,sourceId,fileId):
+        response = requests.get(f"{self.host}/api/file-manager/v1/upload/{sourceId}/{fileId}/{tenant}")
+        pass
