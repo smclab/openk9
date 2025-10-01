995fcbcd896b54ca9c949f4a688a40965cf7df4b
Matteo Festugato <matteo.festugato@smc.it>
2025-10-01 16:40:34 +0200

    Issue #1558: Inject CallBackClient to simulate the call back endpoint

--- enrichers/quarkus-base-enricher/archetype/src/main/resources/archetype-resources/src/main/java/ProcessResourceImpl.java
+++ enrichers/quarkus-base-enricher/archetype/src/main/resources/archetype-resources/src/main/java/ProcessResourceImpl.java
@@ -2,13 +2,21 @@ package ${package};
 
 import io.openk9.enricher.api.ProcessResource;
 import io.openk9.enricher.api.beans.OpenK9Input;
-import io.vertx.core.json.Json;
+import jakarta.inject.Inject;
 import jakarta.validation.constraints.NotNull;
+import org.jboss.logging.Logger;
 
 public class ProcessResourceImpl implements ProcessResource {
 
+    private static final Logger LOGGER = Logger.getLogger(ProcessResourceImpl.class);
+
+    @Inject
+    CallBackClient callBackClient;
+
     @Override
     public void process(@NotNull OpenK9Input data) {
-        System.out.println(Json.encodePrettily(data));
+        LOGGER.info("Starting enrichment of data...");
+        // Simulate call back endpoint
+        callBackClient.callback(data);
     }
 }
