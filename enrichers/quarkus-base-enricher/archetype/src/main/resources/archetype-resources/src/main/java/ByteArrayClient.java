9c996b4419f281054342f4ed6a4a7a2e113afc88
Matteo Festugato <matteo.festugato@smc.it>
2025-10-09 12:14:06 +0200

    Issue #1559: Adds ByteArrayClient.java

---
+++ enrichers/quarkus-base-enricher/archetype/src/main/resources/archetype-resources/src/main/java/ByteArrayClient.java
@@ -0,0 +1,47 @@
+package ${package};
+
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.ext.web.client.HttpResponse;
+import io.vertx.ext.web.client.WebClient;
+import io.vertx.ext.web.codec.BodyCodec;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
+
+/**
+ * This class provides the byte array-encoded format of a binary payload.
+ * To retrieve the correct payload, you must provide both a {@code resourceId} and a {@code schemaName}.
+ *
+ * @see WebClient
+ */
+@ApplicationScoped
+public class ByteArrayClient {
+
+    private static final Logger LOGGER = Logger.getLogger(ByteArrayClient.class);
+
+    @Inject
+    Vertx vertx;
+
+    @ConfigProperty(name = "io.openk9.enricher.byte.array.api.port")
+    private int port;
+
+    @ConfigProperty(name = "io.openk9.enricher.byte.array.api.host")
+    private String host;
+
+    @ConfigProperty(name = "io.openk9.enricher.byte.array.api.path")
+    private String path;
+
+    public Future<Buffer> getByteArray(String resourceId, String schemaName) {
+        WebClient client = WebClient.create(vertx);
+        return client
+                .get(port, host, path + resourceId + "/" + schemaName)
+                .as(BodyCodec.buffer())
+                .send()
+                .map(HttpResponse::body)
+                .onSuccess(res -> LOGGER.info("Result from byte array endpoint: " + res.getBytes()))
+                .onFailure(res -> LOGGER.error("Error requesting byte array binaries: " + res.getMessage()));
+    }
+}
