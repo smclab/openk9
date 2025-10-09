9aeb23443a6f3f8c2f7f48c1fc30cb14c02effa8
Matteo Festugato <matteo.festugato@smc.it>
2025-10-09 12:17:16 +0200

    Issue #1559: Updates ProcessResourceImpl to simulate base 64 and byte array calls

--- enrichers/quarkus-base-enricher/archetype/src/main/resources/archetype-resources/src/main/java/ProcessResourceImpl.java
+++ enrichers/quarkus-base-enricher/archetype/src/main/resources/archetype-resources/src/main/java/ProcessResourceImpl.java
@@ -15,14 +15,28 @@ public class ProcessResourceImpl implements ProcessResource {
     #if ($implementationType == "async")@Inject
     CallBackClient callBackClient;
 
+    @Inject
+    Base64Client base64Client;
+
+    @Inject
+    ByteArrayClient byteArrayClient;
+
     @Override
     public void process(@NotNull OpenK9Input data) {
         if (data.getReplyTo() != null && !data.getReplyTo().isEmpty()) {
             LOGGER.info("Starting enrichment of data...");
             // Simulate call back endpoint
+            /*
             EnrichData enrichData = new EnrichData();
             enrichData.setPayload(data.getPayload());
             callBackClient.callback(enrichData, data.getReplyTo());
+             */
+
+            // Simulate base 64 call endpoint
+            base64Client.getBase64("1", "test1");
+
+            // Simulate byte array call endpoint
+            byteArrayClient.getByteArray("2", "test2");
         }
         else {
             throw new IllegalArgumentException("replyTo is null or empty");
