c93653cf3418c8121b0f4cd67789593f14dc3e56
Matteo Festugato <matteo.festugato@smc.it>
2025-10-13 16:08:29 +0200

    Issue #1591: Adds enrichData as map

--- enrichers/quarkus-base-enricher/archetype/src/main/resources/archetype-resources/src/main/java/EnrichData.java
+++ enrichers/quarkus-base-enricher/archetype/src/main/resources/archetype-resources/src/main/java/EnrichData.java
@@ -1,10 +1,13 @@
 package ${package};
 
 import io.openk9.enricher.api.beans.Payload;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 public class EnrichData {
 
     private Payload payload;
+    private Map<String, Object> enrichData = new LinkedHashMap<>();
 
     public EnrichData() {
     }
@@ -15,5 +18,13 @@ public class EnrichData {
 
     public void setPayload(Payload payload) {
         this.payload = payload;
+    }
+
+    public Map<String, Object> getEnrichData() {
+        return enrichData;
+    }
+
+    public void setEnrichData(String name, Object value) {
+        enrichData.put(name, value);
     }
 }
