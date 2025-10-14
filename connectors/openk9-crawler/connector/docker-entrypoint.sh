b5898d44699bb67ab130e9c5727d082429ec8d9f
Federico Rossi <federico.rossi@smc.it>
2025-10-14 16:08:54 +0200

    Issue #1600: Fixes Scrapyd in docker-entrypoint.sh on container restart. Adds install_playwright.sh, handles playwright dependencies installation. Changes in Dockerfile for smaller image size.

--- connectors/openk9-crawler/connector/docker-entrypoint.sh
+++ connectors/openk9-crawler/connector/docker-entrypoint.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
+pid=$(cat /app/twistd.pid)
+if ! kill -0 $pid 2>/dev/null; then
+    echo "Process not running, removing stale pid file"
+    rm /app/twistd.pid
+else
+    echo "Process is still running, do not remove pid file"
+fi
 scrapyd & /app/wait_for_scrapyd.sh localhost:6800 && scrapyd-deploy
 uvicorn main:app --host 0.0.0.0 --port 5000
