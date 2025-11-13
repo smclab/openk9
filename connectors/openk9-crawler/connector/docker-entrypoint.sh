9788a408c52f21a578183f55ea5b058bbf4eee75
Federico Rossi <federico.rossi@smc.it>
2025-11-13 18:46:57 +0100

    Issue #1603: Changes default Dockerfile to build with --build-arg WITH_SCRAPYDWEB. Changed to build in a single Docker image and container.

--- connectors/openk9-crawler/connector/docker-entrypoint.sh
+++ connectors/openk9-crawler/connector/docker-entrypoint.sh
@@ -7,5 +7,15 @@ if ! kill -0 $pid 2>/dev/null; then
 else
     echo "Process is still running, do not remove pid file"
 fi
+
+with_scrapydweb=${WITH_SCRAPYDWEB}
+if [ $with_scrapydweb -ne 1 ] && [ $with_scrapydweb -ne 0 ]; then
+  with_scrapydweb=0;
+fi
+
 scrapyd & /app/wait_for_scrapyd.sh localhost:6800 && scrapyd-deploy
-uvicorn main:app --host 0.0.0.0 --port 5000
+if [ $with_scrapydweb -eq 1 ]; then
+  scrapydweb & uvicorn main:app --host 0.0.0.0 --port 5000
+else
+  uvicorn main:app --host 0.0.0.0 --port 5000
+fi
