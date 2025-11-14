658a16221ffa827055a9d59cbb78cd68299c3b56
Matteo Festugato <matteo.festugato@smc.it>
2025-11-14 11:52:11 +0100

    Issue #1660: Updates ResourceUri

--- core/app/datasource/src/main/java/io/openk9/datasource/model/ResourceUri.java
+++ core/app/datasource/src/main/java/io/openk9/datasource/model/ResourceUri.java
@@ -1,20 +1,38 @@
+/*
+ * Copyright (c) 2020-present SMC Treviso s.r.l. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package io.openk9.datasource.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 @Embeddable
 @Getter
 @Setter
-@RequiredArgsConstructor
+@ToString
+@NoArgsConstructor
+@Builder
+@AllArgsConstructor
 public class ResourceUri {
 
-	@Column(name = "baseUri")
+	@Column(name = "baseUri", columnDefinition = "text", nullable = false)
 	private String baseUri;
 
-	@Column(name = "path")
+	@Column(name = "path", columnDefinition = "text", nullable = false)
 	private String path;
 }
