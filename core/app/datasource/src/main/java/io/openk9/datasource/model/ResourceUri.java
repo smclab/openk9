5b96e2953ac1a225697a9a39834f24b9ec0fb23f
Matteo Festugato <matteo.festugato@smc.it>
2025-11-27 15:49:38 +0100

    Issue #1660: Removes jdbc annotation; adjusts lombok imports to include only the necessary ones

--- core/app/datasource/src/main/java/io/openk9/datasource/model/ResourceUri.java
+++ core/app/datasource/src/main/java/io/openk9/datasource/model/ResourceUri.java
@@ -19,9 +19,12 @@ package io.openk9.datasource.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import lombok.*;
-import org.hibernate.annotations.JdbcTypeCode;
-import org.hibernate.type.SqlTypes;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
 
 @Embeddable
 @Getter
@@ -33,10 +36,8 @@ import org.hibernate.type.SqlTypes;
 public class ResourceUri {
 
 	@Column(name = "base_uri", nullable = false)
-	@JdbcTypeCode(SqlTypes.LONGVARCHAR)
 	private String baseUri;
 
 	@Column(name = "path")
-	@JdbcTypeCode(SqlTypes.LONGVARCHAR)
 	private String path;
 }
