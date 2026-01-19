43005f1dc65818bd870e665a76fba391373568e1
Giorgio Bartolomeo <giorgio.bartolomeo@yahoo.it>
2026-01-19 12:51:15 +0100

    issue 1665 - sub issue #1666 - minor UI fix to LayoutTab and CodeInput

--- js-packages/admin-ui/src/components/Form/Tabs/LayoutTab.tsx
+++ js-packages/admin-ui/src/components/Form/Tabs/LayoutTab.tsx
@@ -38,15 +38,15 @@ export default function AssociationsLayout({
       </Typography>
 
       <Paper
+        variant="outlined"
         elevation={0}
         sx={{
-          border: "1px solid #e0e0e0",
           borderRadius: "10px",
           width: "100%",
           overflow: "scroll",
         }}
       >
-        <Box sx={{ borderBottom: "1px solid #e0e0e0" }}>
+        <Box>
           <Tabs
             value={value}
             onChange={handleChange}
