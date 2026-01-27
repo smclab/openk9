85996f71bf14822636442543ddb65a72eb9aaa8e
luca.siligato <luca.siligato@smc.it>
2026-01-27 10:29:50 +0100

    Issue #1737: Added license

--- enrichers/docling-processor/enricher/app/utils/format_detect.py
+++ enrichers/docling-processor/enricher/app/utils/format_detect.py
@@ -1,3 +1,20 @@
+#
+# Copyright (c) 2020-present SMC Treviso s.r.l. All rights reserved.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
 import base64
 from io import BytesIO
 
@@ -15,7 +32,7 @@ def extract_extension_base64(base64_content: str) -> str:
         base64_content: String in base64
 
     Returns:
-        File's extension (es. 'pdf', 'docx') or raise an error if 
+        File's extension (es. 'pdf', 'docx') or raise an error if
         -   the extension could not be found
         -   the base64 string could not be decoded
     """
