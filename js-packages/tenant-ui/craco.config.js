f01ca88dfcfb333a6c8f40680b651de9aabace08
Giorgio Bartolomeo <giorgio.bartolomeo@yahoo.it>
2026-03-03 12:24:20 +0100

    issue #1888 - fix build tenant ui

--- js-packages/tenant-ui/craco.config.js
+++ js-packages/tenant-ui/craco.config.js
@@ -1,6 +1,6 @@
 const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
+const CompressionPlugin = require("compression-webpack-plugin");
 const path = require("path");
-// const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
 
 module.exports = {
   webpack: {
@@ -10,9 +10,108 @@ module.exports = {
     },
     plugins: {
       add: [
-        [new MonacoWebpackPlugin(), "append"],
-        // [new BundleAnalyzerPlugin(), "append"],
+        [
+          new MonacoWebpackPlugin({
+            languages: [],
+            features: [
+              "bracketMatching",
+              "clipboard",
+              "contextmenu",
+              "coreCommands",
+              "cursorUndo",
+              "find",
+              "format",
+              "hover",
+              "inPlaceReplace",
+              "iPadShowKeyboard",
+              "links",
+              "multicursor",
+              "suggest",
+            ],
+          }),
+          "append",
+        ],
+        [
+          new CompressionPlugin({
+            filename: "[path][base].gz",
+            algorithm: "gzip",
+            test: /\.(js|css|html|svg)$/,
+            threshold: 10240,
+            minRatio: 0.8,
+          }),
+          "append",
+        ],
+        [
+          new CompressionPlugin({
+            filename: "[path][base].br",
+            algorithm: "brotliCompress",
+            test: /\.(js|css|html|svg)$/,
+            compressionOptions: {
+              level: 11,
+            },
+            threshold: 10240,
+            minRatio: 0.8,
+          }),
+          "append",
+        ],
       ],
+    },
+    configure: (webpackConfig, { env }) => {
+      // Applica splitChunks avanzato solo in production
+      if (env === "production") {
+        webpackConfig.optimization = {
+          ...webpackConfig.optimization,
+          splitChunks: {
+            chunks: "all",
+            cacheGroups: {
+              monacoEditor: {
+                test: /[\\/]node_modules[\\/]monaco-editor[\\/]/,
+                name: "monaco-editor",
+                priority: 40,
+                reuseExistingChunk: true,
+              },
+              mui: {
+                test: /[\\/]node_modules[\\/]@mui[\\/]/,
+                name: "mui",
+                priority: 30,
+                reuseExistingChunk: true,
+              },
+              apollo: {
+                test: /[\\/]node_modules[\\/]@apollo[\\/]/,
+                name: "apollo-client",
+                priority: 25,
+                reuseExistingChunk: true,
+              },
+              react: {
+                test: /[\\/]node_modules[\\/](react|react-dom|react-router|react-router-dom)[\\/]/,
+                name: "react-vendors",
+                priority: 20,
+                reuseExistingChunk: true,
+              },
+              vendors: {
+                test: /[\\/]node_modules[\\/]/,
+                name: "vendors",
+                priority: 10,
+                reuseExistingChunk: true,
+              },
+              common: {
+                minChunks: 2,
+                priority: 5,
+                reuseExistingChunk: true,
+                enforce: true,
+              },
+            },
+            maxInitialRequests: 25,
+            maxAsyncRequests: 25,
+            minSize: 20000,
+          },
+          runtimeChunk: {
+            name: "runtime",
+          },
+        };
+      }
+
+      return webpackConfig;
     },
   },
 };
