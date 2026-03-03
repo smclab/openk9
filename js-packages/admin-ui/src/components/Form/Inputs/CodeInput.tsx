30e5558b91eb43228d429958de5fd6cd90ca801e
Giorgio Bartolomeo <giorgio.bartolomeo@yahoo.it>
2026-03-03 12:49:18 +0100

    issue #1888 - fix build tenant ui

--- js-packages/admin-ui/src/components/Form/Inputs/CodeInput.tsx
+++ js-packages/admin-ui/src/components/Form/Inputs/CodeInput.tsx
@@ -1,19 +1,19 @@
 ﻿/*
-* Copyright (c) 2020-present SMC Treviso s.r.l. All rights reserved.
-*
-* This program is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* This program is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+ * Copyright (c) 2020-present SMC Treviso s.r.l. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 import React from "react";
 import * as monaco from "monaco-editor";
 import { BaseInputProps } from "..";
@@ -43,11 +43,6 @@ monaco.languages.typescript.typescriptDefaults.setCompilerOptions({
   target: monaco.languages.typescript.ScriptTarget.ES2020,
   esModuleInterop: true,
 });
-
-// monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
-//   noSemanticValidation: false,
-//   noSyntaxValidation: false,
-// });
 
 (async () => {
   const response = await fetch("https://unpkg.com/@types/react@18.0.24/index.d.ts");
@@ -301,4 +296,3 @@ function templateTypeDefinition() {
     type Without<T, K> = Pick<T, Exclude<keyof T, K>>; 
 `;
 }
-
