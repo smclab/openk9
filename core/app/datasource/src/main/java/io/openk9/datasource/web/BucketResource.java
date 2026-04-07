74d0e55946f23fac574ad93a773afe38cd54e6fb
Matteo Festugato <matteo.festugato@smc.it>
2026-04-07 17:45:58 +0200

    Issue #1875: Removes unused imports

--- core/app/datasource/src/main/java/io/openk9/datasource/web/BucketResource.java
+++ core/app/datasource/src/main/java/io/openk9/datasource/web/BucketResource.java
@@ -76,12 +76,10 @@ import jakarta.ws.rs.core.Response;
 import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
 import org.hibernate.reactive.mutiny.Mutiny;
 
-import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.media.Content;
 import org.eclipse.microprofile.openapi.annotations.media.Schema;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
 import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
-import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 
 @ApplicationScoped
