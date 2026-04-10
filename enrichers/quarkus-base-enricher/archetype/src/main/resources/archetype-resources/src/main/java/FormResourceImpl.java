d301a0da3bef0ae31cde95ef0bba1b7f4360e3d2
Matteo Festugato <matteo.festugato@smc.it>
2026-04-10 16:28:56 +0200

    Issue #2009: Updates quarkus-openapi-generator-server dependency to 2.14.0-lts

--- enrichers/quarkus-base-enricher/archetype/src/main/resources/archetype-resources/src/main/java/FormResourceImpl.java
+++ enrichers/quarkus-base-enricher/archetype/src/main/resources/archetype-resources/src/main/java/FormResourceImpl.java
@@ -5,6 +5,7 @@ import io.openk9.enricher.api.beans.FieldValue;
 import io.openk9.enricher.api.beans.Form;
 import io.openk9.enricher.api.beans.FormField;
 import io.openk9.enricher.api.beans.FormFieldValidator;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -28,7 +29,7 @@ public class FormResourceImpl implements FormResource {
         formField1.setLabel("Test Form");
         formField1.setRequired(false);
         formField1.setSize(2.0);
-        formField1.setType(FormField.Type.text);
+        formField1.setType(FormField.Type.TEXT);
         formField1.setValidator(getValidator());
         formField1.setValues(getFiledValueList());
         formFieldList.add(formField1);
@@ -40,7 +41,7 @@ public class FormResourceImpl implements FormResource {
         formField2.setLabel("Main Object");
         formField2.setRequired(true);
         formField2.setSize(0.78);
-        formField2.setType(FormField.Type.number);
+        formField2.setType(FormField.Type.NUMBER);
         formField2.setValidator(getValidator());
         formField2.setValues(getFiledValueList());
         formFieldList.add(formField2);
