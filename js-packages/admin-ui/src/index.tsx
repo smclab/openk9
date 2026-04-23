5c97bc77344bdb6e93a4531d07a7e0f753fcf3cb
Giorgio Bartolomeo <giorgio.bartolomeo@smc.it>
2026-04-23 13:23:25 +0200

    issue #1699: init OIDC user before render to avoid 401 race with Apollo

--- js-packages/admin-ui/src/index.tsx
+++ js-packages/admin-ui/src/index.tsx
@@ -17,6 +17,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { authInit } from "./components/authentication";
 import reportWebVitals from "./reportWebVitals";
 
 
@@ -25,11 +26,14 @@ const root = ReactDOM.createRoot(
 );
 export const Id = "";
 
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-);
+authInit().then((ready) => {
+  if (!ready) return;
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+  );
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
