a421a897d745e011369e2a75191c463fcf4ceee4
Giorgio Bartolomeo <giorgio.bartolomeo@smc.it>
2026-04-23 12:55:11 +0200

    issue #1699: drop authInit from bootstrap (handled by AuthProvider)

--- js-packages/admin-ui/src/index.tsx
+++ js-packages/admin-ui/src/index.tsx
@@ -17,7 +17,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { authInit } from "./components/authentication";
 import reportWebVitals from "./reportWebVitals";
 
 
@@ -25,20 +24,12 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
 export const Id = "";
-declare global {
-  interface Window {
-    KEYCLOAK_URL: string;
-    KEYCLOAK_REALM: string;
-    KEYCLOAK_CLIENT_ID: string;
-  }
-}
-authInit().then(() => {
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
-});
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
