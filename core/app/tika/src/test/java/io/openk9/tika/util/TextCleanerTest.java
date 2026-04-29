a6bc4c06b68bf65c671b271cafe24aed327cdf75
Matteo Festugato <matteo.festugato@smc.it>
2026-04-29 10:11:50 +0200

    Issue #1972: Adds tests

---
+++ core/app/tika/src/test/java/io/openk9/tika/util/TextCleanerTest.java
@@ -0,0 +1,85 @@
+/*
+ * Copyright (c) 2020-present SMC Treviso s.r.l. All rights reserved.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.openk9.tika.util;
+
+import org.jboss.logging.Logger;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@DisplayName("TextCleaner")
+public class TextCleanerTest {
+
+	private static final Logger log =  Logger.getLogger(TextCleanerTest.class);
+
+	@Test
+	@DisplayName("Return cleaned text")
+	void returnCleanedHtmlContentText() {
+
+		List<String> rawTextList = new ArrayList<>();
+		rawTextList.add("This&nbsp;is a&nbsp;&nbsp;simple sentence with&nbsp;non-breaking spaces.");
+		rawTextList.add("&nbsp;&nbsp;This sentence starts and ends with non-breaking spaces.&nbsp;");
+		rawTextList.add("This    sentence     has      too many spaces.");
+		rawTextList.add("Special characters: &amp; &quot; &#39; should be decoded.");
+
+		Assertions.assertDoesNotThrow(() -> {
+				rawTextList
+					.forEach(rawText -> {
+						log.info(TextCleaner.cleanText(rawText));
+					});
+			}
+		);
+	}
+
+	@ParameterizedTest
+	@ValueSource(
+		strings = {
+			"""
+				<p>
+				The Euro-
+				pean Central Bank announced a new policy mea-
+				sure aimed at stabilizing inflation across the euro-
+				zone.&nbsp;According to officials, the inter-
+				vention will take effect im-
+				mediately.
+				</p>
+				
+				<p>
+				Experts say that the eco-
+				nomic outlook remains uncer-
+				tain, with several fac-
+				tors contributing to mar-
+				ket volatility.&nbsp;&nbsp;&nbsp;Investors are advised to main-
+				tain a diversified port-
+				folio.
+				</p>
+			"""
+		}
+	)
+	@DisplayName("Return cleaned HTML text")
+	void returnCleanedHtmlText(String rawText) {
+		Assertions.assertDoesNotThrow(() ->
+			log.info(TextCleaner.cleanText(rawText))
+		);
+	}
+}
