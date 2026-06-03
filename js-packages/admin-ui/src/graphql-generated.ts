6ac4b52f3f57a8d141ad4f55fdea1d6c1dee61b4
Lorenzo Venneri <lorenzov96@live.it>
2026-06-03 10:23:50 +0200

    Issue #2127: Enhance GraphQL types by adding comments for Scalars and cleaning up documentation in Autocomplete and Autocorrection inputs

--- js-packages/admin-ui/src/graphql-generated.ts
+++ js-packages/admin-ui/src/graphql-generated.ts
@@ -13,9 +13,13 @@ export type Scalars = {
   Boolean: boolean;
   Int: number;
   Float: number;
+  /** Scalar for BigDecimal */
   BigDecimal: any;
+  /** Scalar for BigInteger */
   BigInteger: any;
+  /** Scalar for DateTime */
   DateTime: any;
+  /** Scalar for FormConfigurations */
   FormConfigurations: any;
 };
 
@@ -157,7 +161,6 @@ export type Autocomplete = {
    *
    * Returns:
    * - The Set with all the associated DocTypeField, or null if not found.
-   *
    */
   fields?: Maybe<Connection_DocTypeField>;
   fuzziness?: Maybe<Fuzziness>;
@@ -185,37 +188,21 @@ export type AutocompleteFieldsArgs = {
 export type AutocompleteDtoInput = {
   description?: InputMaybe<Scalars['String']>;
   /**
-   * 	The field list used in the autocomplete query,
-   * 	must not be empty, all the docTypeField must be of type search_as_you_type
-   * 	 and must have a parent.
-   *
+   * The field list used in the autocomplete query,
+   * must not be empty, all the docTypeField must be of type search_as_you_type
+   *  and must have a parent.
    */
   fieldIds?: InputMaybe<Array<InputMaybe<Scalars['BigInteger']>>>;
-  /**
-   * 	Edit distance allowed for fuzzy matching (e.g., "0", "1", "2", or "AUTO")
-   *
-   */
+  /** 	Edit distance allowed for fuzzy matching (e.g., "0", "1", "2", or "AUTO") */
   fuzziness?: InputMaybe<Fuzziness>;
-  /**
-   * 	Minimum number of optional clauses that must match for a document to be returned
-   *
-   */
+  /** 	Minimum number of optional clauses that must match for a document to be returned */
   minimumShouldMatch?: InputMaybe<Scalars['String']>;
   name: Scalars['String'];
-  /**
-   * 	Boolean operator to combine query terms (AND or OR)
-   *
-   */
+  /** 	Boolean operator to combine query terms (AND or OR) */
   operator?: InputMaybe<BooleanOperator>;
-  /**
-   * 	When disabled, perfect matches with the searched text are removed from suggestions
-   *
-   */
+  /** 	When disabled, perfect matches with the searched text are removed from suggestions */
   perfectMatchIncluded?: InputMaybe<Scalars['Boolean']>;
-  /**
-   * 	Maximum number of results to return.
-   *
-   */
+  /** 	Maximum number of results to return. */
   resultSize?: InputMaybe<Scalars['Int']>;
 };
 
@@ -228,7 +215,6 @@ export type Autocorrection = {
    *
    * Returns:
    * - The associated DocTypeField, or null if not found.
-   *
    */
   autocorrectionDocTypeField?: Maybe<DocTypeField>;
   /** ISO-8601 */
@@ -248,49 +234,40 @@ export type Autocorrection = {
 
 export type AutocorrectionDtoInput = {
   /**
-   * 	The field used in the autocorrection query,
-   * 	must be a valid docTypeFieldId.
-   *
+   * The field used in the autocorrection query,
+   * must be a valid docTypeFieldId.
    */
   autocorrectionDocTypeFieldId?: InputMaybe<Scalars['BigInteger']>;
   description?: InputMaybe<Scalars['String']>;
-  /**
-   * 	If enabled, the search query will automatically be replaced with the corrected version from the autocorrection suggester.
-   *
-   */
+  /** 	If enabled, the search query will automatically be replaced with the corrected version from the autocorrection suggester. */
   enableSearchWithCorrection?: InputMaybe<Scalars['Boolean']>;
   /**
-   * 	The maximum allowed edit distance for suggestions. Valid values are in the range [1, 2].
-   * 	The default value is 2. Higher edit distances can lead to more suggestions, but also to less relevant ones.
-   *
+   * The maximum allowed edit distance for suggestions. Valid values are in the range [1, 2].
+   * The default value is 2. Higher edit distances can lead to more suggestions, but also to less relevant ones.
    */
   maxEdit?: InputMaybe<Scalars['Int']>;
   /**
-   * 	The minimum length of a word for it to be considered for suggestions.
-   * 	Terms shorter than this length will not generate suggestions.
-   *
+   * The minimum length of a word for it to be considered for suggestions.
+   * Terms shorter than this length will not generate suggestions.
    */
   minWordLength?: InputMaybe<Scalars['Int']>;
   name: Scalars['String'];
   /**
-   * 	An integer specifying the minimum length the matching prefix must have to start returning suggestions.
-   * 	For example, if 'prefix_length' is 3, an input of "ap" will not generate suggestions, while "app" will.
-   *
+   * An integer specifying the minimum length the matching prefix must have to start returning suggestions.
+   * For example, if 'prefix_length' is 3, an input of "ap" will not generate suggestions, while "app" will.
    */
   prefixLength?: InputMaybe<Scalars['Int']>;
   /**
-   * 	Specifies how suggestions should be ordered in the response. Valid values are:
-   * 	- 'score' (Default): Orders by similarity score (edit distance), then by document frequency, and finally by the term itself.
-   * 	- 'frequency': Orders by document frequency, then by similarity score, and finally by the term itself.
-   *
+   * Specifies how suggestions should be ordered in the response. Valid values are:
+   * - 'score' (Default): Orders by similarity score (edit distance), then by document frequency, and finally by the term itself.
+   * - 'frequency': Orders by document frequency, then by similarity score, and finally by the term itself.
    */
   sort?: InputMaybe<SortType>;
   /**
-   * 	Controls for which terms suggestions should be included in the response. Valid values are:
-   * 	- 'missing' (Default): Returns suggestions only for input terms not found in the index.
-   * 	- 'popular': Returns suggestions only if the suggested terms appear more frequently in documents than the original query term.
-   * 	- 'always': Always returns matching suggestions for every term in the input text, regardless of its presence or frequency in the index.
-   *
+   * Controls for which terms suggestions should be included in the response. Valid values are:
+   * - 'missing' (Default): Returns suggestions only for input terms not found in the index.
+   * - 'popular': Returns suggestions only if the suggested terms appear more frequently in documents than the original query term.
+   * - 'always': Always returns matching suggestions for every term in the input text, regardless of its presence or frequency in the index.
    */
   suggestMode?: InputMaybe<SuggestMode>;
 };
@@ -753,7 +730,6 @@ export type CreateRagConfigurationDtoInput = {
    * Controls context window merging behavior for chunk processing:
    * 0: Disables chunk merging.
    * > 0: Enables merging with specified window size.
-   *
    */
   chunkWindow?: InputMaybe<Scalars['Int']>;
   description?: InputMaybe<Scalars['String']>;
@@ -767,13 +743,11 @@ export type CreateRagConfigurationDtoInput = {
   /**
    * Prompt template used specifically in RAG-as-tool configurations when the RAG
    * tool is available but not invoked by the LLM.
-   *
    */
   promptNoRag?: InputMaybe<Scalars['String']>;
   /**
    * Description of the RAG tool's capabilities, used in RAG-as-tool implementations
    * to help the LLM decide when to invoke it.
-   *
    */
   ragToolDescription?: InputMaybe<Scalars['String']>;
   /** The range filter configuration with start and end values (default are [0, 5]). */
@@ -781,7 +755,6 @@ export type CreateRagConfigurationDtoInput = {
   /**
    * Boolean flag that controls whether a large language model should reformulate
    * the input prompt before processing it using rephrasePrompt.
-   *
    */
   reformulate?: InputMaybe<Scalars['Boolean']>;
   /** Prompt template used if reformulate is set to true. */
@@ -1711,14 +1684,12 @@ export type EmbeddingModelDtoInput = {
    * Authentication API key required for accessing the embedding model's service.
    * Necessary for providers that require authentication to use their embedding API.
    * Ensure this key is kept confidential.
-   *
    */
   apiKey?: InputMaybe<Scalars['String']>;
   /**
    * The API URL for the embedding model's endpoint.
    * Required only when using a custom embedding service or a model hosted
    * on a private/internal network.
-   *
    */
   apiUrl?: InputMaybe<Scalars['String']>;
   description?: InputMaybe<Scalars['String']>;
@@ -1737,7 +1708,6 @@ export type EmbeddingModelDtoInput = {
    * 768 dimensions: Standard for many BERT-based models
    * 1,024 dimensions: High-performance models
    * 1,536 dimensions: Ultra-high performance models
-   *
    */
   vectorSize: Scalars['Int'];
 };
@@ -2020,7 +1990,6 @@ export type Mutation = {
    *
    * Returns:
    * - A Response containing the created or updated Autocomplete configuration.
-   *
    */
   autocomplete?: Maybe<Response_Autocomplete>;
   /**
@@ -2037,7 +2006,6 @@ export type Mutation = {
    *
    * Returns:
    * - A Response containing the created or updated Autocorrection configuration.
-   *
    */
   autocorrection?: Maybe<Response_Autocorrection>;
   bindAnalyzerToDocTypeField?: Maybe<Tuple2_DocTypeField_Analyzer>;
@@ -2053,7 +2021,6 @@ export type Mutation = {
    * - A tuple containing:
    *   - `bucket`: The updated Bucket with the linked Autocomplete.
    *   - `autocomplete`: The linked Autocomplete.
-   *
    */
   bindAutocompleteToBucket?: Maybe<Tuple2_Bucket_Autocomplete>;
   /**
@@ -2067,7 +2034,6 @@ export type Mutation = {
    * - A tuple containing:
    *   - `bucket`: The updated Bucket with the linked Autocorrection.
    *   - `autocorrection`: The linked Autocorrection.
-   *
    */
   bindAutocorrectionToBucket?: Maybe<Tuple2_Bucket_Autocorrection>;
   bindDataIndexToDatasource?: Maybe<Tuple2_Datasource_DataIndex>;
@@ -2092,7 +2058,6 @@ export type Mutation = {
    * - A tuple containing:
    *   - `bucket`: The updated Bucket with the linked RAGConfiguration.
    *   - `ragConfiguration`: The linked RAGConfiguration.
-   *
    */
   bindRAGConfigurationToBucket?: Maybe<Tuple2_Bucket_RagConfiguration>;
   bindSearchAnalyzerToDocTypeField?: Maybe<Tuple2_DocTypeField_Analyzer>;
@@ -2111,7 +2076,6 @@ export type Mutation = {
    *
    * Returns:
    * - The RAGConfiguration entity created.
-   *
    */
   createRAGConfiguration?: Maybe<Response_RagConfiguration>;
   createSubField?: Maybe<Response_DocTypeField>;
@@ -2127,7 +2091,6 @@ export type Mutation = {
    *
    * Returns:
    * - The deleted Autocomplete configuration.
-   *
    */
   deleteAutocomplete?: Maybe<Autocomplete>;
   /**
@@ -2138,7 +2101,6 @@ export type Mutation = {
    *
    * Returns:
    * - The deleted Autocorrection configuration.
-   *
    */
   deleteAutocorrection?: Maybe<Autocorrection>;
   deleteBucket?: Maybe<Bucket>;
@@ -2147,7 +2109,6 @@ export type Mutation = {
    * Deletes a DataIndex entity by its ID after validating the provided name matches the entity.
    * Requires both the dataIndexId and dataIndexName (as a confirmation mechanism) to prevent
    * accidental deletions.
-   *
    */
   deleteDataIndex?: Maybe<DataIndex>;
   deleteDatasource?: Maybe<Datasource>;
@@ -2155,14 +2116,12 @@ export type Mutation = {
    * Deletes a DocType entity by its ID after validating the provided name matches the entity.
    * Requires both the docTypeId and docTypeName (as a confirmation mechanism) to prevent
    * accidental deletions.
-   *
    */
   deleteDocType?: Maybe<DocType>;
   /**
    * Deletes a DocTypeField entity by its ID after validating the provided name matches the entity.
    * Requires both the docTypeFieldId and docTypeFieldName (as a confirmation mechanism) to prevent
    * accidental deletions.
-   *
    */
   deleteDocTypeField?: Maybe<DocTypeField>;
   deleteDocTypeFieldTranslation?: Maybe<Tuple2_String_String>;
@@ -2193,7 +2152,6 @@ export type Mutation = {
    * Returns validation errors if the provided DTO fails validation.
    * Requires docTypeFieldName (as a confirmation mechanism) to prevent accidental modifications
    * when updating or patching an existing DocTypeField.
-   *
    */
   docTypeField?: Maybe<Response_DocTypeField>;
   /**
@@ -2202,7 +2160,6 @@ export type Mutation = {
    * Otherwise, updates or patches the existing DocTypeField depending on the patch flag.
    * Requires docTypeFieldName (as a confirmation mechanism) to prevent accidental modifications
    * when updating or patching an existing DocTypeField.
-   *
    */
   docTypeFieldWithAnalyzer?: Maybe<Response_DocTypeField>;
   docTypeTemplate?: Maybe<Response_DocTypeTemplate>;
@@ -2270,7 +2227,6 @@ export type Mutation = {
    * - A tuple containing:
    *   - `bucket`: The updated Bucket after unbinding the Autocomplete.
    *   - `autocomplete`: Always null.
-   *
    */
   unbindAutocompleteFromBucket?: Maybe<Tuple2_Bucket_Autocomplete>;
   /**
@@ -2285,7 +2241,6 @@ export type Mutation = {
    * - A tuple containing:
    *   - `bucket`: The updated Bucket after unbinding the Autocorrection.
    *   - `autocorrection`: Always null.
-   *
    */
   unbindAutocorrectionFromBucket?: Maybe<Tuple2_Bucket_Autocorrection>;
   unbindDataIndexFromDatasource?: Maybe<Datasource>;
@@ -2312,7 +2267,6 @@ export type Mutation = {
    * - A tuple containing:
    *   - `bucket`: The updated Bucket after unbinding the RAGConfiguration.
    *   - `ragConfiguration`: Always null.
-   *
    */
   unbindRAGConfigurationFromBucket?: Maybe<Tuple2_Bucket_RagConfiguration>;
   unbindSearchAnalyzerFromDocTypeField?: Maybe<Tuple2_DocTypeField_Analyzer>;
@@ -2329,7 +2283,6 @@ export type Mutation = {
    *
    * Returns:
    * - The RAGConfiguration entity created.
-   *
    */
   updateRAGConfiguration?: Maybe<Response_RagConfiguration>;
   userField?: Maybe<AclMapping>;
@@ -3553,7 +3506,6 @@ export type Query = {
    *
    * Returns:
    * - The requested Autocomplete configuration, or null if not found.
-   *
    */
   autocomplete?: Maybe<Autocomplete>;
   /**
@@ -3573,7 +3525,6 @@ export type Query = {
    *
    * Returns:
    * - A Connection containing the requested Autocomplete configurations with pagination info.
-   *
    */
   autocompletes?: Maybe<Connection_Autocomplete>;
   /**
@@ -3584,7 +3535,6 @@ export type Query = {
    *
    * Returns:
    * - The requested Autocorrection configuration, or null if not found.
-   *
    */
   autocorrection?: Maybe<Autocorrection>;
   /**
@@ -3604,7 +3554,6 @@ export type Query = {
    *
    * Returns:
    * - A Connection containing the requested Autocorrection configurations with pagination info.
-   *
    */
   autocorrections?: Maybe<Connection_Autocorrection>;
   bucket?: Maybe<Bucket>;
@@ -3689,7 +3638,6 @@ export type Query = {
    *
    * Returns:
    * - A list of unbound Autocomplete configurations available for the specified Bucket.
-   *
    */
   unboundAutocompleteByBucket?: Maybe<Array<Maybe<Autocomplete>>>;
   /**
@@ -3702,7 +3650,6 @@ export type Query = {
    *
    * Returns:
    * - A list of unbound Autocorrection configurations available for the specified Bucket.
-   *
    */
   unboundAutocorrectionByBucket?: Maybe<Array<Maybe<Autocorrection>>>;
   unboundBucketsByDatasource?: Maybe<Array<Maybe<Bucket>>>;
@@ -3720,7 +3667,6 @@ export type Query = {
    *
    * Returns:
    * - A list of unbound DocTypeFields available for the specified Autocomplete.
-   *
    */
   unboundDocTypeFieldByAutocomplete?: Maybe<Array<Maybe<DocTypeField>>>;
   /**
@@ -3734,7 +3680,6 @@ export type Query = {
    *
    * Returns:
    * - A list of unbound DocTypeFields available for the specified Autocorrection.
-   *
    */
   unboundDocTypeFieldByAutocorrection?: Maybe<Array<Maybe<DocTypeField>>>;
   /** Fetches DocTypeFields unbound to the provided SuggestionCategory ID with FieldType KEYWORD or I18N. */
@@ -3754,7 +3699,6 @@ export type Query = {
    *
    * Returns:
    * - A list of RAGConfiguration entities that match the criteria.
-   *
    */
   unboundRAGConfigurationByBucket?: Maybe<Array<Maybe<RagConfiguration>>>;
   unboundTabsByTokenTab?: Maybe<Array<Maybe<Tab>>>;
@@ -4593,7 +4537,6 @@ export type RagConfigurationDtoInput = {
    * Controls context window merging behavior for chunk processing:
    * 0: Disables chunk merging.
    * > 0: Enables merging with specified window size.
-   *
    */
   chunkWindow?: InputMaybe<Scalars['Int']>;
   description?: InputMaybe<Scalars['String']>;
@@ -4607,13 +4550,11 @@ export type RagConfigurationDtoInput = {
   /**
    * Prompt template used specifically in RAG-as-tool configurations when the RAG
    * tool is available but not invoked by the LLM.
-   *
    */
   promptNoRag?: InputMaybe<Scalars['String']>;
   /**
    * Description of the RAG tool's capabilities, used in RAG-as-tool implementations
    * to help the LLM decide when to invoke it.
-   *
    */
   ragToolDescription?: InputMaybe<Scalars['String']>;
   /** The range filter configuration with start and end values (default are [0, 5]). */
@@ -4621,7 +4562,6 @@ export type RagConfigurationDtoInput = {
   /**
    * Boolean flag that controls whether a large language model should reformulate
    * the input prompt before processing it using rephrasePrompt.
-   *
    */
   reformulate?: InputMaybe<Scalars['Boolean']>;
   /** Prompt template used if reformulate is set to true. */
@@ -9241,6 +9181,7 @@ export type CreateOrUpdateDocumentTypeFieldMutationFn = Apollo.MutationFunction<
  *   variables: {
  *      documentTypeId: // value for 'documentTypeId'
  *      documentTypeFieldId: // value for 'documentTypeFieldId'
+ *      docTypeFieldName: // value for 'docTypeFieldName'
  *      name: // value for 'name'
  *      fieldName: // value for 'fieldName'
  *      description: // value for 'description'
@@ -16274,4 +16215,4 @@ export function useEnrichPipelineWithItemsMutation(baseOptions?: Apollo.Mutation
 export type EnrichPipelineWithItemsMutationHookResult = ReturnType<typeof useEnrichPipelineWithItemsMutation>;
 export type EnrichPipelineWithItemsMutationResult = Apollo.MutationResult<EnrichPipelineWithItemsMutation>;
 export type EnrichPipelineWithItemsMutationOptions = Apollo.BaseMutationOptions<EnrichPipelineWithItemsMutation, EnrichPipelineWithItemsMutationVariables>;
-// Generated on 2026-05-18T11:58:31+02:00
+// Generated on 2026-06-03T10:23:19+02:00
